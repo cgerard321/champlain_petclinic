f30412eca4461c183e80cd179c45b039af117d5f
Fube <fubeitch@gmail.com>
2021-09-15 14:56:53 -0400

    Added self referencing parent to roles migration script

--- auth-service/src/main/resources/db/migration/V1.1__Add_roles_table.sql
+++ auth-service/src/main/resources/db/migration/V1.1__Add_roles_table.sql
@@ -1,5 +1,7 @@
 CREATE TABLE auth.ROLES(
         id INT NOT NULL AUTO_INCREMENT,
         name VARCHAR(2048) NOT NULL UNIQUE,
+        parent_id int NULL,
+        FOREIGN KEY(parent_id) REFERENCES auth.ROLES(id),
         PRIMARY KEY(id)
-)
+);
