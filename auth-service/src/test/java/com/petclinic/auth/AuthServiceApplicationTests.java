781e91b1125ecc5d08a83fb7ff854fc8c8ac54f0
Fube <8064235+Fube@users.noreply.github.com>
2021-09-15 11:54:18 -0400

    conf(AUTH-CPC-59): add user model (#4)
    * Added auth-service to project
    
    * Removed useless files
    
    * Added JWT dep
    
    * Added User model
    
    * Added auth dockerfile
    
    * Added UserRepo tests
    
    * Added flyway core dep
    
    * Added flyway and datasource properties
    
    * Added flyway init script
    
    * Added mysql for auth to docker-compose
    
    * Changed default flyway pass
    
    * Changed datasource port
    
    * Removed deprecated connector property
    
    * Changed flyway properties
    
    * Changed flyway password in properties
    
    * Added id as primary key
    
    * Refactored persistence tests to their own file
    
    * Switched to yml config
    
    * Finished h2 setup and initial test
    
    * Added cleanUp code
    
    * Refactored shared default user save
    
    * Added remove test
    
    * Switched to alpine
    
    * Fixed docker-compose a bit. Added docker profile to config
    
    * Removed docker-compose versioning to support deprecated directives like mem_limit and depends_on
    
    * Added auth-service dependency
    
    * Added email uniqueness test
    
    * Added builder for User. refactored addDefaultUser to make a deep copy of default object
    
    * Changed addDefaultUser to randomize id
    
    * Revert "Changed addDefaultUser to randomize id"
    
    This reverts commit 67d159d8eae14855419116bc28009f23f46d3565.
    
    * Changed addDefaultUser to randomize id
    
    * Refactored add two users with same email check to use addDefaultUser
    
    * Turned off hibernate auto ddl
    
    * Removed unique annotation for User.email
    
    * Changed back to SpringBootTest. Specified schema and table to User entity
    
    * Added test profile to application tests

---
+++ auth-service/src/test/java/com/petclinic/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,58 @@
+package com.petclinic.auth;
+
+import com.petclinic.auth.User.User;
+import com.petclinic.auth.User.UserRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AuthServiceApplicationTests {
+
+	@MockBean
+	private UserRepo mockUserRepo;
+
+	private List<User> MOCK_USERS;
+	private final int MOCK_USER_LEN = 10;
+
+	@BeforeEach
+	void setup() {
+
+		MOCK_USERS = new ArrayList<>();
+		for (int i = 1; i <= MOCK_USER_LEN; i++) {
+
+			MOCK_USERS.add(new User(
+					i,
+					format("username-%d", i),
+					format("password-%d", i),
+					format("email-%d", i)
+			));
+		}
+		when(mockUserRepo.findAll())
+				.thenReturn(MOCK_USERS);
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+
+	@Test
+	@DisplayName("Retrieve all Users from mock database")
+	void retrieve_all_users_from_mock_database() {
+		assertEquals(mockUserRepo.findAll().size(), 10);
+	}
+
+}
