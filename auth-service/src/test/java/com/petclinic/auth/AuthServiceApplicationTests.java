d2d4e2e9ea3e4aa7ccd3991ee124c6d554dce2ed
Fube <fubeitch@gmail.com>
2021-09-15 00:28:03 -0400

    Added test profile to application tests

--- auth-service/src/test/java/com/petclinic/auth/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/petclinic/auth/AuthServiceApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ import static org.mockito.Mockito.when;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
 
 	@MockBean
