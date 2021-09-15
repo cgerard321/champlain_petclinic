cc6edded5d384c7d3866bbdebfc9360489034a78
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-09-14 20:04:01 -0400

    Created the Billing microservice folder to the repository

---
+++ billing-service/src/test/java/com/petclinic/billing/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
