b7132f3b4465f9141ae58919121d176f4910e912
Fube <fubeitch@gmail.com>
2021-09-16 17:27:58 -0400

    Fleshed out RoleController

--- auth-service/src/main/java/com/petclinic/auth/Role/RoleController.java
+++ auth-service/src/main/java/com/petclinic/auth/Role/RoleController.java
@@ -1,5 +1,7 @@
 package com.petclinic.auth.Role;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/roles")
+@Slf4j
+@RequiredArgsConstructor
 public class RoleController {
 
+    private final RoleRepo roleRepo;
+    private final RoleMapper roleMapper;
+
     @PostMapping
-    public Role createRole(@RequestBody RoleIDLessDTO role) {
-        return null;
+    public Role createRole(@RequestBody RoleIDLessDTO dto) {
+
+        log.info("Received role dto, trying to convert model");
+        log.info("DTO info: { name={}, parent={} }", dto.getName(), dto.getParent());
+        final Role role = roleMapper.idLessDTOToModel(dto);
+        log.info("Successfully converted dto -> model");
+
+        log.info("Trying to persist role");
+        final Role saved = roleRepo.save(role);
+        log.info("Successfully persisted role");
+
+        return saved;
     }
 }
