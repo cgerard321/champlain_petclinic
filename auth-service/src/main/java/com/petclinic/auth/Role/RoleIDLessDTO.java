8afb3f0730f01549615785f815bd6127143e8794
Fube <fubeitch@gmail.com>
2021-09-16 16:58:15 -0400

    Added name only constructor to RoleIdLessDTO

--- auth-service/src/main/java/com/petclinic/auth/Role/RoleIDLessDTO.java
+++ auth-service/src/main/java/com/petclinic/auth/Role/RoleIDLessDTO.java
@@ -11,6 +11,11 @@ import lombok.NoArgsConstructor;
 @Builder(toBuilder = true)
 public class RoleIDLessDTO {
 
+    public RoleIDLessDTO(String name) {
+        this.name = name;
+        this.parent = null;
+    }
+
     private String name;
     private Role parent;
 }
