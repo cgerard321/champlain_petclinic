44ec2f691f9de12dd85aaf1def98e3f85787a9ca
LemireCoding <54686754+LemireCoding@users.noreply.github.com>
2021-09-17 14:32:03 -0400

    Feat/bill cpc 108 new repo module (#10)
    * Changed the contents of the create-ms-projects file to now create a billing microservice
    
    * Created the Billing microservice folder to the repository
    
    * Altered the settings.gradle of the repo to build the billing service
    
    Co-authored-by: Tristan Lafleur <tristanblacklafleur@hotmail.ca>
    Co-authored-by: Trim0500 <70395022+Trim0500@users.noreply.github.com>

--- create-ms-projects.bash
+++ create-ms-projects.bash
@@ -5,48 +5,12 @@ spring init \
 --build=gradle \
 --java-version=1.8 \
 --packaging=jar \
---name=customers-service \
---package-name=com.petclinic.customers \
---groupId=com.petclinic.customers \
+--name=billing-service \
+--package-name=com.petclinic.billing \
+--groupId=com.petclinic.billing \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
-customers-service
-
-spring init \
---boot-version=2.3.2.RELEASE \
---build=gradle \
---java-version=1.8 \
---packaging=jar \
---name=vets-service \
---package-name=com.petclinic.vets \
---groupId=com.petclinic.vets \
---dependencies=actuator,webflux \
---version=1.0.0-SNAPSHOT \
-vets-service
-
-spring init \
---boot-version=2.3.2.RELEASE \
---build=gradle \
---java-version=1.8 \
---packaging=jar \
---name=visits-service \
---package-name=com.petclinic.visits \
---groupId=com.petclinic.visits \
---dependencies=actuator,webflux \
---version=1.0.0-SNAPSHOT \
-visits-service
-
-spring init \
---boot-version=2.3.2.RELEASE \
---build=gradle \
---java-version=1.8 \
---packaging=jar \
---name=api-gateway \
---package-name=com.petclinic.api-gateway \
---groupId=com.petclinic.api-gateway \
---dependencies=actuator,webflux \
---version=1.0.0-SNAPSHOT \
-api-gateway
+billing-service
 
 spring init \
 --boot-version=2.3.2.RELEASE \
