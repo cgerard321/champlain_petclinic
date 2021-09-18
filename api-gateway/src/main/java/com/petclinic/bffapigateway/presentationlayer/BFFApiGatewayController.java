98c5711f77ee93b4bb4707092934d412331b50cd
Fube <fubeitch@gmail.com>
2021-09-17 23:48:14 -0400

    Removed test code from BFFController

--- api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/BFFApiGatewayController.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/BFFApiGatewayController.java
@@ -69,46 +69,21 @@ public class BFFApiGatewayController {
         return vetsServiceClient.getVets();
     }
 
-    private final HashMap<Integer, HashMap<Object, Object>> roles = new HashMap<Integer, HashMap<Object, Object>>() {{
-        put(1, new HashMap<Object, Object>(){{ put("name", "test"); }});
-        put(2, new HashMap<Object, Object>(){{ put("name", "test1"); }});
-        put(3, new HashMap<Object, Object>(){{ put("name", "test2"); }});
-    }};
-
+    // TODO: Hook this up to auth service
     @GetMapping(value = "/admin/roles")
-    public Map<String, Object> getRoles() {
-        Map<String, Object> toRet = new HashMap<>();
-        List<Map<Object, Object>> content = new ArrayList<>();
-
-
-        roles.forEach((k, v) -> {
-            HashMap<Object, Object> role = new HashMap<>();
-            role.put("id", k);
-            role.put("name", v.get("name"));
-            content.add(role);
-        });
-
-        toRet.put("content", content);
-
-        return toRet;
+    public Object getRoles() {
+        return null;
     }
 
+    // TODO: Hook this up to auth service
     @DeleteMapping(value = "/admin/roles/{id}")
     public void deleteRole(@PathVariable int id) {
-        roles.remove(id);
+
     }
 
+    // TODO: Hook this up to auth service
     @PostMapping(value = "/admin/roles")
-    public Map<Object, Object> addRole(
-            @RequestBody Map<String, String> body
-    ) {
-        final String name = body.get("name");
-        final int id = new Random().nextInt() & Integer.MAX_VALUE;
-        roles.put(id, new HashMap<Object, Object>(){{ put("name", name); }});
-
-        return new HashMap<Object, Object>() {{
-            put("id", id);
-            put("name", roles.get(id).get("name"));
-        }};
+    public Object addRole() {
+        return null;
     }
 }
