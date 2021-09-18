1f3dce70b833071988d72e2022c630a9e033f472
Fube <fubeitch@gmail.com>
2021-09-17 20:51:48 -0400

    Added really dirty mockdata to render on roles page

--- api-gateway/src/main/resources/static/scripts/auth/admin/roles-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/auth/admin/roles-details.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('rolesDetails')
     .controller('RolesDetailsController', ['$http', '$stateParams', function ($http, $stateParams) {
-        $http.get('api/gateway/roles')
+        $http.get('api/gateway/admin/roles')
             .then(res => (this.roles = res.data))
             .catch(console.log);
     }]);
