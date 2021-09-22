5d416dd6809d84323df879d07a1f11b08cac75ab
Christian Chitanu <Christian@Cristians-MacBook-Pro.local>
2021-09-21 21:50:41 -0400

    first commit to branch with testing changes to for modal

--- api-gateway/src/main/resources/static/scripts/vet-list/vet-list.controller.js
+++ api-gateway/src/main/resources/static/scripts/vet-list/vet-list.controller.js
@@ -1,8 +1,19 @@
 'use strict';
 
-angular.module('vetList')
-    .controller('VetListController', ['$http', function ($http) {
+app = angular.module('vetList')
+.controller('VetListController', ['$http', function ($http) {
         var self = this;
+        this.show = (e) => {
+//               let body = document.getElementsByTagName("body")[0];
+               let child = e.target.
+               child.classList.add("modalOn");
+               child.classList.remove("modalOff");
+
+        }
+        this.hide = () => {
+//            document.querySelector('.modal').remove();
+        }
+        this.test = console.log;
 
         $http.get('api/gateway/vets').then(function (resp) {
             self.vetList = resp.data;
