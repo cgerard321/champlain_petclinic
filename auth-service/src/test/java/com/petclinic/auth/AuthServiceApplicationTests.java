efb50bb0b49d82c7e0dbdedf02d3e4d741b2c5d8
Fube <fubeitch@gmail.com>
2021-09-22 00:28:34 -0400

    Got JaCoCo to ignore AuthServiceApplication in coverage report

--- auth-service/src/test/java/com/petclinic/auth/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/petclinic/auth/AuthServiceApplicationTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -56,8 +57,7 @@ class AuthServiceApplicationTests {
 	}
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads(){}
 
 	@Test
 	@DisplayName("Map id less role to role")
