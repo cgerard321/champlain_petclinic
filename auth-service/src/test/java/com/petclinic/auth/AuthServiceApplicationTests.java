081cef3a99e836bad13bb5ffe79e17a8726d38bc
Fube <fubeitch@gmail.com>
2021-09-22 00:09:45 -0400

    Added more asserts for better coverage

--- auth-service/src/test/java/com/petclinic/auth/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/petclinic/auth/AuthServiceApplicationTests.java
@@ -16,14 +16,27 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class AuthServiceApplicationTests {
+
+	final String
+			USER = "user",
+			PASS = "pass",
+			EMAIL = "email",
+			ROLE_NAME = "role";
+	final Role role = new Role(0, ROLE_NAME);
+
+	final Set<Role> ROLES = new HashSet<Role>() {{
+		add(role);
+	}};
+
+	final long ID = 1L;
+
 
 	@Autowired
 	private UserRepo userRepo;
@@ -60,25 +73,37 @@ class AuthServiceApplicationTests {
 	@DisplayName("User setters test")
 	void user_setters() {
 
-		final String
-				USER = "user",
-				PASS = "pass",
-				EMAIL = "email",
-				ROLE_NAME = "role";
-		final Role role = new Role(0, ROLE_NAME);
-
-		final long ID = 1L;
-
-		final Set<Role> ROLES = new HashSet<Role>() {{
-			add(role);
-		}};
-
 		final User user = new User();
 		user.setUsername(USER);
 		user.setRoles(ROLES);
 		user.setEmail(EMAIL);
 		user.setPassword(PASS);
 		user.setId(ID);
+
+		assertEquals(USER, user.getUsername());
+		assertEquals(PASS, user.getPassword());
+		assertEquals(EMAIL, user.getEmail());
+		assertEquals(ID, user.getId());
+		assertTrue(user.getRoles().stream().anyMatch(n -> n.getName().equals(ROLE_NAME)));
+	}
+
+	@Test
+	@DisplayName("User builder test")
+	void user_builder() {
+		final User user = User.builder()
+				.roles(ROLES)
+				.id(ID)
+				.email(EMAIL)
+				.password(PASS)
+				.username(USER)
+				.build();
+
+		assertEquals(
+				format(
+				"User.UserBuilder(id=%d, username=%s, password=%s, email=%s, roles=%s)",
+						user.getId(), user.getUsername(), user.getPassword(), user.getEmail(),
+						user.getRoles().toString()),
+				user.toBuilder().toString());
 
 		assertEquals(USER, user.getUsername());
 		assertEquals(PASS, user.getPassword());
