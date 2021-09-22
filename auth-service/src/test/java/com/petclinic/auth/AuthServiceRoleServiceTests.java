6e76b512ebf2357def73858cb2ab56a5b17db492
Fube <fubeitch@gmail.com>
2021-09-21 23:05:31 -0400

    Added failing test for delete role by id through role service

--- auth-service/src/test/java/com/petclinic/auth/AuthServiceRoleServiceTests.java
+++ auth-service/src/test/java/com/petclinic/auth/AuthServiceRoleServiceTests.java
@@ -1,7 +1,9 @@
 package com.petclinic.auth;
 
 import com.petclinic.auth.Role.Role;
+import com.petclinic.auth.Role.RoleRepo;
 import com.petclinic.auth.Role.RoleService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,11 +22,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AuthServiceRoleServiceTests {
 
     @Autowired
+    private RoleRepo roleRepo;
+
+    @Autowired
     private RoleService roleService;
 
+    @BeforeEach
+    void setup() {
+        roleRepo.deleteAll();
+    }
+
     @Test
-    @DisplayName("Create role through role service")
-    void create_role_through_role_service() {
+    @DisplayName("Create role")
+    void create_role() {
 
         final Role role = new Role(0, "TEST_ROLE");
         final Role saved = roleService.createRole(role);
@@ -32,5 +42,17 @@ public class AuthServiceRoleServiceTests {
         assertEquals(saved.getName(), role.getName());
         assertEquals(saved.getParent(), role.getParent());
         assertThat(saved.getId(), instanceOf(Long.TYPE));
+    }
+
+    @Test
+    @DisplayName("Delete role by id")
+    void delete_role_by_id() {
+
+        final Role role = new Role(0, "TEST_ROLE");
+        final Role saved = roleService.createRole(role);
+
+        roleService.deleteById(saved.getId());
+
+        assertEquals(0, roleRepo.count());
     }
 }
