e27ef5780d168a98c71e2c1c2d4ef3e6aa1d4ace
ArianneBonneau <70343893+ArianneBonneau@users.noreply.github.com>
2021-09-23 17:52:44 -0400

    feat(AUTH-CPC-105): clear text pwd for login (#24)
    * UI for login using angular
    
    * added clear text password
    
    * test login
    
    * removed test for the login
    
    * added clear text password option
    
    * removed login service field

--- api-gateway/src/main/resources/static/scripts/login-form/login-form.component.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('loginForm')
-    .component('loginForm', {
-        templateUrl: 'scripts/login-form/login-form.template.html',
-        controller: 'LoginFormController'
-    });
