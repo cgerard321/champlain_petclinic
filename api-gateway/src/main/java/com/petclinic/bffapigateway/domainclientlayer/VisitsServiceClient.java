77cdf670b50b40c57c0d7a413e6da58db3a7348c
cjayneb <70963776+cjayneb@users.noreply.github.com>
2021-09-22 20:08:18 -0400

    Added import for require args constructor
    I dont know why it got removed, but now its back to not cause any problem

--- api-gateway/src/main/java/com/petclinic/bffapigateway/domainclientlayer/VisitsServiceClient.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/domainclientlayer/VisitsServiceClient.java
@@ -1,6 +1,7 @@
 package com.petclinic.bffapigateway.domainclientlayer;
 
 import com.petclinic.bffapigateway.dtos.Visits;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
