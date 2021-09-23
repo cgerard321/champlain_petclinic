96475de4f66c62806fe85e7294bfc45d508e17e3
cjayneb <70963776+cjayneb@users.noreply.github.com>
2021-09-22 21:46:31 -0400

    conf(VISE-CPC-174): Adding a business layer and test classes (#20)
    * delete request does not reach the repository call to delete or repository delete methods simply dont work(unlikely)
    
    * reput the specific deletion of a visit for repository method call
    
    * BIG REFACTOR
    
    * 3 layers are in place with start of getVisitsForPet along with one test example for each layer
    
    * Added import for require args constructor: I dont know why it got removed, but now its back to not cause any problem

---
+++ visits-service/src/main/java/com/petclinic/visits/businesslayer/VisitsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.petclinic.visits.businesslayer;
+
+import com.petclinic.visits.datalayer.Visit;
+import com.petclinic.visits.datalayer.VisitRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class VisitsServiceImpl implements VisitsService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitsServiceImpl(VisitRepository repo){
+        this.visitRepository = repo;
+    }
+
+    @Override
+    public Visit addVisit(Visit visit) {
+        return null;
+    }
+
+    @Override
+    public List<Visit> getVisitsForPet(int petId) {
+        log.info("Calling visit repo to get visits for pet with petId: {}", petId);
+        return visitRepository.findByPetId(petId);
+    }
+
+    @Override
+    public void deleteVisit(int visitId) {
+
+    }
+
+    @Override
+    public List<Visit> getVisitsForPets(List<Integer> petIds) {
+        return visitRepository.findByPetIdIn(petIds);
+    }
+}
---
+++ visits-service/src/test/java/com/petclinic/visits/datalayer/PersistenceTests.java
@@ -0,0 +1,53 @@
+package com.petclinic.visits.datalayer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Transactional(propagation = NOT_SUPPORTED)
+public class PersistenceTests {
+
+    @Autowired
+    private VisitRepository repo;
+
+    @BeforeEach
+    public void setupDb(){
+        repo.deleteAll();
+
+        // add setup data here
+        Visit visit = Visit.visit()
+                .id(1)
+                .petId(1)
+                .build();
+        Visit visit1 = Visit.visit()
+                .id(2)
+                .petId(1)
+                .build();
+
+        List<Visit> list = Arrays.asList(visit, visit1);
+        repo.saveAll(list);
+    }
+
+    @Test
+    public void getVisitsForPet(){
+        List<Visit> repoResponse = repo.findByPetId(1);
+
+        assertThat(repoResponse, hasSize(2));
+    }
+}
