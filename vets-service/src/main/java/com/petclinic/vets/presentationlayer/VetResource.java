461060a4371953df5b6e2cb2213de654812707f3
Iana-Sha <70303879+Iana-Sha@users.noreply.github.com>
2021-09-24 11:03:33 -0400

    (Feat/VETS CPC-33) Added the Display info page when clicked on the vet's name (#31)

--- vets-service/src/main/java/com/petclinic/vets/presentationlayer/VetResource.java
+++ vets-service/src/main/java/com/petclinic/vets/presentationlayer/VetResource.java
@@ -4,10 +4,12 @@ import com.petclinic.vets.datalayer.Vet;
 import com.petclinic.vets.datalayer.VetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Juergen Hoeller
@@ -29,4 +31,10 @@ class VetResource {
     public List<Vet> showResourcesVetList() {
         return vetRepository.findAll();
     }
+
+    @GetMapping(value = "/{vetId}")
+    public Optional<Vet> findVet(@PathVariable("vetId") int vetId) {
+        return vetRepository.findById(vetId);
+    }
+
 }
