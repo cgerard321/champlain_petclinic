bf61b77f109b75344d1e44af828bd550fe93bbee
Christian C <christian.chitanu@hotmail.com>
2021-09-26 15:21:37 -0400

    changed some ui html input submission elements and refined the current VetResourceTests

--- vets-service/src/test/java/com/petclinic/vets/presentationlayer/VetResourceTest.java
+++ vets-service/src/test/java/com/petclinic/vets/presentationlayer/VetResourceTest.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 import static  org.hamcrest.MatcherAssert.assertThat;
 import static  org.hamcrest.Matcher.*;
 import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -46,7 +47,7 @@ class VetResourceTest {
 
 
 	@Test
-	@DisplayName("Should get a list of vets")
+	@DisplayName("Get List of Vets Resource Test")
 	void shouldGetAListOfVets() throws Exception {
 
 		Vet vet = new Vet();
@@ -60,21 +61,7 @@ class VetResourceTest {
 	}
 
 	@Test
-	void disableAVet() throws Exception {
-		//arrange
-		Vet vet = new Vet();
-		vet.setIsActive(0);
-		//act
-		given(vetRepository.findAll()).willReturn(asList(vet));
-		//assert
-		mvc.perform(get("/vets").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$[0].isActive").value(0));
-	}
-
-
-	@Test
-	@DisplayName("Should get all the fields for a vet and check if they are okay")
+	@DisplayName("Get all Vets Resource Test")
 	void shouldGetAllTheFieldsForAVet() throws Exception{
 
 		Vet vet = new Vet();
@@ -105,7 +92,7 @@ class VetResourceTest {
 
 
 	@Test
-	@DisplayName("Vet input submission cleanup and validation Test")
+	@DisplayName("Vet input submission cleanup and validation Resource Test")
 	void VetRegisterDataFilterAndValidationTest() throws Exception{
 
 		Vet vet = new Vet();
@@ -135,12 +122,33 @@ class VetResourceTest {
 	}
 
 	@Test
+	@DisplayName("Add a New Vet Resource Test")
 	void addANewVet() throws Exception {
 		//arrange
 		Vet vet = new Vet();
 		vet.setId(1);
-		//act //assert
+		vet.setVetId(874130);
+		vet.setFirstName("James");
+		vet.setLastName("Carter");
+		vet.setEmail("carter.james@email.com");
+		vet.setPhoneNumber("2384");
+		vet.setResume("Practicing since 3 years");
+		vet.setWorkday("Monday, Tuesday, Friday");
+		vet.setIsActive(1);
+
 		given(vetRepository.findAll()).willReturn(asList(vet));
-		assertEquals(vet.getId(), 1);
+
+		mvc.perform(get("/vets").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].id").value(1))
+				.andExpect(jsonPath("$[0].vetId").value(874130))
+				.andExpect(jsonPath("$[0].firstName").value("James"))
+				.andExpect(jsonPath("$[0].lastName").value("Carter"))
+				.andExpect(jsonPath("$[0].email").value("carter.james@email.com"))
+				.andExpect(jsonPath("$[0].phoneNumber").value("(514)-634-8276 #2384"))
+				.andExpect(jsonPath("$[0].resume").value("Practicing since 3 years"))
+				.andExpect(jsonPath("$[0].workday").value("Monday, Tuesday, Friday"))
+				.andExpect(jsonPath("$[0].isActive").value(1));
+
 	}
 }
