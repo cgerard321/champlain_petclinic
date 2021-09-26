c47f4fbc3d352c174374e9bd7819e780e55df610
Chloedunwoody <chloe.dunwoody@hotmail.com>
2021-09-26 14:08:05 -0400

    test disable vet

--- vets-service/src/main/java/com/petclinic/vets/presentationlayer/VetResource.java
+++ vets-service/src/main/java/com/petclinic/vets/presentationlayer/VetResource.java
@@ -2,11 +2,11 @@ package com.petclinic.vets.presentationlayer;
 
 import com.petclinic.vets.datalayer.Vet;
 import com.petclinic.vets.datalayer.VetRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.var;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,7 +21,6 @@ import java.util.Optional;
 
 @RequestMapping("/vets")
 @RestController
-@RequiredArgsConstructor
 class VetResource {
 
     private final VetRepository vetRepository;
@@ -32,18 +31,13 @@ class VetResource {
     public List<Vet> showResourcesVetList() {
         return vetRepository.findAll();
     }
-    @PutMapping(path = "/{vetId}",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public Optional<Vet> disableVet(@PathVariable("vetId") int vetId) {
-        var vet = findVet(vetId);
-        vet.get().setIsActive(1);
-        return vetRepository.findById(vetId);
-    }
+
 
     @GetMapping(value = "/{vetId}")
     public Optional<Vet> findVet(@PathVariable("vetId") int vetId) {
         return vetRepository.findById(vetId);
     }
+
+
 
 }
