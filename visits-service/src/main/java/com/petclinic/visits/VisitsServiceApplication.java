0942fe865de11f9c9d522de1ac0eb001bc36799d
Kenny-star <54686876+Kenny-star@users.noreply.github.com>
2021-09-25 22:15:42 -0400

    Git is complicated, no cap, no cap

--- visits-service/src/main/java/com/petclinic/visits/VisitsServiceApplication.java
+++ visits-service/src/main/java/com/petclinic/visits/VisitsServiceApplication.java
@@ -3,10 +3,7 @@ package com.petclinic.visits;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-<<<<<<< HEAD
 import org.springframework.stereotype.Component;
-=======
->>>>>>> c845a03... Added routings for Add/Delete/Update visit in the API and created Junit tests.
 
 @SpringBootApplication
 @ComponentScan("com.petclinic")
