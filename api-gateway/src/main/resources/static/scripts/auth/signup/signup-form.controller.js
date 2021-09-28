3ac73b20424dfd1f1c062d2c07c9481fa27cb725
Jordan Albayrak <70965032+JordanAlbayrak@users.noreply.github.com>
2021-09-27 21:05:16 -0400

    Bug(AUTH-CPC-205): signup password hidden (#85)
    * Added auth-service to project
    
    * Removed useless files
    
    * Added JWT dep
    
    * Added User model
    
    * Added auth dockerfile
    
    * Added UserRepo tests
    
    * Added flyway core dep
    
    * Added flyway and datasource properties
    
    * Added flyway init script
    
    * Added mysql for auth to docker-compose
    
    * Changed default flyway pass
    
    * Changed datasource port
    
    * Removed deprecated connector property
    
    * Changed flyway properties
    
    * Changed flyway password in properties
    
    * Added id as primary key
    
    * Refactored persistence tests to their own file
    
    * Switched to yml config
    
    * Finished h2 setup and initial test
    
    * Added cleanUp code
    
    * Refactored shared default user save
    
    * Added remove test
    
    * Switched to alpine
    
    * Fixed docker-compose a bit. Added docker profile to config
    
    * Removed docker-compose versioning to support deprecated directives like mem_limit and depends_on
    
    * Added auth-service dependency
    
    * Added email uniqueness test
    
    * Added builder for User. refactored addDefaultUser to make a deep copy of default object
    
    * Changed addDefaultUser to randomize id
    
    * Revert "Changed addDefaultUser to randomize id"
    
    This reverts commit 67d159d8eae14855419116bc28009f23f46d3565.
    
    * Changed addDefaultUser to randomize id
    
    * Refactored add two users with same email check to use addDefaultUser
    
    * Turned off hibernate auto ddl
    
    * Removed unique annotation for User.email
    
    * Changed back to SpringBootTest. Specified schema and table to User entity
    
    * Added test profile to application tests
    
    * Added packages for login and signup services. Also, I added two js files in the api-gateway for the front end of login and signup. No actual code has been added. I also went through what Nariman pushed to try the tests.
    
    * Add empty tests
    
    * Added bcrypt password encoder file, as well as possible tests without bodies.
    
    * Added a login and signup directory inside of ressources/static/auth. Filled the signup directory with files for the signup form. Created the signup UI and added code for its functionality but the code is incomplete.
    
    * Added UI for a signup form, Added User classes for the fonctionality of the signup user interface.
    
    * Fixed issues with the Signup UI not showing on the nav, and the password not accepting any input.
    
    * Implemented an add_user test
    
    * Added more incomplete applciation tests.
    
    * Made username, password, email not nullable so that they cannot be empty inside of the form. Also added persistence tests and application tests.
    
    * Fixed some tests
    
    * Added more applciation tests
    
    * Changed toolVersion for jacoco
    
    * Added all necessary tests using JaCoCo <3
    
    * 100% in the JaCoCo Tests lets gooo, fixedmost issues.
    
    * Fixed issue with UserController using the UserRepo instead of the UserService, modified a realted test aswell.
    
    * Fixed small issues, such as changing the title of the roles page from the relatable Agony to Roles
    
    * Made the UserService take in a UserDTO instead of a User
    
    * Fixed the password in the signup page to make it hidden
    
    * Added some headers
    
    Co-authored-by: Fube <fubeitch@gmail.com>
    Co-authored-by: Jon Ma <jonathan.marcotte.7@gmail.com>

--- api-gateway/src/main/resources/static/scripts/auth/signup/signup-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/auth/signup/signup-form.controller.js
@@ -1,3 +1,7 @@
+<!--        * Created by IntelliJ IDEA.-->
+<!--        * User: @JordanAlbayrak-->
+<!--        * Date: 22/09/21-->
+<!--        * Ticket: feat(AUTH-CPC-102)_signup_user-->
 'use strict';
 
 angular.module('signupForm')
