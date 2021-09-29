4ae9fa67dff4d508280efd2a8bad63efcabdc998
Fube <fubeitch@gmail.com>
2021-09-28 20:19:43 -0400

    Removed useless test

--- mailer-service/mailer/controller/mailer.controller_test.go
+++ mailer-service/mailer/controller/mailer.controller_test.go
@@ -1,13 +1,11 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"strings"
 	"testing"
 )
 
@@ -33,22 +31,9 @@ func testHTTPResponse(t *testing.T, r *gin.Engine, req *http.Request, f func(w *
 	}
 }
 
-func TestAnyResponseFromMailerEndpoint(t *testing.T) {
+func TestMailerControllerImpl_Routes(t *testing.T) {
 
 	router := gin.Default()
 	mC := MailerControllerImpl{}
-	mC.Routes(router)
-
-	req, err := http.NewRequest(http.MethodPost, "/mail", strings.NewReader(""))
-
-	if err != nil {
-		fmt.Println(err)
-		t.Fatal(err)
-	}
-
-	testHTTPResponse(t, router, req, func(w *httptest.ResponseRecorder) bool {
-
-		assert.Equal(t, http.StatusOK, w.Code)
-		return true
-	})
+	assert.Nil(t, mC.Routes(router))
 }
