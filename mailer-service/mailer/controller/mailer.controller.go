0808b3acbad54c1915f50733b80e397018e03905
Fube <fubeitch@gmail.com>
2021-09-28 22:09:12 -0400

    Got nil mail test for handle mail POST passing

--- mailer-service/mailer/controller/mailer.controller.go
+++ mailer-service/mailer/controller/mailer.controller.go
@@ -16,7 +16,7 @@ func (m MailerControllerImpl) New() {
 func handleMailPOST(context *gin.Context) {
 
 	get, exists := context.Get("mail")
-	if !exists {
+	if !exists || get == nil {
 		fmt.Println("E-mail not in context")
 		context.JSON(http.StatusBadRequest, "Unable to parse e-mail from body")
 		return
