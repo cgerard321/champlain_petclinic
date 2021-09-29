e42427fc99359988d1673c94170582bd876a2e85
Fube <fubeitch@gmail.com>
2021-09-28 21:03:05 -0400

    Added unmarshall empty subject test

--- mailer-service/mailer/controller/middleware_test.go
+++ mailer-service/mailer/controller/middleware_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 )
 
-func TestUnMarshallMail(t *testing.T) {
+func TestUnMarshallMailValid(t *testing.T) {
 
 	recorder := httptest.NewRecorder()
 	context, _ := gin.CreateTestContext(recorder)
@@ -20,6 +20,31 @@ func TestUnMarshallMail(t *testing.T) {
 	const subject = "subject"
 	const message = "message"
 	marshal, _ := json.Marshal(mailer.Mail{To: email, Subject: subject, Message: message})
+	serial := string(marshal)
+
+	context.Request, _ = http.NewRequest("test-method", "test-url", strings.NewReader(serial))
+	UnMarshallMail(context)
+
+	get, exists := context.Get("mail")
+
+	assert.True(t, exists)
+
+	mail := get.(*mailer.Mail)
+
+	assert.Equal(t, email, mail.To)
+	assert.Equal(t, subject, mail.Subject)
+	assert.Equal(t, message, mail.Message)
+}
+
+func TestUnMarshallMailValidEmptySubject(t *testing.T) {
+
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+
+	const email = "test@test.test"
+	const subject = ""
+	const message = "message"
+	marshal, _ := json.Marshal(mailer.Mail{To: email, Subject: "", Message: message})
 	serial := string(marshal)
 
 	context.Request, _ = http.NewRequest("test-method", "test-url", strings.NewReader(serial))
