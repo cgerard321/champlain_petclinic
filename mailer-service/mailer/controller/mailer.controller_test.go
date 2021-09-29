1bc484259f90d5438ac5db4de86b085a97c51205
Fube <fubeitch@gmail.com>
2021-09-28 20:04:33 -0400

    Added HTTP test helper func

--- mailer-service/mailer/controller/mailer.controller_test.go
+++ mailer-service/mailer/controller/mailer.controller_test.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -13,4 +15,17 @@ func TestMain(m *testing.M) {
 
 	// Run the other tests
 	os.Exit(m.Run())
+}
+
+func testHTTPResponse(t *testing.T, r *gin.Engine, req *http.Request, f func(w *httptest.ResponseRecorder) bool) {
+
+	// Create a response recorder
+	w := httptest.NewRecorder()
+
+	// Create the service and process the above request.
+	r.ServeHTTP(w, req)
+
+	if !f(w) {
+		t.Fail()
+	}
 }
