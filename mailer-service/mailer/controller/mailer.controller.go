c1a71d1e38bffa7cf1b3e9b3d9a1c9341cf9b13e
Fube <fubeitch@gmail.com>
2021-09-28 22:05:05 -0400

    Refactoored anonnymous /mail POST handler to handleMailPOST for testability

--- mailer-service/mailer/controller/mailer.controller.go
+++ mailer-service/mailer/controller/mailer.controller.go
@@ -13,24 +13,25 @@ type MailerControllerImpl struct {
 
 func (m MailerControllerImpl) New() {
 }
+func handleMailPOST(context *gin.Context) {
+
+	get, exists := context.Get("mail")
+	if !exists {
+		fmt.Println("E-mail not in context")
+		context.JSON(http.StatusBadRequest, "Unable to parse e-mail from body")
+		return
+	}
+
+	mail := get.(*mailer.Mail)
+
+	context.IndentedJSON(http.StatusOK, fmt.Sprintf("Message sent to %s", mail.To))
+}
 
 func (m MailerControllerImpl) Routes(engine *gin.Engine) error {
 
 	group := engine.Group("/mail").Use(UnMarshallMail, ValidateEmail)
 
-	group.POST("", func(context *gin.Context) {
-
-		get, exists := context.Get("mail")
-		if !exists {
-			fmt.Println("E-mail not in context")
-			context.JSON(http.StatusBadRequest, "Unable to parse e-mail from body")
-			return
-		}
-
-		mail := get.(*mailer.Mail)
-
-		context.IndentedJSON(http.StatusOK, fmt.Sprintf("Message sent to %s", mail.To))
-	})
+	group.POST("", handleMailPOST)
 
 	return nil
 }
