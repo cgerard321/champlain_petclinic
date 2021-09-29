9fb5f0f7951c8b1025f4a92c409e3db05f6ee4d5
Anh-Toine <Nick1234567890a@gmail.com>
2021-09-29 16:37:59 -0400

    Changed the SQL scripts

--- customers-service/src/main/resources/schema-h2.sql
+++ customers-service/src/main/resources/schema-h2.sql
@@ -15,7 +15,6 @@ CREATE TABLE owners (
                         address    VARCHAR(255),
                         city       VARCHAR(80),
                         telephone  VARCHAR(20),
-                        custodian VARCHAR(20)
 );
 CREATE INDEX owners_last_name ON owners (last_name);
 
