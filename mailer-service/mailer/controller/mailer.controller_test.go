9936bd52691b3c7c2b4b5adb0294e2cbd760e838
Fube <fubeitch@gmail.com>
2021-09-28 20:45:59 -0400

    Added failing test for unmarshall mail

--- mailer-service/mailer/controller/mailer.controller_test.go
+++ mailer-service/mailer/controller/mailer.controller_test.go
@@ -1,11 +1,14 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -36,4 +39,31 @@ func TestMailerControllerImpl_Routes(t *testing.T) {
 	router := gin.Default()
 	mC := MailerControllerImpl{}
 	assert.Nil(t, mC.Routes(router))
+}
+
+func TestMailerControllerImpl_Unmarshalls(t *testing.T) {
+
+	router := gin.Default()
+	mC := MailerControllerImpl{}
+	assert.Nil(t, mC.Routes(router))
+
+	const email = "example@test.com"
+
+	bodyS := fmt.Sprintf("\"to\": \"%s\", \"message\": \"%s\", \"subject\": \"%s\"", email, "message", "subject")
+
+	req, err := http.NewRequest(http.MethodPost, "/mail", strings.NewReader(bodyS))
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fatal(err)
+	}
+
+	testHTTPResponse(t, router, req, func(w *httptest.ResponseRecorder) bool {
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		body, err := ioutil.ReadAll(w.Result().Body)
+		assert.Nil(t, err)
+		assert.Contains(t, body, "Message sent to " + email)
+		return true
+	})
 }
