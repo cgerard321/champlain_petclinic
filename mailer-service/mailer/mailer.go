f7f2dbbb77cc490ae5d08e8721b392046b9eaf4a
Fube <fubeitch@gmail.com>
2021-09-28 20:46:29 -0400

    Added Mail struct

--- mailer-service/mailer/mailer.go
+++ mailer-service/mailer/mailer.go
@@ -6,3 +6,9 @@ type MailerController interface {
 	New()
 	Routes(engine *gin.Engine) error
 }
+
+type Mail struct {
+	To string `json:"to" validate:"required,email"`
+	Message string `json:"message" validate:"required"`
+	Subject string `json:"subject"`
+}
