63fc445398a6647a5bdaa0374a304878fdfde98a
Fube <fubeitch@gmail.com>
2021-09-29 11:00:29 -0400

    Added failing SendMail validmail test

--- mailer-service/mailer/service/mailer.service_test.go
+++ mailer-service/mailer/service/mailer.service_test.go
@@ -6,6 +6,9 @@ import (
 	"testing"
 )
 
+var ValidMail = &mailer.Mail{To: "example@test.com", Subject: "Test subject", Message: "Test message"}
+
+
 func TestMain(m *testing.M) {
 
 	os.Exit(m.Run())
@@ -15,4 +18,12 @@ func TestMailerService_New(t *testing.T) {
 	mS := MailerServiceImpl{}
 	dialer := mailer.CreateDialer("smtp.invalid.com", "a@b.c", "pass")
 	mS.New(dialer)
+}
+
+func TestMailerServiceImpl_SendMailValidMail(t *testing.T) {
+	mS := MailerServiceImpl{}
+	dialer := mailer.CreateDialer("smtp.invalid.com", "a@b.c", "pass")
+	mS.New(dialer)
+
+	mS.SendMail(ValidMail)
 }
