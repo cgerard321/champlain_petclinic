f83fd859cc3b94558d009f26651f786290eebec7
CJay <jc_ben@live.ca>
2021-09-29 12:02:36 -0400

    Added utils package for Exception handling with InvalidInputException and NotFoundException

---
+++ visits-service/src/test/java/com/petclinic/visits/utils/exceptions/InvalidInputException.java
@@ -0,0 +1,14 @@
+package com.petclinic.visits.utils.exceptions;
+
+public class InvalidInputException extends RuntimeException{
+
+    public InvalidInputException(){
+
+    }
+
+    public InvalidInputException(String message){ super(message);}
+
+    public InvalidInputException(Throwable cause){ super(cause);}
+
+    public InvalidInputException(String message, Throwable cause){ super(message, cause);}
+}
---
+++ visits-service/src/test/java/com/petclinic/visits/utils/exceptions/NotFoundException.java
@@ -0,0 +1,15 @@
+package com.petclinic.visits.utils.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(){
+
+    }
+
+    public NotFoundException(String message){ super(message);}
+
+    public NotFoundException(Throwable cause){ super(cause);}
+
+    public NotFoundException(String message, Throwable cause){ super(message, cause);}
+
+}
