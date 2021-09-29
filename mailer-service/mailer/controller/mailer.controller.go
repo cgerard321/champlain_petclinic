1694e49cf66b101301e9fb1b6f275c039c736a1d
Fube <fubeitch@gmail.com>
2021-09-28 20:13:08 -0400

    Added bare minimuim for mail controller impl

--- mailer-service/mailer/controller/mailer.controller.go
+++ mailer-service/mailer/controller/mailer.controller.go
@@ -1,2 +1,22 @@
 package controller
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type MailerControllerImpl struct {
+	
+}
+
+func (m MailerControllerImpl) New() {
+}
+
+func (m MailerControllerImpl) Routes(engine *gin.Engine) error {
+
+	group := engine.Group("/mail")
+	group.POST("", func(context *gin.Context) {
+		context.IndentedJSON(http.StatusOK, "Hello there")
+	})
+	return nil
+}
