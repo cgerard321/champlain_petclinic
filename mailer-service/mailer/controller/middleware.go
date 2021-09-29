ede7c950b8e6f6bab6bca67f1d039a95a6659c59
Fube <fubeitch@gmail.com>
2021-09-28 21:01:41 -0400

    Added unmarshall mail middleware

--- mailer-service/mailer/controller/middleware.go
+++ mailer-service/mailer/controller/middleware.go
@@ -1,9 +1,22 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"mailer-service/mailer"
+	"net/http"
 )
 
 func UnMarshallMail(c *gin.Context) {
 
+	var mail mailer.Mail
+
+	if err := c.ShouldBindJSON(&mail); err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	c.Set("mail", &mail)
+	c.Next()
 }
