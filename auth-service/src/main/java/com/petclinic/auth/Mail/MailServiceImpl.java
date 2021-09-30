3c2a972c22a9efa31d07be3306b75f58124e3713
Fube <fubeitch@gmail.com>
2021-09-30 13:13:13 -0400

    Added MailServiceImpl skeleton

---
+++ auth-service/src/main/java/com/petclinic/auth/Mail/MailServiceImpl.java
@@ -0,0 +1,16 @@
+package com.petclinic.auth.Mail;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class MailServiceImpl implements MailService {
+
+
+
+    @Override
+    public String sendMail(Mail mail) {
+        return null;
+    }
+}
