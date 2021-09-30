9982ef299930d9d9970696fa17e43d1192fd4f0a
Fube <fubeitch@gmail.com>
2021-09-30 13:56:10 -0400

    Added retrofit mock call

--- auth-service/src/test/java/com/petclinic/auth/AuthMailServiceTests.java
+++ auth-service/src/test/java/com/petclinic/auth/AuthMailServiceTests.java
@@ -3,15 +3,22 @@ package com.petclinic.auth;
 
 import com.petclinic.auth.Mail.Mail;
 import com.petclinic.auth.Mail.MailService;
+import com.petclinic.auth.Mail.MailServiceCall;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.HttpClientErrorException;
+import retrofit2.mock.Calls;
 
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -20,9 +27,21 @@ public class AuthMailServiceTests {
     @Autowired
     private MailService mailService;
 
+    @MockBean
+    private MailServiceCall mockMailCall;
+
     private final Mail
             EMAIL_VALID = new Mail("to@test.com", "test-subject", "test-message"),
             EMAIL_INVALID = new Mail();
+
+    @BeforeEach
+    void setUp() {
+        when(mockMailCall.sendMail(EMAIL_VALID))
+                .thenReturn(Calls.response(format("Message sent to %s", EMAIL_VALID.getTo())));
+
+        when(mockMailCall.sendMail(EMAIL_INVALID))
+                .thenThrow(new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to send mail"));
+    }
 
     @Test
     void loads(){}
