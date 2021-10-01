20a47aaaac0818d61e46803ca12e7a4ab3e6205e
Olivier Lemire <olivierlemire@urbanimmersive.com>
2021-09-30 21:06:25 -0400

    bug/BILL-CPC-244 fixed bug in docker / was caused due to a field in the schemas having a typo

--- billing-service/src/main/resources/schema-mysql.sql
+++ billing-service/src/main/resources/schema-mysql.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS billings (
     id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     visitType VARCHAR(80),
     date DATE,
-    amount DOUBLE,
-    INDEX(visit_type)
+    amount DOUBLE
     ) engine=InnoDB;
