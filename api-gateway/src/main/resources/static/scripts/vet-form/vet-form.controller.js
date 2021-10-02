f03d6130c7559258e185042ab66e1bb3f8614762
Chloe <chloe.dunwoody@hotmail.com>
2021-10-02 16:12:57 -0400

    ids added to file inputs

--- api-gateway/src/main/resources/static/scripts/vet-form/vet-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/vet-form/vet-form.controller.js
@@ -33,3 +33,45 @@ angular.module('vetForm')
             });
         };
     }]);
+
+
+angular.module('uploadModule', [])
+    .controller('uploadCtrl', [
+        '$scope',
+        '$upload',
+        function ($scope, $upload) {
+            $scope.model = {};
+            $scope.selectedFile = [];
+            $scope.uploadProgress = 0;
+
+            $scope.uploadFile = function () {
+                var file = $scope.selectedFile[0];
+                $scope.upload = $upload.upload({
+                    url: 'api/gateway/vets/',
+                    method: 'POST',
+                    data: angular.toJson($scope.model),
+                    file: file
+                }).progress(function (evt) {
+                    $scope.uploadProgress = parseInt(100.0 * evt.loaded / evt.total, 10);
+                }).success(function (data) {
+                    //do something
+                });
+            };
+
+            $scope.onFileSelect = function ($files) {
+                $scope.uploadProgress = 0;
+                $scope.selectedFile = $files;
+            };
+        }
+    ])
+    .directive('progressBar', [
+        function () {
+            return {
+                link: function ($scope, el, attrs) {
+                    $scope.$watch(attrs.progressBar, function (newValue) {
+                        el.css('width', newValue.toString() + '%');
+                    });
+                }
+            };
+        }
+    ]);
