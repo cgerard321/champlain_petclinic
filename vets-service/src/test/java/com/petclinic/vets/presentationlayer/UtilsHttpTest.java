c6d551cde7c32e28641fcc3136ec5e995dd2b2bf
Christian C <christian.chitanu@hotmail.com>
2021-10-03 15:21:33 -0400

    added serviceUtilsTest

--- vets-service/src/test/java/com/petclinic/vets/presentationlayer/UtilsHttpTest.java
+++ vets-service/src/test/java/com/petclinic/vets/presentationlayer/UtilsHttpTest.java
@@ -1,4 +1,21 @@
 package com.petclinic.vets.presentationlayer;
+
+import com.petclinic.vets.utils.http.ServiceUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 /**
  * Simple Http Utils tests
  *
@@ -6,4 +23,11 @@ package com.petclinic.vets.presentationlayer;
  */
 public class UtilsHttpTest {
 
+    final ServiceUtil su = new ServiceUtil("7002");
+    @Test
+    @DisplayName("Service Utils Test")
+    void serviceUtilsTest(){
+
+        assertNotNull(su.getServiceAddress());
+    }
 }
