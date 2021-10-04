7b6714cc95edcdeea1c42c78911df3c8c5dcfa71
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-10-04 19:57:55 -0400

    Addressed the comments to the PR, removed commented out code, renamed the PR more suitably and removed the comments in code

--- api-gateway/src/main/resources/static/scripts/bill-history/bill-history.component.js
+++ api-gateway/src/main/resources/static/scripts/bill-history/bill-history.component.js
@@ -1,8 +1,7 @@
 'use strict';
 
-// Configure a module for the bill history page
-angular.module('billHistory') // Give the module a name
-    .component('billHistory', { // Use the component method to add various scripts and links to a specified module
-        templateUrl: 'scripts/bill/bill-history.template.html', // Add in a page template to append to (bill-history.template.html)
-        controller: 'BillHistoryController' // Add a controller to the module which will tie a HTTP protocol to the page for specific methods
+angular.module('billHistory')
+    .component('billHistory', {
+        templateUrl: 'scripts/bill/bill-history.template.html',
+        controller: 'BillHistoryController'
     });
--- api-gateway/src/main/resources/static/scripts/bill-history/bill-history.js
+++ api-gateway/src/main/resources/static/scripts/bill-history/bill-history.js
@@ -1,11 +1,8 @@
 'use strict';
 
-// Configure an existing module from the component script
 angular.module('billHistory', ['ui.router'])
-    // Call the config method of the module to alter an argument called state provider as a method
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider
-            // Access the state property to use a template called bills
             .state('bills', {
                 parent: 'app',
                 url: '/bills',
