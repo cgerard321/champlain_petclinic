1d68615c0109f1dd649729f7bfcce220d46b9734
Louis-Philippe Simard <l.psimard901@gmail.com>
2021-10-06 18:08:21 -0400

    Update PetServiceImpl.java

--- customers-service/src/main/java/com/petclinic/customers/businesslayer/PetServiceImpl.java
+++ customers-service/src/main/java/com/petclinic/customers/businesslayer/PetServiceImpl.java
@@ -6,7 +6,6 @@ import com.petclinic.customers.presentationlayer.PetRequest;
 import com.petclinic.customers.presentationlayer.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
