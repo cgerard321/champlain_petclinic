d7ca520261705493745e8ea2105c3b8aff5dfa3d
Nicolas <nespitalier012@gmail.com>
2021-10-07 19:42:23 -0400

    Change requested done

--- api-gateway/src/main/resources/static/scripts/visits/visits.controller.js
+++ api-gateway/src/main/resources/static/scripts/visits/visits.controller.js
@@ -123,7 +123,6 @@ angular.module('visits')
 
         self.deleteVisit = function (visitId){
             $http.delete("api/gateway/visits/" + visitId).then(function () {
-                self.visits.splice(visitId, 1);
                 window.location.reload();
             }, function (response) {
                 var error = response.data;
