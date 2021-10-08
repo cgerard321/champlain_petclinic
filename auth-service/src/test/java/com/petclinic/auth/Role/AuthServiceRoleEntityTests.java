1360a54c9b6427b9be00e210b932bfb750ac4fb9
Trilikin21 <53579676+Trilikin21@users.noreply.github.com>
2021-10-08 14:29:22 -0400

    feat(AUTH-CPC-213) add get and delete to user entity (#138)
    * Added TDD testing to get, getAll and delete services.Seperated testing.
    
    * Added get and delete services.
    
    * mistakingly deleted AuthMail, readd it.
    
    * incorporated changes as the reviewer requested

---
+++ auth-service/src/test/java/com/petclinic/auth/Role/AuthServiceRoleEntityTests.java
@@ -0,0 +1,62 @@
+package com.petclinic.auth.Role;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class AuthServiceRoleEntityTests {
+
+    final String
+            ROLE_NAME = "role";
+
+    final Role role = new Role(0, ROLE_NAME);
+
+    final Set<Role> ROLES = new HashSet<Role>() {{
+        add(role);
+    }};
+    final long ID = 1L;
+
+    @Test
+    @DisplayName("Role setters")
+    void role_setters() {
+
+        final Role role = new Role();
+        role.setId(ID);
+        role.setName(ROLE_NAME);
+        role.setParent(null);
+
+        assertEquals(ID, role.getId());
+        assertEquals(ROLE_NAME, role.getName());
+        assertNull(role.getParent());
+
+        final Role PARENT = new Role();
+
+        role.setParent(PARENT);
+        assertEquals(PARENT, role.getParent());
+    }
+
+    @Test
+    @DisplayName("Role builder")
+    void role_builder() {
+
+        final Role role = Role.builder()
+                .id(ID)
+                .name(ROLE_NAME)
+                .parent(null)
+                .build();
+
+        assertEquals(ID, role.getId());
+        assertEquals(ROLE_NAME, role.getName());
+        assertNull(role.getParent());
+        assertEquals(
+                format("Role.RoleBuilder(id=%d, name=%s, parent=%s)", role.getId(), role.getName(), role.getParent()),
+                role.toBuilder().toString()
+        );
+    }
+}
