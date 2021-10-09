95c08ee45f97c19665daabe6149c4d5882865b0d
Fube <fubeitch@gmail.com>
2021-10-09 17:14:26 -0400

    Renamed PasswordEncoder -> PasswordEncoderConfig to avoid confusion with built-in Spring Security class

--- auth-service/src/main/java/com/petclinic/auth/Config/PasswordEncoderConfig.java
+++ auth-service/src/main/java/com/petclinic/auth/Config/PasswordEncoderConfig.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class PasswordEncoder {
+public class PasswordEncoderConfig {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
