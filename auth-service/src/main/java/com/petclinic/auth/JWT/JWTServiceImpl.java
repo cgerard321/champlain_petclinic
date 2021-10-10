e0866bf814ed4bd22629d00c5879b14904aa2aea
Fube <fubeitch@gmail.com>
2021-10-10 15:39:51 -0400

    Added JWTservice skeleton

---
+++ auth-service/src/main/java/com/petclinic/auth/JWT/JWTServiceImpl.java
@@ -0,0 +1,21 @@
+package com.petclinic.auth.JWT;
+
+import com.petclinic.auth.User.User;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: @Fube
+ * Date: 2021-10-10
+ * Ticket: feat(AUTH-CPC-357)
+ */
+public class JWTServiceImpl implements JWTService {
+    @Override
+    public String encrypt(User user) {
+        return null;
+    }
+
+    @Override
+    public User decrypt(String token) {
+        return null;
+    }
+}
