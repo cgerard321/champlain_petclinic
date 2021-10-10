e3e636d99b77712640f28e74270a3be627451acd
Fube <fubeitch@gmail.com>
2021-10-10 15:44:25 -0400

    Added JWT test skeleton

---
+++ auth-service/src/test/java/com/petclinic/auth/JWT/JWTServiceTests.java
@@ -0,0 +1,16 @@
+package com.petclinic.auth.JWT;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: @Fube
+ * Date: 2021-10-10
+ * Ticket: feat(AUTH-CPC-357)
+ */
+
+public class JWTServiceTests {
+
+    @Test
+    void setup(){}
+}
