e9de2378c7e7edd40a983d856a7990b520fa3df1
Fube <fubeitch@gmail.com>
2021-10-10 15:55:13 -0400

    Added JWT encrypt impl

--- auth-service/src/main/java/com/petclinic/auth/JWT/JWTServiceImpl.java
+++ auth-service/src/main/java/com/petclinic/auth/JWT/JWTServiceImpl.java
@@ -1,6 +1,14 @@
 package com.petclinic.auth.JWT;
 
 import com.petclinic.auth.User.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -8,10 +16,24 @@ import com.petclinic.auth.User.User;
  * Date: 2021-10-10
  * Ticket: feat(AUTH-CPC-357)
  */
+
+@Service
 public class JWTServiceImpl implements JWTService {
+
+    private final int expiration;
+
+    public JWTServiceImpl(@Value("{jwt.expiration}") int expiration) {
+        this.expiration = expiration;
+    }
+
     @Override
     public String encrypt(User user) {
-        return null;
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(Keys.secretKeyFor(SignatureAlgorithm.HS256))
+                .compact();
     }
 
     @Override
