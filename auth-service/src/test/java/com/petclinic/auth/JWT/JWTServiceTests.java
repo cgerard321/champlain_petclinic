3b3c696223f0dbb02176717ee3812aa72d738c0a
Fube <fubeitch@gmail.com>
2021-10-10 15:48:15 -0400

    Added JWT encrypt test

--- auth-service/src/test/java/com/petclinic/auth/JWT/JWTServiceTests.java
+++ auth-service/src/test/java/com/petclinic/auth/JWT/JWTServiceTests.java
@@ -1,6 +1,16 @@
 package com.petclinic.auth.JWT;
 
+import com.petclinic.auth.Role.Role;
+import com.petclinic.auth.User.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestComponent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,8 +19,31 @@ import org.junit.jupiter.api.Test;
  * Ticket: feat(AUTH-CPC-357)
  */
 
+@TestComponent
 public class JWTServiceTests {
+
+    @Autowired
+    private JWTService jwtService;
+
+    private final User USER = new User(
+            1,
+            "uname",
+            "pwd",
+            "a@b.c",
+            false,
+            null);
+
+    private final Set<Role> ROLES = new HashSet<Role>(){{
+        add(new Role(1, "TEST"));
+    }};
 
     @Test
     void setup(){}
+
+    @Test
+    @DisplayName("Given user, get JWT")
+    void get_jwt_from_user() {
+
+        assertNotNull(jwtService.encrypt(USER));
+    }
 }
