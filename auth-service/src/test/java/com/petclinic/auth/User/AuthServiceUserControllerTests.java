dbee0985fdcdb413802cd1e0fd7602b7a78da1e3
Fube <fubeitch@gmail.com>
2021-10-10 20:05:27 -0400

    Use mock bean for mailService in user controller tests

--- auth-service/src/test/java/com/petclinic/auth/User/AuthServiceUserControllerTests.java
+++ auth-service/src/test/java/com/petclinic/auth/User/AuthServiceUserControllerTests.java
@@ -25,12 +25,14 @@
 package com.petclinic.auth.User;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.petclinic.auth.Mail.MailService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -88,6 +90,9 @@ public class AuthServiceUserControllerTests {
 
     @Autowired
     private UserController userController;
+
+    @MockBean
+    private MailService mailService;
 
     private final UserIDLessDTO ID_LESS_USER = new UserIDLessDTO(USER, PASS, EMAIL);
 
