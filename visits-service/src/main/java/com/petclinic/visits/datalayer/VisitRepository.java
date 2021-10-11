80cb0e1ed2f1620e5b6ebeab8503e332f9238aee
CJay <jc_ben@live.ca>
2021-10-11 11:37:08 -0400

    Added methods in each layer for getting list of string dates for a practitioner

--- visits-service/src/main/java/com/petclinic/visits/datalayer/VisitRepository.java
+++ visits-service/src/main/java/com/petclinic/visits/datalayer/VisitRepository.java
@@ -29,6 +29,8 @@ public interface VisitRepository extends JpaRepository<Visit, Integer> {
 
     List<Visit> findByPetIdIn(Collection<Integer> petIds);
 
+    List<Visit> findVisitsByPractitionerId(int practitionerId);
+
     void findVisitById(int visitId);
 
 }
