1b559d5161ecc995ef03e322a92dc4e42be79e3d
Amine Mazigh <54552812+amine-12@users.noreply.github.com>
2021-10-12 10:42:25 -0400

    feat(VETS CPC-230) availability and  visit link integration (#173)
    * Gets visits dates from api to create a list of days to remove from the availability. (big commit)
    
    * Finished implementation and fixed bugs.
    
    * Removed console.logs
    
    Co-authored-by: Shariful Islam <71034196+Louenes099@users.noreply.github.com>

--- api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
@@ -3,13 +3,17 @@
 angular.module('vetDetails')
     .controller('VetDetailsController', ['$http','$stateParams', function ($http, $stateParams) {
         var self = this;
-        let visitsURL = "api/gateway/visits/vets/";
+        let visits;
+
         /* added /{{vet.vetID}} in the url */
         $http.get('api/gateway/vets/'+$stateParams.vetId).then(function (resp) {
+            console.log($stateParams.vetId)
             self.vet = resp.data;
         });
 
-        $http.get(visitsURL + "/" + +$stateParams.vetId).then(function (resp) {
-            self.visits = resp.data;
+        $http.get("api/gateway/visits/vets/" +$stateParams.vetId).then(function (resp) {
+            self.visitsList = resp.data;
+            console.log(self.visitsList)
         });
+
     }]);
