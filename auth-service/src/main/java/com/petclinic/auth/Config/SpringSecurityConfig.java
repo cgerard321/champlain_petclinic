31884cd7ffcc71ad7f604492eea9dd06378e8a2f
Fube <fubeitch@gmail.com>
2021-10-14 16:48:59 -0400

    Refactored bcrypt bean into SpringSecurityConfig

--- auth-service/src/main/java/com/petclinic/auth/Config/SpringSecurityConfig.java
+++ auth-service/src/main/java/com/petclinic/auth/Config/SpringSecurityConfig.java
@@ -16,13 +16,20 @@
 
 package com.petclinic.auth.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     private static final String[] AUTH_WHITELIST = {
             // -- Swagger UI v2
