d8741fdd61b8911c4cfe5ca33db877dfadfc3a47
Fube <fubeitch@gmail.com>
2021-10-14 19:15:54 -0400

    Added register_user skeleton

--- auth-service/src/test/java/com/petclinic/auth/e2e/User/AuthServiceE2ETests.java
+++ auth-service/src/test/java/com/petclinic/auth/e2e/User/AuthServiceE2ETests.java
@@ -9,11 +9,16 @@
 package com.petclinic.auth.e2e.User;
 
 import com.petclinic.auth.Mail.MailService;
+import com.petclinic.auth.User.UserRepo;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -26,9 +31,22 @@ public class AuthServiceE2ETests {
     @MockBean
     private MailService mailService;
 
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @BeforeEach
     void setup() {
+        userRepo.deleteAllInBatch();
         when(mailService.sendMail(any()))
                 .thenReturn("Your verification link: someFakeLink");
+    }
+
+    @Test
+    @DisplayName("When valid user info, register")
+    void register_user() {
+
     }
 }
