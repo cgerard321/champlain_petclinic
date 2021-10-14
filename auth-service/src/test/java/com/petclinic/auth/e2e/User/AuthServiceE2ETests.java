7f7a0eab718ebb77824a9a4b4637f16817b6f816
Fube <fubeitch@gmail.com>
2021-10-14 19:32:57 -0400

    Added E2E register test

--- auth-service/src/test/java/com/petclinic/auth/e2e/User/AuthServiceE2ETests.java
+++ auth-service/src/test/java/com/petclinic/auth/e2e/User/AuthServiceE2ETests.java
@@ -8,7 +8,11 @@
 
 package com.petclinic.auth.e2e.User;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.petclinic.auth.Mail.MailService;
+import com.petclinic.auth.User.User;
+import com.petclinic.auth.User.UserIDLessDTO;
+import com.petclinic.auth.User.UserMapper;
 import com.petclinic.auth.User.UserRepo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,11 +21,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,8 +45,20 @@ public class AuthServiceE2ETests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final User USER = User.builder()
+            .email("e@mail.com")
+            .password("MyV3ryC00lP@$$w0rd")
+            .username("my cool username")
+            .build();
+
+    private UserIDLessDTO ID_LESS_USER;
+
     @BeforeEach
     void setup() {
+        ID_LESS_USER = objectMapper.convertValue(USER, UserIDLessDTO.class);
         userRepo.deleteAllInBatch();
         when(mailService.sendMail(any()))
                 .thenReturn("Your verification link: someFakeLink");
@@ -46,7 +66,17 @@ public class AuthServiceE2ETests {
 
     @Test
     @DisplayName("When valid user info, register")
-    void register_user() {
+    void register_user() throws Exception {
 
+        final String asString = objectMapper.writeValueAsString(ID_LESS_USER);
+
+        mockMvc.perform(post("/users").contentType(APPLICATION_JSON).content(asString))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.password").doesNotExist())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.email").value(USER.getEmail()))
+                .andExpect(jsonPath("$.username").value(USER.getUsername()))
+                .andExpect(jsonPath("$.roles.length()").value(0));
     }
 }
