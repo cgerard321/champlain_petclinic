f28ece8719830746d151b434ca9bcc57c2c439b4
Fube <fubeitch@gmail.com>
2021-10-14 19:13:52 -0400

    Added skeleton for E2E test

---
+++ auth-service/src/test/java/com/petclinic/auth/e2e/User/AuthServiceE2ETests.java
@@ -0,0 +1,35 @@
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * User: @Fube
+ * Date: 2021-10-14
+ * Ticket: feat(AUTH-CPC-388)
+ */
+
+package com.petclinic.auth.e2e.User;
+
+import com.petclinic.auth.Mail.MailService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class AuthServiceE2ETests {
+
+    @MockBean
+    private MailService mailService;
+
+    @BeforeEach
+    void setup() {
+        when(mailService.sendMail(any()))
+                .thenReturn("Your verification link: someFakeLink");
+    }
+}
