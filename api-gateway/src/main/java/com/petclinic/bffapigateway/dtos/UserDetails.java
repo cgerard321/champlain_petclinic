ff3550d2e75f6fa8d48b0ecad64571773ae4d1c8
Fube <fubeitch@gmail.com>
2021-10-15 18:18:42 -0400

    And all the shit that goes with the Builder

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/UserDetails.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/UserDetails.java
@@ -1,11 +1,15 @@
 package com.petclinic.bffapigateway.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder(toBuilder = true)
 public class UserDetails {
 
