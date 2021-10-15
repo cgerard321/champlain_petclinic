731f738c9a8d1cccd60e637cd15451e02db90219
Fube <fubeitch@gmail.com>
2021-10-14 21:44:09 -0400

    Added E2E verification test

--- auth-service/src/test/java/com/petclinic/auth/e2e/User/AuthServiceE2ETests.java
+++ auth-service/src/test/java/com/petclinic/auth/e2e/User/AuthServiceE2ETests.java
@@ -9,10 +9,12 @@
 package com.petclinic.auth.e2e.User;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.petclinic.auth.Mail.Mail;
 import com.petclinic.auth.Mail.MailService;
 import com.petclinic.auth.User.User;
 import com.petclinic.auth.User.UserIDLessRoleLessDTO;
 import com.petclinic.auth.User.UserRepo;
+import com.petclinic.auth.User.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,9 +25,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.MediaType.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -36,6 +43,9 @@ public class AuthServiceE2ETests {
 
     @MockBean
     private MailService mailService;
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private UserRepo userRepo;
@@ -74,6 +84,45 @@ public class AuthServiceE2ETests {
                 .andExpect(jsonPath("$.password").doesNotExist())
                 .andExpect(jsonPath("$.verified").doesNotExist())
                 .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.email").value(USER.getEmail()))
+                .andExpect(jsonPath("$.username").value(USER.getUsername()))
+                .andExpect(jsonPath("$.roles.length()").value(0));
+    }
+
+    @Test
+    @DisplayName("When given verification URL, verify email")
+    void login_user() throws Exception {
+
+        final String asString = objectMapper.writeValueAsString(ID_LESS_USER);
+        AtomicReference<String> verificationJWT = new AtomicReference<>();
+        final UserService spy = spy(userService);
+
+
+        when(spy.generateVerificationMail(
+                argThat( n -> n.getEmail().equals(ID_LESS_USER.getEmail() ))
+        )).thenAnswer(n -> {
+            final Mail mail = (Mail) n.callRealMethod();
+            verificationJWT.set(mail.getMessage().split("/verification/")[1]);
+            return mail;
+        });
+
+
+        mockMvc.perform(post("/users").contentType(APPLICATION_JSON).content(asString))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.password").doesNotExist())
+                .andExpect(jsonPath("$.verified").doesNotExist())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.email").value(USER.getEmail()))
+                .andExpect(jsonPath("$.username").value(USER.getUsername()))
+                .andExpect(jsonPath("$.roles.length()").value(0));
+
+
+        mockMvc.perform(get("/users/verification" + verificationJWT))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.password").doesNotExist())
+                .andExpect(jsonPath("$.verified").doesNotExist())
                 .andExpect(jsonPath("$.email").value(USER.getEmail()))
                 .andExpect(jsonPath("$.username").value(USER.getUsername()))
                 .andExpect(jsonPath("$.roles.length()").value(0));
