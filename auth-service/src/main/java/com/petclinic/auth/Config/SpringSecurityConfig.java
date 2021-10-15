1d4a36682f97f429c85d444a5891b9d4dadae58a
Fube <fubeitch@gmail.com>
2021-10-14 22:48:41 -0400

    Removed useless imports

--- auth-service/src/main/java/com/petclinic/auth/Config/SpringSecurityConfig.java
+++ auth-service/src/main/java/com/petclinic/auth/Config/SpringSecurityConfig.java
@@ -16,12 +16,10 @@
 
 package com.petclinic.auth.Config;
 
-import com.petclinic.auth.User.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
