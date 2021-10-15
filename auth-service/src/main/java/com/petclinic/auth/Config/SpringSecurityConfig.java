324532064ef983ea2287b997d552b9f311d0ab67
Fube <fubeitch@gmail.com>
2021-10-14 23:06:46 -0400

    Expose authentication manager

--- auth-service/src/main/java/com/petclinic/auth/Config/SpringSecurityConfig.java
+++ auth-service/src/main/java/com/petclinic/auth/Config/SpringSecurityConfig.java
@@ -21,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -90,4 +91,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + "not found"))
         );
     }
+
+    @Override @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
