6fd5a0778199e0b87841b5f83f38bc6c23adfe47
Fube <fubeitch@gmail.com>
2021-10-14 22:59:09 -0400

    Added doFilterInternal impl

--- auth-service/src/main/java/com/petclinic/auth/Config/JWTFilter.java
+++ auth-service/src/main/java/com/petclinic/auth/Config/JWTFilter.java
@@ -1,7 +1,13 @@
 package com.petclinic.auth.Config;
 
 import com.petclinic.auth.JWT.JWTService;
+import com.petclinic.auth.User.User;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +15,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.util.StringUtils.isEmpty;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,6 +40,35 @@ public class JWTFilter extends OncePerRequestFilter {
             FilterChain filterChain)
     throws ServletException, IOException {
 
+        // Get authorization header and validate
+        final String header = request.getHeader(AUTHORIZATION);
+        if (isEmpty(header) || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
+        final String token = header.split(" ")[1].trim();
+
+        try {
+
+            UserDetails user = jwtService.decrypt(token);
+            UsernamePasswordAuthenticationToken
+                    authentication = new UsernamePasswordAuthenticationToken(
+                    user, null,
+                    user == null ?
+                            Collections.emptyList() : user.getAuthorities()
+            );
+
+            authentication.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request)
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+
+        } catch (JwtException e) {
+
+            filterChain.doFilter(request, response);
+        }
     }
 }
