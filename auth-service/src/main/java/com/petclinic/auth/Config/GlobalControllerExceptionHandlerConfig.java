f98ace96bd776869652ce0b1362e8a7ffb020797
Trilikin21 <53579676+Trilikin21@users.noreply.github.com>
2021-10-15 22:08:07 -0400

    fixed issue with the implementation for InvalidInputException

--- auth-service/src/main/java/com/petclinic/auth/Config/GlobalControllerExceptionHandlerConfig.java
+++ auth-service/src/main/java/com/petclinic/auth/Config/GlobalControllerExceptionHandlerConfig.java
@@ -4,18 +4,16 @@ import com.petclinic.auth.Exceptions.HTTPErrorMessage;
 import com.petclinic.auth.Exceptions.IncorrectPasswordException;
 import com.petclinic.auth.Exceptions.InvalidInputException;
 import com.petclinic.auth.Exceptions.NotFoundException;
-import org.aspectj.weaver.ast.Not;
-import org.omg.CORBA.DynAnyPackage.Invalid;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 
 import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandlerConfig {
+
 
     @ExceptionHandler(value = IncorrectPasswordException.class)
     @ResponseStatus(value = UNAUTHORIZED)
@@ -31,7 +29,9 @@ public class GlobalControllerExceptionHandlerConfig {
         return new HTTPErrorMessage(404, ex.getMessage());
     }
 
-    @ExceptionHandler(value = IncorrectPasswordException.class)
+
+
+    @ExceptionHandler(value = InvalidInputException.class)
     @ResponseStatus(value = UNPROCESSABLE_ENTITY)
     public HTTPErrorMessage resourceNotFoundException(InvalidInputException ex, WebRequest request) {
 
--- auth-service/src/main/java/com/petclinic/auth/Exceptions/InvalidInputException.java
+++ auth-service/src/main/java/com/petclinic/auth/Exceptions/InvalidInputException.java
@@ -1,13 +1,15 @@
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * User: @Trilikin21
+ * Date: 2021-10-15
+ * Ticket: feat(AUTH-CPC-391)
+ */
+
 package com.petclinic.auth.Exceptions;
 
 public class InvalidInputException extends  RuntimeException {
 
-    public InvalidInputException(){
-    }
-
     public InvalidInputException(String message){ super((message)); }
 
-    public InvalidInputException(Throwable cause) { super((cause)); }
-
-    public InvalidInputException(String message, Throwable cause){ super(message, cause); }
 }
