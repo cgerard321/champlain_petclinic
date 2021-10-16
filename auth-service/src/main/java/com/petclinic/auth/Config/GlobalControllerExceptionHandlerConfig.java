956c30180b6c5f68be56f313d5d75f681eed62cb
Trilikin21 <53579676+Trilikin21@users.noreply.github.com>
2021-10-15 21:38:14 -0400

    added exceptions for not finding the Id and for unprocessable_entity

--- auth-service/src/main/java/com/petclinic/auth/Config/GlobalControllerExceptionHandlerConfig.java
+++ auth-service/src/main/java/com/petclinic/auth/Config/GlobalControllerExceptionHandlerConfig.java
@@ -2,12 +2,17 @@ package com.petclinic.auth.Config;
 
 import com.petclinic.auth.Exceptions.HTTPErrorMessage;
 import com.petclinic.auth.Exceptions.IncorrectPasswordException;
+import com.petclinic.auth.Exceptions.InvalidInputException;
+import com.petclinic.auth.Exceptions.NotFoundException;
+import org.aspectj.weaver.ast.Not;
+import org.omg.CORBA.DynAnyPackage.Invalid;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandlerConfig {
@@ -17,5 +22,19 @@ public class GlobalControllerExceptionHandlerConfig {
     public HTTPErrorMessage resourceNotFoundException(IncorrectPasswordException ex, WebRequest request) {
 
         return new HTTPErrorMessage(401, ex.getMessage());
+    }
+
+    @ExceptionHandler(value = NotFoundException.class)
+    @ResponseStatus(value = NOT_FOUND)
+    public HTTPErrorMessage NotFoundException(NotFoundException ex, WebRequest request) {
+
+        return new HTTPErrorMessage(404, ex.getMessage());
+    }
+
+    @ExceptionHandler(value = IncorrectPasswordException.class)
+    @ResponseStatus(value = UNPROCESSABLE_ENTITY)
+    public HTTPErrorMessage resourceNotFoundException(InvalidInputException ex, WebRequest request) {
+
+        return new HTTPErrorMessage(422, ex.getMessage());
     }
 }
