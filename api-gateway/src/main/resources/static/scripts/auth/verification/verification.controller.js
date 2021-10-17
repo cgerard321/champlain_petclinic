e91d80b9b4689baf6e97dd8374276681178383e8
Fube <fubeitch@gmail.com>
2021-10-16 23:23:35 -0400

    More angular stuff

--- api-gateway/src/main/resources/static/scripts/auth/verification/verification.controller.js
+++ api-gateway/src/main/resources/static/scripts/auth/verification/verification.controller.js
@@ -3,11 +3,11 @@
 angular.module('verification')
     .controller('VerificationController', ['$http', '$scope', "$location", "$stateParams", function ($http, $scope, $location, $stateParams) {
 
-        this.test = () => $http.get('/api/gateway/users/verification/' + $stateParams.token)
+        this.test = () => $http.get('/api/gateway/verification/' + $stateParams.token)
             .then(() => $location.path("/login"))
             .catch(n => {
-                $scope.errorMessage = n.data.message;
                 console.log(n);
+                $scope.errorMessages = n.data.message.split`\n`;
                 console.log("I am in agony")
             });
     }]);
