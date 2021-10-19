ad1b6d6f8f770faffe5dda2086ef00fb12a484e5
Fube <fubeitch@gmail.com>
2021-10-18 21:46:19 -0400

    Fixed login controller

--- api-gateway/src/main/resources/static/scripts/auth/login-form/login-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/auth/login-form/login-form.controller.js
@@ -3,4 +3,17 @@
 angular.module('loginForm')
     .controller('LoginFormController', ["$http", '$state', '$stateParams', function ($http, $state, $stateParams) {
 
+        this.login = $http.post("/api/gateway/users/login", {
+            username: $scope.login.email,
+            password: $scope.login.password,
+        })
+            .then(n => {
+                console.log(n)
+            })
+            .catch(n => {
+                $scope.errorMessages = n.data.message.split`\n`;
+                console.log(n);
+            })
+
+        this.keypress = ({ originalEvent: { key } }) => key === 'Enter' && this.add()
     }]);
