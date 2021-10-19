eaebb61ab092e7e2c2ec4e81a28b5dd2bee50367
Fube <8064235+Fube@users.noreply.github.com>
2021-10-19 15:40:19 -0400

    feat(APIG-CPC-467): Integrate login (#191)
    * Added failing login test
    
    * Added failing bad login test
    
    * Fixed broken test
    
    * Added login impl
    
    * Updated test
    
    * More updates to the test
    
    * Updated login definition to return tuple
    
    * I am an idiot
    
    * Added login impl
    
    * Fixed broken test
    
    * Fixed broken test
    
    * This is the best I could do
    
    * Added my plea
    
    * Got test passing
    
    * Added failing controller test
    
    * Added failing controller test
    
    * Bind /users/login
    
    * Changed to return response entity instead of string
    
    * Got tests to pass
    
    * How is each file worse than the last
    
    * Fixed login controller
    
    * Fixed some typos
    
    * Show unauthorized error message
    
    * Same look and feel as signup
    
    * Fixed signup and login not being on the right side of nav
    
    * Store username, email, and token on successful login
    
    * On successful login, redirect to home
    
    * Added authProvider factory
    
    * Refactored login controlelr to use authProvider
    
    * If not authenticated, redirect to login
    
    * Added purgeUserto authProvider
    
    * Added purgeUser on authenticated/forbidden request, forcing a new login
    
    * Added conditional rendering of nav
    
    * Whitelist register and login to avoid redirect
    
    * Redirect to login, not error on purge
    
    * Whitelist for Q aswell
    
    * Fixed navbar requiring refresh for update after login

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Login.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Login.java
@@ -1,8 +1,14 @@
 package com.petclinic.bffapigateway.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class Login {
 
     private String email;
