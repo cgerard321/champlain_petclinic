e83e40539dc6b495c9f3d7393eb01933075d2bc8
Fube <fubeitch@gmail.com>
2021-10-18 22:28:58 -0400

    Store username, email, and token on successful login

--- api-gateway/src/main/resources/static/scripts/auth/login-form/login-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/auth/login-form/login-form.controller.js
@@ -1,14 +1,19 @@
 'use strict';
 
 angular.module('loginForm')
-    .controller('LoginFormController', ["$http", '$state', '$stateParams', "$scope", function ($http, $state, $stateParams, $scope) {
+    .controller('LoginFormController', ["$http", '$state', '$window', "$scope", function ($http, $state, $window, $scope) {
 
         this.login = () => $http.post("/api/gateway/users/login", {
             email: $scope.login.email,
             password: $scope.login.password,
         })
             .then(n => {
-                console.log(n)
+                const token = n.headers("Authorization");
+                const { data: { username, email } } = n;
+
+                $window.localStorage.setItem("token", token)
+                $window.localStorage.setItem("username", username)
+                $window.localStorage.setItem("email", email)
             })
             .catch(n => {
                 $scope.errorMessages = n.data.message.split`\n`;
