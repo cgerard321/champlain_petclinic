3931442c38257da592f8ffd582e9538970f5f13e
Fube <8064235+Fube@users.noreply.github.com>
2021-10-19 22:31:43 -0400

    feat(AUTH-CPC-486): Default admin account (#195)
    * Changed to AuthenticationManagerBuilder.apply
    
    * Aded E2E test for default admin login
    
    * Added in-mem auth impl
    
    * Fixed broken defaul admin user login test
    
    * Should be verified
    
    * Added default admin credentials injection from env
    
    * Use spring style default value fallback

--- auth-service/src/main/java/com/petclinic/auth/Config/SpringSecurityConfig.java
+++ auth-service/src/main/java/com/petclinic/auth/Config/SpringSecurityConfig.java
@@ -22,13 +22,17 @@ package com.petclinic.auth.Config;
 
 import com.petclinic.auth.User.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -46,6 +50,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserRepo userRepo;
     private final JWTFilter jwtFilter;
+    @Value("${default-admin.username:admin}")
+    private String DEFAULT_ADMIN_USERNAME;
+    @Value("${default-admin.password:admin}")
+    private String DEFAULT_ADMIN_PASSWORD;
 
     private static final String[] AUTH_WHITELIST = {
             // -- Swagger UI v2
@@ -88,9 +96,19 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.userDetailsService(username -> userRepo.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + "not found"))
-        );
+        final DaoAuthenticationConfigurer dao = new DaoAuthenticationConfigurer<>(username -> userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + "not found")));
+
+        dao.passwordEncoder(bCryptPasswordEncoder());
+
+
+        final InMemoryUserDetailsManagerConfigurer inMem = new InMemoryUserDetailsManagerConfigurer();
+        inMem.withUser(DEFAULT_ADMIN_USERNAME)
+                .password("{noop}" + DEFAULT_ADMIN_PASSWORD)
+                .roles("ADMIN");
+
+        auth.apply(inMem);
+        auth.apply(dao);
     }
 
     @Override @Bean
