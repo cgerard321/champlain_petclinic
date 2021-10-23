f0a4334d47bb200eef6e2e305054a01c30bd6212
Vlad Popa <vpopa18@gmail.com>
2021-10-23 18:01:29 -0400

    Customer service is fked gg.

--- api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/BFFApiGatewayController.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/BFFApiGatewayController.java
@@ -76,7 +76,7 @@ public class BFFApiGatewayController {
 
     @PostMapping(value = "owners/{ownerId}/pets" , produces = "application/json", consumes = "application/json")
     public Mono<PetDetails> createPet(@RequestBody PetDetails pet, @PathVariable int ownerId){
-        return customersServiceClient.createPet(ownerId,pet);
+        return customersServiceClient.createPet(pet, ownerId);
     }
 
     //Testing purpose
