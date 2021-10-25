6899c7d2b82a3c8aa0d6e9ff6d5f4e870c3a8edc
Justin Matte <54686741+partenersintime@users.noreply.github.com>
2021-10-25 11:11:54 -0400

    Feat(BILL-CPC-447): Configure Bill Details (#259)
    * Added Link to Bill Details in Bill History Page and Began work on Bill Details.
    
    * Fixed Bill History Details Link to now redirect to the right page.
    
    * Fixed Bill Details and Bill Controller routing to properly display DB data.
    
    Co-authored-by: Justin matte <crakdaple386@hotmail.com>

--- api-gateway/src/main/resources/static/scripts/bill-details/bill-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/bill-details/bill-details.controller.js
@@ -4,11 +4,7 @@ angular.module('billDetails')
     .controller('BillDetailsController', ['$http', '$stateParams', function ($http, $stateParams) {
         var self = this;
 
-        $http.get('api/gateway/bill/' + $stateParams.billId).then(function (resp) {
-            self.bills = resp.data;
-        });
-
-        $http.delete('api/gateway/bill/' + $stateParams.billId).then(function (resp) {
+        $http.get('api/gateway/bills/' + $stateParams.billId).then(function (resp) {
             self.bills = resp.data;
         });
     }]);
