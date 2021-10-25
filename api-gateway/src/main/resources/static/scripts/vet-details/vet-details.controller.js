a12bdeb46a5628182201f2f7826f10751084bedc
yanak <yana.kapeluhova@gmail.com>
2021-10-25 00:26:05 -0400

    Cleaned vet-detail-controller.js

--- api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('vetDetails')
-    .controller('VetDetailsController', ['$http','$stateParams', function ($http, $stateParams) {
+    .controller('VetDetailsController', ['$http', '$stateParams', function ($http, $stateParams) {
         var self = this;
 
         /* added /{{vet.vetID}} in the url */
-        $http.get('api/gateway/vets/'+$stateParams.vetId).then(function (resp) {
+        $http.get('api/gateway/vets/' + $stateParams.vetId).then(function (resp) {
             self.vet = resp.data;
         });
 
-        $http.get("api/gateway/visits/vets/" +$stateParams.vetId).then(function (resp) {
+        $http.get("api/gateway/visits/vets/" + $stateParams.vetId).then(function (resp) {
             self.visitsList = resp.data;
         });
 
