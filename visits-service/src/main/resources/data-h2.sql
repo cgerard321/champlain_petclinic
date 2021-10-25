f8f9cecaf6752ac9ce27d62789f3dcc131d3911a
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-10-25 08:18:13 -0400

    Feat(VISE-CPC-201): Add and edit visit working with calendar UI (#257)
    * fixed error when user didn't select practitioner id and a request was made.
    
    * Fixed bug where selected date wasn't appearing on screen sometimes
    
    * Added functionnality to edit form for the date field
    
    * Added method to parse data from visits controller to the calendar ui
    
    * Fixed issue where edit date wouldn't show up on the calendar UI
    
    * Finished functionality for edit visits on the UI and added a editSelection class to show previous date on calendar
    
    * should all be working fine

--- visits-service/src/main/resources/data-h2.sql
+++ visits-service/src/main/resources/data-h2.sql
@@ -5,10 +5,10 @@ INSERT INTO visits VALUES (4, '3e4e8504-f5d1-448b-8f90-c9b220cdb5a8', 7, '2008-0
 INSERT INTO visits VALUES (5, '7e4e8504-f5d1-448b-8f90-c9b220cdb5a8', 1, '2020-03-04', 'rabies shot', 234568, 0);
 INSERT INTO visits VALUES (6, '60747fd2-e06f-47a3-9e33-96f2fd240fec', 1, '2022-03-04', 'rabies shot', 327874, 1);
 INSERT INTO visits VALUES (7, '50747fd2-e06f-47a3-9e33-96f2fd240fec', 2, '2023-06-04', 'neutered', 327874, 1);
-INSERT INTO visits VALUES (8, '40747fd2-e06f-47a3-9e33-96f2fd240fec', 2,  '2008-09-04', 'spayed', 238372, 1);
+INSERT INTO visits VALUES (8, '40747fd2-e06f-47a3-9e33-96f2fd240fec', 2, '2008-09-04', 'spayed', 238372, 1);
 INSERT INTO visits VALUES (9, '30747fd2-e06f-47a3-9e33-96f2fd240fec', 3, '2010-03-04', 'rabies shot', 234568, 0);
 INSERT INTO visits VALUES (10, '20747fd2-e06f-47a3-9e33-96f2fd240fec', 3, '2031-03-04', 'rabies shot', 327874, 1);
-INSERT INTO visits VALUES (11, '10747fd2-e06f-47a3-9e33-96f2fd240fec', 4,  '2009-06-04', 'neutered', 327874, 1);
+INSERT INTO visits VALUES (11, '10747fd2-e06f-47a3-9e33-96f2fd240fec', 4, '2009-06-04', 'neutered', 327874, 1);
 INSERT INTO visits VALUES (12, '00747fd2-e06f-47a3-9e33-96f2fd240fec', 4, '2022-09-04', 'spayed', 238372, 1);
 INSERT INTO visits VALUES (13, '70747fd2-e06f-47a3-9e33-96f2fd240fec', 5, '2023-03-04', 'rabies shot', 234568, 0);
 INSERT INTO visits VALUES (14, '80747fd2-e06f-47a3-9e33-96f2fd240fec', 5, '2011-03-04', 'rabies shot', 327874, 1);
@@ -19,6 +19,6 @@ INSERT INTO visits VALUES (18, '63747fd2-e06f-47a3-9e33-96f2fd240fec', 7, '2022-
 INSERT INTO visits VALUES (19, '64747fd2-e06f-47a3-9e33-96f2fd240fec', 7, '2021-10-11', 'stomach medication', 327874, 1);
 INSERT INTO visits VALUES (20, '65747fd2-e06f-47a3-9e33-96f2fd240fec', 7, '2019-04-20', 'animal care', 238372, 1);
 INSERT INTO visits VALUES (21, '66747fd2-e06f-47a3-9e33-96f2fd240fec', 7, '2019-09-11', 'examination', 327874, 1);
-INSERT INTO visits VALUES (22, '67747fd2-e06f-47a3-9e33-96f2fd240fec', 7, '2021-11-11', 'examination', 234568, 1);
-INSERT INTO visits VALUES (23, '67747fd2-e06f-47a3-9e33-96f2fd240fey', 7,'2021-11-11', 'stomach medication', 327874, 1);
-INSERT INTO visits VALUES (24,'67747fd2-e06f-47a3-9e33-96f2fd240fet', 7, '2021-11-10', 'animal care', 327874, 1);
+INSERT INTO visits VALUES (22, '67747fd2-e06f-47a3-9e33-96f2fd240fec', 7, '2021-11-09', 'examination', 234568, 1);
+INSERT INTO visits VALUES (23, '67947fd2-e06f-47a3-9e33-96f2fd240fec', 7, '2021-11-11', 'stomach medication', 327874, 1);
+INSERT INTO visits VALUES (24, '67847fd2-e06f-47a3-9e33-96f2fd240fec', 7, '2021-11-10', 'animal care', 327874, 1);
