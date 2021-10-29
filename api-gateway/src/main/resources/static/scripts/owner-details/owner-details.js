3ec678111dcaeec85d33e27a940a01966d897920
CrxJr <50372323+CrxJr@users.noreply.github.com>
2021-10-29 02:44:18 -0400

    change some bugs

--- api-gateway/src/main/resources/static/scripts/owner-details/owner-details.js
+++ api-gateway/src/main/resources/static/scripts/owner-details/owner-details.js
@@ -3,7 +3,7 @@
 angular.module('ownerDetails', ['ui.router'])
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider
-            .state('owner.details', {
+            .state('ownerDetails', {
                 parent: 'app',
                 url: '/owners/details/:ownerId',
                 template: '<owner-details></owner-details>'
