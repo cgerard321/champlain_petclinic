d9d69673f4773b827c15134b79e67a8f1f42e865
Fube <8064235+Fube@users.noreply.github.com>
2021-11-14 18:54:51 -0500

    conf(NULL-CPC-NULL): Configure Nginx (#203)
    * Added Nginx and HEAD preflight endpoint to auth controller
    
    * Removed unused NJS file
    
    * More Nginx config
    
    * Added all the mappings
    
    * Run @WithMockUser when authentication is needed
    
    * Readded /users::POST to SpringSec whitelist
    
    * Changed billing mapping to match updated routes
    
    * Updated .gitignore to account for .log files
    
    * Added owners X visits aggregation
    
    * Fixed visits god forsaken routes a little
    
    * Fixed aggreagte owners not working with trailing slash
    
    * Added singleAggregation NJS and location
    
    * Updated proxy/owners internal routing to be stricter
    
    * Added singleAggregationGET logic
    
    * Fixed aggregate only returning visits for last pet
    
    * Added singleAggregationPUT logic
    
    * Fixed owners::aggregation only working for GET

---
+++ njs/owners.js
@@ -0,0 +1,116 @@
+const catcher501 = (req) =>
+    req.return(
+        501,
+        JSON.stringify({
+            message: err.message,
+            timestamp: new Date().toISOString(),
+        })
+    );
+
+function aggregationGET(req) {
+    req.subrequest(`/proxy/owners`)
+        .then((res) => {
+            const all = [];
+            const owners = JSON.parse(res.responseText);
+            for (let i = 0; i < owners.length; i++) {
+                const petIds = owners[i].pets.map((pet) => pet.id);
+                const cI = i; // TODO: Fix extremely hacky to ensure the index is kept in scope
+                all.push(
+                    // TODO: Probably better to use a pool...
+                    Promise.all(
+                        petIds.map((petId) => getAllVisitsFor(petId, req))
+                    )
+                        .then((visits) =>
+                            visits.reduce((a, b) => a.concat(b), [])
+                        )
+                        .then((visits) => (owners[cI].visits = visits))
+                );
+            }
+
+            Promise.all(all).then(() => {
+                req.return(200, JSON.stringify(owners));
+            });
+        })
+        .catch(catcher501);
+}
+
+function aggregation(req) {
+    req.headersOut["Content-Type"] = "application/json;charset=UTF-8";
+    switch (req.method) {
+        case "GET":
+            aggregationGET(req);
+            break;
+        default:
+            req.subrequest(`/proxy/owners`, {
+                method: req.method,
+                body: req.requestText,
+            }).then((res) => {
+                req.return(res.status, res.responseText);
+            });
+    }
+}
+
+/**
+ *
+ * @param {Number} id - Pet id
+ * @param {Object} req - NJS Request object
+ */
+function getAllVisitsFor(id, req) {
+    return req.subrequest(`/proxy/visits/${id}`).then((res) => {
+        let visits = [];
+
+        if (res.status === 200) {
+            visits = JSON.parse(res.responseText);
+        }
+
+        return visits;
+    });
+}
+
+function singleAggregationGET(req) {
+    const id = req.uri.split`/`.pop();
+    req.subrequest(`/proxy/owners/${id}`)
+        .then((res) => {
+            const owner = JSON.parse(res.responseText);
+            const petIds = owner.pets.map((pet) => pet.id);
+            Promise.all(petIds.map((n) => getAllVisitsFor(n, req)))
+                .then((visits) => visits.reduce((a, b) => a.concat(b), [])) // Array.flat is not implemented
+                .then((visits) => {
+                    owner.visits = visits;
+                    req.return(200, JSON.stringify(owner));
+                });
+        })
+        .catch(catcher501);
+}
+
+function singleAggregationPUT(req) {
+    const id = req.uri.split`/`.pop();
+    req.subrequest(`/proxy/owners/${id}`, {
+        method: "PUT",
+        body: req.requestText,
+    })
+        .then((res) => req.return(res.status, res.responseText))
+        .catch(catcher501);
+}
+
+function singleAggregation(req) {
+    req.headersOut["Content-Type"] = "application/json;charset=UTF-8";
+    switch (req.method) {
+        case "GET":
+            singleAggregationGET(req);
+            break;
+        case "PUT":
+            singleAggregationPUT(req);
+            break;
+        default:
+            req.return(
+                405,
+                JSON.stringify({
+                    message: `Method ${req.method} not allowed`,
+                    timestamp: new Date().toISOString(),
+                })
+            );
+    }
+}
+
+export default { aggregation, singleAggregation };
