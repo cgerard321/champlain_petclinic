5a245267bb2dbe67616dcd9825dd9a9c18515b9e
Fube <fubeitch@gmail.com>
2021-11-14 16:55:33 -0500

    Added owners X visits aggregation

---
+++ njs/owners.js
@@ -0,0 +1,53 @@
+/**
+ *
+ * @param {Request} req
+ */
+function simple(req) {
+    var result = {
+        code: 200,
+        desc: "This is the description",
+    };
+
+    req.headersOut["Content-Type"] = "application/json;charset=UTF-8";
+
+    req.subrequest(`/proxy/owners`)
+        .then((res) => {
+            const all = [];
+            const owners = JSON.parse(res.responseText);
+            for (let i = 0; i < owners.length; i++) {
+                const petIds = owners[i].pets.map((pet) => pet.id);
+                const cI = i;
+                all.push(
+                    // TODO: Probably better to use a pool...
+                    Promise.all(
+                        petIds.map((petId) =>
+                            req
+                                .subrequest(`/proxy/visits/${petId}`)
+                                .then((vRes) => {
+                                    let visits = [];
+                                    if (vRes.status === 200) {
+                                        visits = JSON.parse(vRes.responseText);
+                                    }
+                                    owners[cI].visits = visits;
+                                })
+                        )
+                    )
+                );
+            }
+
+            Promise.all(all).then(() => {
+                req.return(200, JSON.stringify(owners));
+            });
+        })
+        .catch((err) => {
+            req.return(
+                501,
+                JSON.stringify({
+                    message: err.message,
+                    timestamp: new Date().toISOString(),
+                })
+            );
+        });
+}
+
+export default { simple };
