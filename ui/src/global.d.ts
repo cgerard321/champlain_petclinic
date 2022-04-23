4d76034b0c1d2b0ca1bc1707cc78f986c2d01ed7
Fube <fubeitch@gmail.com>
2022-04-23 00:45:22 -0400

    Added generic Table component

--- ui/src/global.d.ts
+++ ui/src/global.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference types="svelte" />
+
+interface TableColumn<T> {
+	key: string;
+	title: string;
+	value: (v: T) => T[keyof T];
+	class?: string;
+	sortable?: boolean;
+	searchValue?: (v: T) => unknown;
+	filterOptions?: ((v: Thing[]) => Record<string, unknown>[]) | Y[];
+	filterValue?: (v: T) => unknown;
+	headerFilterClass?: string;
+	renderValue?: (v: T) => T[keyof T];
+	headerClass?: string;
+	renderComponent?: SvelteComponent;
+}
---
+++ ui/src/lib/components/Table.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import SvelteTable from 'svelte-table';
+
+	export let columns: object[];
+	export let rows: object[];
+	export let sortOrders: number[] = [1, -1];
+	// READ AND WRITE
+	export let sortBy: string = '';
+	export let sortOrder: number = sortOrders?.[0] || 1;
+	export let filterSelections: Object = {};
+	// expand
+	export let expanded: (string | number)[] = [];
+
+	// READ ONLY
+	export let expandRowKey: string = null;
+	export let expandSingle: boolean = false;
+	export let iconAsc: string = '▲';
+	export let iconDesc: string = '▼';
+	export let iconSortable: string = '';
+	export let iconExpand: string = '▼';
+	export let iconExpanded: string = '▲';
+	export let showExpandIcon: boolean = false;
+	export let classNameTable: string = '';
+	export let classNameThead: string = '';
+	export let classNameTbody: string = '';
+	export let classNameSelect: string = '';
+	export let classNameInput: string = '';
+	export let classNameRow: string = '';
+	export let classNameCell: string = '';
+	export let classNameRowExpanded: string = '';
+	export let classNameExpandedContent: string = '';
+	export let classNameCellExpand: string = '';
+</script>
+
+<SvelteTable
+	{columns}
+	{rows}
+	{sortBy}
+	{sortOrder}
+	{sortOrders}
+	{iconAsc}
+	{iconDesc}
+	{iconSortable}
+	{iconExpand}
+	{iconExpanded}
+	{classNameTable}
+	{classNameThead}
+	{classNameTbody}
+	{classNameSelect}
+	{classNameInput}
+	{classNameRow}
+	{classNameRowExpanded}
+	{classNameExpandedContent}
+	{classNameCell}
+	{classNameCellExpand}
+	{expanded}
+	{expandSingle}
+	{expandRowKey}
+	{filterSelections}
+	{showExpandIcon}
+/>
