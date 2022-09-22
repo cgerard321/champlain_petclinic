426038163ca1c6a0ee1f1bc4be39a69c704b67f4
Brandon Levis <54686780+Brandon84-alt@users.noreply.github.com>
2022-09-22 18:54:43 -0400

    Feat/vvs cpc 553 add vets and visists to be reactive (#270)
    * Creation of New Service (Reactive) - Vet is still unactive until launch.
    
    * Deactivated vet from docker-compose

---
+++ vet-service/src/main/java/com/petclinic/vet/VetServiceApplication.java
@@ -0,0 +1,10 @@
+package com.petclinic.vet;
+
+import org.springframework.boot.SpringApplication;
+
+public class VetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetServiceApplication.class, args);
+    }
+}
