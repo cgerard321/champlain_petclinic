cefdff1a058f2894e1555c4cd8eb0e638e2605a9
DmitryGrigoryev <grigoryev.dima@gmail.com>
2022-09-28 19:11:03 -0400

    Revert "temp commit"
    This reverts commit 11e845663e370be795893408c6b18788a7027002.
    
    # Conflicts:
    #	customers-service/src/main/resources/db/hsqldb/schema.sql
    #	customers-service/src/main/resources/db/mysql/schema.sql
    #	customers-service/src/main/resources/schema-h2.sql

--- customers-service/src/main/resources/schema-mysql.sql
+++ customers-service/src/main/resources/schema-mysql.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS photos (
     name VARCHAR(80),
     types VARCHAR(80),
     photo LONGBLOB,
-    INDEX(id)
+    INDEX(name)
     ) engine=InnoDB;
 
 
@@ -22,7 +22,6 @@ CREATE TABLE IF NOT EXISTS owners (
     address VARCHAR(255),
     city VARCHAR(80),
     telephone VARCHAR(20),
-    image_id INT(4),
     INDEX(last_name)
     ) engine=InnoDB;
 
