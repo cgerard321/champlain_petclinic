b41300c92022f0782c2854849ec5cfaf90b75e6d
Marina1411 <marinamel123@live.com>
2022-10-08 15:24:21 -0400

    final fixes

--- api-gateway/src/main/resources/static/scripts/bill-details/bill-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/bill-details/bill-details.controller.js
@@ -3,8 +3,13 @@
 angular.module('billDetails')
     .controller('BillDetailsController', ['$http', '$stateParams', function ($http, $stateParams) {
         var self = this;
+        var customerId = 0;
 
         $http.get('api/gateway/bills/' + $stateParams.billId).then(function (resp) {
             self.bills = resp.data;
         });
+        $http.get('api/gateway/owners/' + $stateParams.ownerId).then(function (resp) {
+            self.owner = resp.data;
+        });
+
     }]);
