3aaefbff77b183afe2986a76cc50d41878d96c10
Kam1st <102925920+Kam1st@users.noreply.github.com>
2022-10-09 18:49:41 -0400

    feat/(VVS-CPC-627): Implement all get and delete methods in api gateway for vets (#299)
    Ready to create the integrations tests for service client class

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/VetDTO.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/VetDTO.java
@@ -1,25 +1,10 @@
 package com.petclinic.bffapigateway.dtos;
-/**
- @author Kamilah Hatteea & Brandon Levis : Vet-Service
-  * Worked together with (Code with Friends) on IntelliJ IDEA
-  * <p>
-  * User: @Kamilah Hatteea
-  * Date: 2022-09-22
-  * Ticket: feat(VVS-CPC-554): edit veterinarian
-  * User: Brandon Levis
-  * Date: 2022-09-22
-  * Ticket: feat(VVS-CPC-553): add veterinarian
- */
-
 import lombok.*;
-
 import java.util.Set;
 
 
 @Getter
-@Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class VetDTO {
     private String vetId;
---
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/domainclientlayer/VetsServiceClientIntegrationTest.java
@@ -0,0 +1,105 @@
+package com.petclinic.bffapigateway.domainclientlayer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.petclinic.bffapigateway.dtos.VetDTO;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWebTestClient
+class VetsServiceClientIntegrationTest {
+
+
+    private VetsServiceClient vetsServiceClient;
+
+    private MockWebServer server;
+
+    private ObjectMapper mapper;
+    VetDTO vetDTO = buildVetDTO();
+    String VET_ID = vetDTO.getVetId();
+    String INVALID_VET_ID = "mjbedf";
+
+    @BeforeEach
+    void setup() {
+
+        server = new MockWebServer();
+        vetsServiceClient = new VetsServiceClient(
+                WebClient.builder(),
+                server.getHostName(),
+                String.valueOf(server.getPort())
+        );
+        mapper = new ObjectMapper();
+    }
+
+    @AfterEach
+    void shutdown() throws IOException {
+        server.shutdown();
+    }
+
+//    @Test
+//    void getVetByVetId() throws JsonProcessingException {
+//        prepareResponse(response -> response
+//                .setHeader("Content-Type", "application/json")
+//                .setBody("    {\n" +
+//                        "        \"vetId\": \"678910\",\n" +
+//                        "        \"firstName\": \"Clementine\",\n" +
+//                        "        \"lastName\": \"LeBlanc\",\n" +
+//                        "        \"email\": \"skjfhf@gmail.com\",\n" +
+//                        "        \"phoneNumber\": \"947-238-2847\",\n" +
+////                        "        \"image\": \"\",\n" +
+//                        "        \"resume\": \"Just became a vet\",\n" +
+//                        "        \"workday\": \"Monday\",\n" +
+//                        "        \"active\": false\n" +
+//                        "    }"));
+//
+//        final VetDTO vet = vetsServiceClient.getVetByVetId("678910").block();
+//        assertFalse(vet.isActive());
+//        assertEquals(vetDTO.getFirstName(), vet.getFirstName());
+//        assertEquals(vetDTO.getLastName(), vet.getLastName());
+//        assertEquals(vetDTO.getEmail(), vet.getEmail());
+//        assertEquals(vetDTO.getPhoneNumber(), vet.getPhoneNumber());
+//        assertEquals(vetDTO.getResume(), vet.getResume());
+//        assertEquals(vetDTO.getWorkday(), vet.getWorkday());
+//    }
+
+
+    private void prepareResponse(Consumer<MockResponse> consumer) {
+        MockResponse response = new MockResponse();
+        consumer.accept(response);
+        this.server.enqueue(response);
+    }
+
+    private VetDTO buildVetDTO() {
+        return VetDTO.builder()
+                .vetId("678910")
+                .firstName("Clementine")
+                .lastName("LeBlanc")
+                .email("skjfhf@gmail.com")
+                .phoneNumber("947-238-2847")
+                .resume("Just became a vet")
+                .image("kjd".getBytes())
+                .workday("Monday")
+                .specialties(new HashSet<>())
+                .isActive(false)
+                .build();
+    }
+
+
+
+}
