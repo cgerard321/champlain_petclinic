fad674ecfbfefdc3ccef685e669b230750ad8088
Félix Étienne Labrie <fel.labrie@live.ca>
2022-10-09 02:29:20 -0400

    corrected repo for Pet

--- customers-service-reactive/src/main/java/com/petclinic/customersservice/data/PetRepo.java
+++ customers-service-reactive/src/main/java/com/petclinic/customersservice/data/PetRepo.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Repository
 public interface PetRepo extends ReactiveMongoRepository<Pet, Integer> {
 
-    Flux<Pet> findPetsByOwnerId(int ownerId);
+    Flux<Pet> findAllPetByOwnerId(int ownerId);
     Flux<PetType> findPetTypes();
+    Optional<PetType> findPetTypeById();
+    Optional<Pet> findPetByOwner(int ownerId, int petId);
 
 }
