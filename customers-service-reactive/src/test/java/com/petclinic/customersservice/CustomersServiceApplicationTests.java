d40b7574907dadb66f7eaf7ff00dbf8e953674ca
Félix Étienne Labrie <fel.labrie@live.ca>
2022-10-09 01:25:58 -0400

    created new reactive customers-service and setup the docker environment for the mongo and the service itself

---
+++ customers-service-reactive/src/test/java/com/petclinic/customersservice/CustomersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic.customersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
