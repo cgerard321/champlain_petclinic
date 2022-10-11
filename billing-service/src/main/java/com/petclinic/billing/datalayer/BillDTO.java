1cd168d4f13240ce23de16404ece96172851b3c7
swafit <william.chalifoux@gmail.com>
2022-10-11 08:02:01 -0400

    Feat/bill cpc 630 upgrade UI form to include vets and upgrade minor details on the form (#313)
    * Added vetID
    
    * minor fix
    
    * Form is finally working
    
    * Final fix to make the vetId appear properly
    
    Co-authored-by: ElricVermaut <52209241+ElricVermaut@users.noreply.github.com>

--- billing-service/src/main/java/com/petclinic/billing/datalayer/BillDTO.java
+++ billing-service/src/main/java/com/petclinic/billing/datalayer/BillDTO.java
@@ -15,12 +15,14 @@ public class BillDTO {
     private String billId;
     private int customerId;
     private String visitType;
+    private String vetId;
     private Date date;
     private double amount;
 
-    public BillDTO(int customerId, String visitType, Date date, double amount) {
+    public BillDTO(int customerId, String visitType, String vetId, Date date, double amount) {
         this.customerId = customerId;
         this.visitType = visitType;
+        this.vetId = vetId;
         this.date = date;
         this.amount = amount;
     }
