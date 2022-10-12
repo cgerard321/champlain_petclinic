cb74b72beebc307e49ebb5639f47f84d23f73a9c
Félix Étienne Labrie <felix.labrie@icloud.com>
2022-10-12 19:39:00 -0400

    trouble shoot insert for pet type

---
+++ customers-service-reactive/src/main/java/com/petclinic/customersservice/data/PetTypeRepo.java
@@ -0,0 +1,10 @@
+package com.petclinic.customersservice.data;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface PetTypeRepo extends ReactiveMongoRepository<Pet, Integer> {
+
+}
