38ff6caae4a53243ff88d4be856be764739c0d07
Félix Étienne Labrie <felix.labrie@icloud.com>
2022-10-13 01:54:01 -0400

    added PetServiceImpl

---
+++ customers-service-reactive/src/main/java/com/petclinic/customersservice/business/PetServiceImpl.java
@@ -0,0 +1,20 @@
+package com.petclinic.customersservice.business;
+
+import com.petclinic.customersservice.data.Pet;
+import com.petclinic.customersservice.data.PetRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class PetServiceImpl implements PetService {
+
+    @Autowired
+    PetRepo petRepo;
+
+    @Override
+    public Mono<Pet> insertPet(Mono<Pet> petMono) {
+        return petMono
+                .flatMap(petRepo::insert);
+    }
+}
