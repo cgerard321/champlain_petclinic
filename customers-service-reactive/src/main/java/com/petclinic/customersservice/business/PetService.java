3e1d3c534de7a5b1c76322820846bf43d60fff7f
Félix Étienne Labrie <felix.labrie@icloud.com>
2022-10-13 01:52:33 -0400

    added PetService

---
+++ customers-service-reactive/src/main/java/com/petclinic/customersservice/business/PetService.java
@@ -0,0 +1,10 @@
+package com.petclinic.customersservice.business;
+
+import com.petclinic.customersservice.data.Pet;
+import reactor.core.publisher.Mono;
+
+public interface PetService {
+
+    Mono<Pet> insertPet(Mono<Pet> petMono);
+
+}
