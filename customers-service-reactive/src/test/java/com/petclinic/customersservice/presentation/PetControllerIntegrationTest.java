167695ecac1ee0e960cce417bfb37456b731b95e
Stacey Jin <jin.stacey@gmail.com>
2022-10-15 18:42:29 -0400

    Fixed ControllerIntegration test.

--- customers-service-reactive/src/test/java/com/petclinic/customersservice/presentation/PetControllerIntegrationTest.java
+++ customers-service-reactive/src/test/java/com/petclinic/customersservice/presentation/PetControllerIntegrationTest.java
@@ -1,11 +1,16 @@
 package com.petclinic.customersservice.presentation;
 
+import com.petclinic.customersservice.data.Owner;
 import com.petclinic.customersservice.data.Pet;
 import com.petclinic.customersservice.data.PetRepo;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
 
 import java.util.Date;
 
@@ -21,27 +26,29 @@ class PetControllerIntegrationTest {
 
 
 
-//    @Test
-//    void deletePetByPetId() {
-//
-//        Pet pet = buildPet();
-//        petRepo.save(pet);
-//
-//        Publisher<Void> setup = petRepo.deletePetById(buildPet().getId());
-//
-//        StepVerifier
-//                .create(setup)
-//                .expectNextCount(0)
-//                .verifyComplete();
-//
-//        webTestClient.delete()
-//                .uri("/pets/" + pet.getId())
-//                .accept(MediaType.APPLICATION_JSON)
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectBody();
-//
-//    }
+    @Test
+    void deletePetByPetId() {
+
+        Pet pet = buildPet();
+        petRepo.save(pet);
+
+        Owner owner = buildOwner();
+
+        Publisher<Void> setup = petRepo.deletePetById(buildPet().getId());
+
+        StepVerifier
+                .create(setup)
+                .expectNextCount(0)
+                .verifyComplete();
+
+        webTestClient.delete()
+                .uri("/owners/" + owner.getId() + "/pets/" + pet.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody();
+
+    }
 
     Date date = new Date(20221010);
 
@@ -53,6 +60,18 @@ class PetControllerIntegrationTest {
                 .photoId(3)
                 .birthDate(date)
                 .ownerId(4)
+                .build();
+    }
+
+    private Owner buildOwner() {
+        return Owner.builder()
+                .id(55)
+                .firstName("FirstName")
+                .lastName("LastName")
+                .address("Test address")
+                .city("test city")
+                .telephone("telephone")
+                .photoId(1)
                 .build();
     }
 }
