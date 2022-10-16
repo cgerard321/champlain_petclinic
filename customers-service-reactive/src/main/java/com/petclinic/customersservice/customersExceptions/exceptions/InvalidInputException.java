39b639b12194f870e5175fd9c7ec49ccb1b4b173
cubajosh <70965052+cubajosh@users.noreply.github.com>
2022-10-16 17:51:48 -0400

    Visit Service Completely Done Testing Passes at 97

--- customers-service-reactive/src/main/java/com/petclinic/customersservice/customersExceptions/exceptions/InvalidInputException.java
+++
@@ -1,9 +0,0 @@
-package com.petclinic.customersservice.customersExceptions.exceptions;
-
-public class InvalidInputException extends RuntimeException {
-
-    public InvalidInputException() {}
-
-    public InvalidInputException(String message) { super(message); }
-
-}
--- customers-service-reactive/src/main/java/com/petclinic/customersservice/customersExceptions/exceptions/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.petclinic.customersservice.customersExceptions.exceptions;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException() {}
-
-    public NotFoundException(String message) { super(message); }
-
-}
