b8445072fa74c1a8bad1a75bddec24039e3362cb
Brandon <brandonlevis84@gmail.com>
2022-10-20 09:00:14 -0400

    Front-End photos, need main to be fixed before I can see if it works properly...
    Last push to main broke the branch. Can't view Owners, Veterinarians data

--- api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
@@ -13,4 +13,40 @@ angular.module('vetDetails')
             self.visitsList = resp.data;
         });
 
+        //photo
+        $http.get('api/gateway/vets/photo/' + $stateParams.vetId).then(function (resp) {
+            self.vetPhoto = resp.data;
+        });
+        const fileInput = document.querySelector('input[id="photoVet"]');
+        let vetPhoto = "";
+        fileInput.addEventListener('change', (e) => {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                vetPhoto = reader.result
+                    .replace('data:', '')
+                    .replace(/^.+,/, '');
+                self.PreviewImage = vetPhoto;
+                var image = {
+                    name: uuidv4(),
+                    type: "jpeg",
+                    photo: vetPhoto
+                };
+                var test = $http.post('api/gateway/vets/photo/' + $stateParams.vetId, image);
+                console.log(test);
+
+            };
+            reader.readAsDataURL(file);
+        });
+
+        self.init = function (id){
+            $http.get('api/gateway/vets/' + $stateParams.vetId + '/vet/photo/' + id).then(function (resp) {
+                self.vetPhoto = resp.data;
+            });
+        }
+        function uuidv4() {
+            return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+            );
+        };
     }]);
