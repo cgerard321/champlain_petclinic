bd8e4b6faa36fd3fb6327c1fdd28cc3f22a2fccd
Marina1411 <marinamel123@live.com>
2022-10-20 16:34:30 -0400

    added vet name to bill details

--- billing-service/src/main/java/com/petclinic/billing/datalayer/DataSetupService.java
+++ billing-service/src/main/java/com/petclinic/billing/datalayer/DataSetupService.java
@@ -25,8 +25,13 @@ public class DataSetupService implements CommandLineRunner {
        BillDTO b4 = new BillDTO( 4, "injury", "823097", Date.valueOf("2021-09-22"), 199.99);
        BillDTO b2 = new BillDTO( 2, "operation", "784233",  Date.valueOf("2021-09-20"), 199.99);
        BillDTO b5 = new BillDTO( 5, "chronic", "823097", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b6 = new BillDTO( 5, "chronic", "238372", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b7 = new BillDTO( 1, "operation", "823097", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b8 = new BillDTO( 2, "chronic", "784233", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b9 = new BillDTO( 2, "injury", "238372", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b10 = new BillDTO( 4, "chronic", "784233", Date.valueOf("2021-09-23"), 199.99);
 
-        Flux.just(b1,b2,b3,b4,b5)
+        Flux.just(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10)
                 .flatMap(b -> billService.CreateBill(Mono.just(b))
                         .log(b.toString()))
                 .subscribe();
