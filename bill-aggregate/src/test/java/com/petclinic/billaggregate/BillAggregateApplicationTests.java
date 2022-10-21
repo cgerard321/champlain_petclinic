d0bdb836be911e107b8121b7fb5b674363e2ce10
Marina1411 <marinamel123@live.com>
2022-10-21 17:14:11 -0400

    aggregate service added

---
+++ bill-aggregate/src/test/java/com/petclinic/billaggregate/BillAggregateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic.billaggregate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillAggregateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
