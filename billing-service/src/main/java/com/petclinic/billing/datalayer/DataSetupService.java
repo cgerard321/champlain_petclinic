e8435bb198708d0ac4dfd4bc27322922490f3049
Marina1411 <marinamel123@live.com>
2022-10-21 17:59:32 -0400

    vetBill id matches both services

--- billing-service/src/main/java/com/petclinic/billing/datalayer/DataSetupService.java
+++ billing-service/src/main/java/com/petclinic/billing/datalayer/DataSetupService.java
@@ -20,16 +20,16 @@ public class DataSetupService implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-       BillDTO b1 = new BillDTO( 1, "general", "234568", Date.valueOf("2021-09-19"),59.99);
-       BillDTO b3 = new BillDTO( 3, "operation", "327874", Date.valueOf("2021-09-21"), 199.99);
-       BillDTO b4 = new BillDTO( 4, "injury", "238372", Date.valueOf("2021-09-22"), 199.99);
-       BillDTO b2 = new BillDTO( 2, "operation", "823097", Date.valueOf("2021-09-20"), 199.99);
-       BillDTO b5 = new BillDTO( 5, "chronic", "842370", Date.valueOf("2021-09-23"), 199.99);
-        BillDTO b6 = new BillDTO( 5, "chronic", "234568", Date.valueOf("2021-09-23"), 199.99);
-        BillDTO b7 = new BillDTO( 1, "operation", "234568", Date.valueOf("2021-09-23"), 199.99);
-        BillDTO b8 = new BillDTO( 2, "chronic", "842370", Date.valueOf("2021-09-23"), 199.99);
-        BillDTO b9 = new BillDTO( 2, "injury", "823097", Date.valueOf("2021-09-23"), 199.99);
-        BillDTO b10 = new BillDTO( 4, "chronic", "238372", Date.valueOf("2021-09-23"), 199.99);
+       BillDTO b1 = new BillDTO( 1, "general", "1", Date.valueOf("2021-09-19"),59.99);
+       BillDTO b3 = new BillDTO( 3, "operation", "2", Date.valueOf("2021-09-21"), 199.99);
+       BillDTO b4 = new BillDTO( 4, "injury", "1", Date.valueOf("2021-09-22"), 199.99);
+       BillDTO b2 = new BillDTO( 2, "operation", "1", Date.valueOf("2021-09-20"), 199.99);
+       BillDTO b5 = new BillDTO( 5, "chronic", "2", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b6 = new BillDTO( 5, "chronic", "3", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b7 = new BillDTO( 1, "operation", "4", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b8 = new BillDTO( 2, "chronic", "5", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b9 = new BillDTO( 2, "injury", "6", Date.valueOf("2021-09-23"), 199.99);
+        BillDTO b10 = new BillDTO( 4, "chronic", "6", Date.valueOf("2021-09-23"), 199.99);
 
         Flux.just(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10)
                 .flatMap(b -> billService.CreateBill(Mono.just(b))
