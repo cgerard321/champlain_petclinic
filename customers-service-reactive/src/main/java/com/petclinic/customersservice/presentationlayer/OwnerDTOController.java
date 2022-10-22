e1b1cb68754b98f009ef2ad9d578594c3ad3e50f
Félix Étienne Labrie <48634914+felixlabrie@users.noreply.github.com>
2022-10-22 19:39:45 -0400

    test

--- customers-service-reactive/src/main/java/com/petclinic/customersservice/presentationlayer/OwnerDTOController.java
+++ customers-service-reactive/src/main/java/com/petclinic/customersservice/presentationlayer/OwnerDTOController.java
@@ -3,6 +3,8 @@ package com.petclinic.customersservice.presentationlayer;
 
 import com.petclinic.customersservice.business.OwnerDTO;
 import com.petclinic.customersservice.business.OwnerDTOService;
+import com.petclinic.customersservice.business.OwnerService;
+import com.petclinic.customersservice.data.Owner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +19,16 @@ public class OwnerDTOController {
     @Autowired
     private OwnerDTOService ownerDTOService;
 
-    @GetMapping("/{ownerId}")
-    public Mono<OwnerDTO> getOwnerDTOByOwnerId(@PathVariable String ownerId) {
-        return ownerDTOService.getOwnerDTOByOwnerId(ownerId);
+    @Autowired
+    private OwnerService ownerService;
+
+//    @GetMapping("/{ownerId}")
+//    public Mono<OwnerDTO> getOwnerDTOByOwnerId(@PathVariable String ownerId) {
+//        return ownerDTOService.getOwnerDTOByOwnerId(ownerId);
+//    }
+    @GetMapping("/{ownderId}")
+    public Mono<Owner> getOwnerByOwnerId(@PathVariable String ownerId) {
+        return ownerService.getOwnerByOwnerId(ownerId);
     }
 
 }
