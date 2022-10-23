96189ab2f807dd5e25a48b957206b090e715f8c9
Félix Étienne Labrie <48634914+felixlabrie@users.noreply.github.com>
2022-10-23 02:47:02 -0400

    fix updateOwner

--- customers-service-reactive/src/main/java/com/petclinic/customersservice/business/OwnerServiceImpl.java
+++ customers-service-reactive/src/main/java/com/petclinic/customersservice/business/OwnerServiceImpl.java
@@ -34,11 +34,6 @@ public class OwnerServiceImpl implements OwnerService {
         return repo.findById(ownerId)
                 .flatMap(p -> ownerMono
                         .doOnNext(e -> e.setId(p.getId()))
-                        .doOnNext(e -> e.setFirstName(p.getFirstName()))
-                        .doOnNext(e -> e.setLastName(p.getLastName()))
-                        .doOnNext(e -> e.setAddress(p.getAddress()))
-                        .doOnNext(e -> e.setCity(p.getCity()))
-                        .doOnNext(e -> e.setTelephone(p.getTelephone()))
                         .doOnNext(e -> e.setPhotoId(p.getPhotoId()))
                 )
                 .flatMap(repo::save);
