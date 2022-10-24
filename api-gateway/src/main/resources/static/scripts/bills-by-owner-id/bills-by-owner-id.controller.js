4b0f98f545b2177591ba6515bdf031fe3aa36f38
swafit <william.chalifoux@gmail.com>
2022-10-24 09:57:49 -0400

    Added the delete button for deleteBillsByCustomerId (#364)

--- api-gateway/src/main/resources/static/scripts/bills-by-owner-id/bills-by-owner-id.controller.js
+++ api-gateway/src/main/resources/static/scripts/bills-by-owner-id/bills-by-owner-id.controller.js
@@ -12,4 +12,27 @@ angular.module('billsByOwnerId')
             self.owner = resp.data;
         });
 
+        $stateParams.deleteBillsByOwnerId = function (customerId) {
+            let varIsConf = confirm('You are about to delete all the bills for the owner with ownerId  ' + customerId + '. Are you sure this is what you want to do?');
+            if (varIsConf) {
+
+                $http.delete('api/gateway/bills/customer/' + customerId).then(successCallback, errorCallback)
+
+                function successCallback(response) {
+                    $stateParams.errors = [];
+                    alert("The bills for owner with id " + customerId + " were deleted successfully");
+                    console.log(response, 'res');
+                    // on success refresh bills to confirm they have all been deleted
+                    $http.get("api/gateway/bills/customer/" + ($stateParams.customerId)).then(function (resp) {
+                        self.billsByOwnerId = resp.data;
+                    });
+                }
+
+                function errorCallback(error) {
+                    alert(data.errors);
+                    console.log(error, 'Could not receive data');
+                }
+            }
+        };
+
     }]);
