f031df05dba75d84eb92a84ae32a73b227cbd445
Marina1411 <marinamel123@live.com>
2022-10-23 23:33:25 -0400

    delete all implemented

--- api-gateway/src/main/resources/static/scripts/bills-by-vet-id/bills-by-vet-id.js
+++ api-gateway/src/main/resources/static/scripts/bills-by-vet-id/bills-by-vet-id.js
@@ -8,4 +8,9 @@ angular.module('billsByVetId', ['ui.router'])
                 url: '/bills/vetId/:vetId',
                 template: '<bills-by-vet-id></bills-by-vet-id>'
             })
+            .state('deleteBillsByVetId', {
+                parent: 'app',
+                url: '/bills/vetId/:vetId/deleteBill',
+                template: '<bill-by-vet-id></bill-by-vet-id>'
+            })
     }]);
