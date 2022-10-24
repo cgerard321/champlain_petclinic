dcf9e3d526a05c2642c89a1a2ef96513c260e078
William <william.chalifoux@gmail.com>
2022-10-24 01:30:15 -0400

    Added the delete method for deleteBillsByCustomerId

--- billing-service/src/main/java/com/petclinic/billing/businesslayer/BillServiceImpl.java
+++ billing-service/src/main/java/com/petclinic/billing/businesslayer/BillServiceImpl.java
@@ -65,8 +65,17 @@ public class BillServiceImpl implements BillService{
         return billRepository.findByCustomerId(customerId).map(EntityDtoUtil::toDto);
     }
 
+
+
     @Override
     public Flux<BillDTO> GetBillsByVetId(String vetId) {
         return billRepository.findByVetId(vetId).map(EntityDtoUtil::toDto);
+    }
+
+
+    @Override
+    public Flux<Void> DeleteBillsByCustomerId(int customerId){
+        return billRepository.deleteBillsByCustomerId(customerId);
+
     }
 }
