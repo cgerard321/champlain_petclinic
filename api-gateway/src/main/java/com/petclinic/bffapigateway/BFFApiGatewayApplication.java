5f7708f52b54bcebb46a5ec7d88e2b566d96779a
William Chalifoux <william.chalifoux@gmail.com>
2023-09-16 20:11:13 -0400

    VisitDTO split to VisitRequestDTO and VisitResponseDTO. Removed a few unused tests and

--- api-gateway/src/main/java/com/petclinic/bffapigateway/BFFApiGatewayApplication.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/BFFApiGatewayApplication.java
@@ -2,8 +2,6 @@ package com.petclinic.bffapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class BFFApiGatewayApplication {
