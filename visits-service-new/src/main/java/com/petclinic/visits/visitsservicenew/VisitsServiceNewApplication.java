ff14cc44207f721ff328782938df0954d9c2341d
William Chalifoux <william.chalifoux@gmail.com>
2023-09-16 20:48:32 -0400

    Unused import removal

--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/VisitsServiceNewApplication.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/VisitsServiceNewApplication.java
@@ -2,13 +2,10 @@ package com.petclinic.visits.visitsservicenew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class VisitsServiceNewApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VisitsServiceNewApplication.class, args);
     }
-
 }
