08092c4ffa27c579d18e8fd03432c503fe3602ef
Nic Martoccia <nicholasmartoccia04@icloud.com>
2023-09-17 22:32:08 -0400

    Added the list of products to test and added the basic files for the product form

---
+++ api-gateway/src/main/resources/static/scripts/inventory-product-list/inventory-product-list.component.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('bundleList')
+    .component('bundleList', {
+        templateUrl: 'scripts/bundle-list/bundle-list.template.html',
+        controller: 'BundleListController'
+    });
