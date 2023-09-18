5e98d760d2871cc40e9b80e26ed053832efac6bc
David Rallo <davidrallo28@gmail.com>
2023-09-18 16:52:33 -0400

    Implemented delete tests and get all tests

--- api-gateway/src/test/java/com/petclinic/bffapigateway/domainclientlayer/BillServiceClientIntegrationTest.java
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/domainclientlayer/BillServiceClientIntegrationTest.java
@@ -10,17 +10,25 @@ import com.petclinic.bffapigateway.dtos.Visits.VisitResponseDTO;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.*;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 
 class BillServiceClientIntegrationTest {
 
@@ -30,26 +38,23 @@ class BillServiceClientIntegrationTest {
 
     private ObjectMapper mapper = new ObjectMapper();
 
-    @BeforeAll
-    static void beforeAllSetUp() throws IOException {
-        server = new MockWebServer();
-        server.start();
-    }
     @BeforeEach
-    void setUp() {
-        billServiceClient = new BillServiceClient(WebClient.builder(),
+    void setUp(){
+
+        server = new MockWebServer();
+        billServiceClient = new BillServiceClient(
+                WebClient.builder(),
                 server.getHostName(),
-                String.valueOf(server.getPort()));
-    }
-    @AfterAll
-    static void tearDown() throws IOException {
-        server.shutdown();
+                String.valueOf(server.getPort())
+        );
+        mapper = new ObjectMapper();
     }
 
     @AfterEach
     void shutdown() throws IOException {
         this.server.shutdown();
     }
+
 
     private void prepareResponse(Consumer<MockResponse> consumer) {
         MockResponse response = new MockResponse();
@@ -66,6 +71,24 @@ class BillServiceClientIntegrationTest {
             .date(null)
             .build();
 
+    private final BillResponseDTO billResponseDTO2 = BillResponseDTO.builder()
+            .billId("2")
+            .amount(150.0)
+            .customerId(2)
+            .vetId("2")
+            .visitType("Check up")
+            .date(null)
+            .build();
+
+    private final BillResponseDTO billResponseDTO3 = BillResponseDTO.builder()
+            .billId("3")
+            .amount(250.0)
+            .customerId(3)
+            .vetId("3")
+            .visitType("Check up")
+            .date(null)
+            .build();
+
     @Test
     void getBillById() throws Exception {
         server.enqueue(new MockResponse().setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -74,6 +97,28 @@ class BillServiceClientIntegrationTest {
         Mono<BillResponseDTO> billResponseDTOMono = billServiceClient.getBilling("1");
         StepVerifier.create(billResponseDTOMono)
                 .expectNextMatches(returnedBillResponseDTO1 -> returnedBillResponseDTO1.getBillId().equals("1"))
+                .verifyComplete();
+    }
+
+    @Test
+    void getBillByVetId() throws Exception {
+        server.enqueue(new MockResponse().setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setBody(mapper.writeValueAsString(billResponseDTO)).addHeader("Content-Type", "application/json"));
+
+        Flux<BillResponseDTO> billResponseDTOMono = billServiceClient.getBillsByVetId("1");
+        StepVerifier.create(billResponseDTOMono)
+                .expectNextMatches(returnedBillResponseDTO1 -> returnedBillResponseDTO1.getVetId().equals("1"))
+                .verifyComplete();
+    }
+
+    @Test
+    void getBillsByCustomerId() throws Exception {
+        server.enqueue(new MockResponse().setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setBody(mapper.writeValueAsString(billResponseDTO)).addHeader("Content-Type", "application/json"));
+
+        Flux<BillResponseDTO> billResponseDTOMono = billServiceClient.getBillsByOwnerId(1);
+        StepVerifier.create(billResponseDTOMono)
+                .expectNextMatches(returnedBillResponseDTO1 -> returnedBillResponseDTO1.getCustomerId() == 1)
                 .verifyComplete();
     }
 
@@ -96,6 +141,78 @@ class BillServiceClientIntegrationTest {
         final Mono<Void> empty = billServiceClient.deleteBill(bill.getBillId());
 
         assertNull(empty.block());
+    }
+
+    @Test
+    void shouldDeleteBillByVetId() throws JsonProcessingException {
+        final BillDetails bill = BillDetails.builder()
+                .billId(UUID.randomUUID().toString())
+                .vetId("15")
+                .customerId(2)
+                .date(null)
+                .amount(100)
+                .visitType("Check")
+                .build();
+
+        final String body = mapper.writeValueAsString(mapper.convertValue(bill, BillDetails.class));
+        prepareResponse(response -> response
+                .setHeader("Content-Type", "application/json")
+                .setBody(body));
+
+        final Flux<Void> empty = billServiceClient.deleteBillsByVetId(bill.getVetId());
+
+        StepVerifier.create(empty)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void shouldDeleteBillByCustomerId() throws JsonProcessingException {
+        final BillDetails bill = BillDetails.builder()
+                .billId(UUID.randomUUID().toString())
+                .vetId("15")
+                .customerId(2)
+                .date(null)
+                .amount(100)
+                .visitType("Check")
+                .build();
+
+        final String body = mapper.writeValueAsString(mapper.convertValue(bill, BillDetails.class));
+        prepareResponse(response -> response
+                .setHeader("Content-Type", "application/json")
+                .setBody(body));
+
+        final Flux<Void> empty = billServiceClient.deleteBillsByCustomerId(bill.getCustomerId());
+
+        StepVerifier.create(empty)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void getAllBills() throws JsonProcessingException {
+        // Prepare a list of bill responses as if they were returned from the service
+        List<BillResponseDTO> billResponseList = Arrays.asList(
+            billResponseDTO, billResponseDTO2, billResponseDTO3
+        );
+
+        final String body = mapper.writeValueAsString(billResponseList);
+
+        prepareResponse(response -> response
+                .setHeader("Content-Type", "application/json")
+                .setBody(body));
+
+        Flux<BillResponseDTO> billResponseFlux = billServiceClient.getAllBilling();
+
+        StepVerifier.create(billResponseFlux.collectList())
+                .expectNextMatches(returnedBillList -> {
+                    assertEquals(3, returnedBillList.size());
+                    assertTrue(returnedBillList.stream().anyMatch(bill -> "1".equals(bill.getBillId())));
+                    assertTrue(returnedBillList.stream().anyMatch(bill -> "2".equals(bill.getBillId())));
+                    assertTrue(returnedBillList.stream().anyMatch(bill -> "3".equals(bill.getBillId())));
+                    return true;
+                })
+                .verifyComplete();
     }
 
 
