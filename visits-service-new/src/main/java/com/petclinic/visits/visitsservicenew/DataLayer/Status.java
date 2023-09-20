d942bcf4e3e9e671ac52363793659bfd2a52b15d
CptDoritos <fred.vezina@hotmail.com>
2023-09-20 15:50:50 -0400

    Changed Status into an Enum

---
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DataLayer/Status.java
@@ -0,0 +1,17 @@
+package com.petclinic.visits.visitsservicenew.DataLayer;
+
+public enum Status {
+
+    UPCOMING,
+
+    REQUESTED,
+
+    CONFIRMED,
+
+    CANCELLED,
+
+    IN_PROGRESS,
+
+    COMPLETED
+
+}
