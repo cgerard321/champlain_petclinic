542f99fb3077c45168668860e5554bfe0685727c
MichaelVacca <michaelvacca07@gmail.com>
2023-09-20 23:24:43 -0400

    Fixed rouuting issue with create visit button

--- api-gateway/src/main/resources/static/scripts/visits/visits.js
+++ api-gateway/src/main/resources/static/scripts/visits/visits.js
@@ -8,15 +8,4 @@ angular.module('visits', ['ui.router'])
                 url: '/visits/new',
                 template: '<visits></visits>'
             })
-<<<<<<< HEAD
     }]);
-=======
-    }]);
-
-<<<<<<< HEAD
-//url: '/owners/:ownerId/pets/:petId/visits',
-//                url: '/visits/new',
-=======
-//url: '/owners/:ownerId/pets/:petId/visits',
->>>>>>> 67834aa0 (feat/(VIST-CPC-762) User can now navigate to create new visit page (#415))
->>>>>>> 75d3a380 (feat/(VIST-CPC-762) User can now navigate to create new visit page (#415))
