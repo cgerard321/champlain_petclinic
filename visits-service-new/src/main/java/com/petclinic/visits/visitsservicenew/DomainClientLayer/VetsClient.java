f191cb486fcf122aec04e4d912d3ce32cfd88ec7
Cristian <cf93082@gmail.com>
2023-09-21 15:59:22 -0400

    Started Domain Client

--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/VetsClient.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/VetsClient.java
@@ -1,6 +1,5 @@
 package com.petclinic.visits.visitsservicenew.DomainClientLayer;
 
-
 import com.petclinic.visits.visitsservicenew.Exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+
 @Service
 public class VetsClient {
 
@@ -16,7 +16,7 @@ public class VetsClient {
     private final String vetClientServiceBaseURL;
 
     public VetsClient(@Value("${app.vet-service.host}") String vetServiceHost,
-                        @Value("${app.vet-service.port}") String vetServicePort){
+                      @Value("${app.vet-service.port}") String vetServicePort) {
 
         vetClientServiceBaseURL = "http://" + vetServiceHost + ":" + vetServicePort + "/vets";
 
@@ -26,6 +26,7 @@ public class VetsClient {
 
     }
 
+
     public Mono<VetDTO> getVetByVetId(String vetId) {
         Mono<VetDTO> vetDTOMono =
                 webClient
@@ -34,7 +35,7 @@ public class VetsClient {
                         .retrieve()
                         .onStatus(HttpStatusCode::is4xxClientError, error -> {
                             HttpStatusCode statusCode = error.statusCode();
-                            if(statusCode.equals(HttpStatus.NOT_FOUND))
+                            if (statusCode.equals(HttpStatus.NOT_FOUND))
                                 return Mono.error(new NotFoundException("No veterinarian was found with vetId: " + vetId));
                             return Mono.error(new IllegalArgumentException("Something went wrong"));
                         })
