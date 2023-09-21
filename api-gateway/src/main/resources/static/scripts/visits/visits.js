1ba7de6b2dc308fc9db9e0624500c4f642249068
MichaelVacca <michaelvacca07@gmail.com>
2023-09-20 21:09:44 -0400

    Trying to fix button

--- api-gateway/src/main/resources/static/scripts/visits/visits.js
+++ api-gateway/src/main/resources/static/scripts/visits/visits.js
@@ -3,9 +3,10 @@
 angular.module('visits', ['ui.router'])
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider
-            .state('visits', {
+            .state('visitsNew', {
                 parent: 'app',
                 url: '/owners/:ownerId/pets/:petId/visits',
                 template: '<visits></visits>'
             })
     }]);
+
