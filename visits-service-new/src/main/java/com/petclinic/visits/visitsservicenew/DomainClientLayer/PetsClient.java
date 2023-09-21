f8ef57a703ac062e395d91043a7912970df79a93
Cristian <cf93082@gmail.com>
2023-09-21 15:59:22 -0400

    Started Domain Client

--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/PetsClient.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/PetsClient.java
@@ -1,6 +1,5 @@
 package com.petclinic.visits.visitsservicenew.DomainClientLayer;
 
-
 import com.petclinic.visits.visitsservicenew.Exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+
 @Service
 public class PetsClient {
 
@@ -16,10 +16,10 @@ public class PetsClient {
     private final String petClientServiceBaseURL;
 
 
-    public PetsClient(@Value("${app.customers-service.host}") String petsServiceHost,
-                      @Value("${app.customers-service.port}") String petsServicePort) {
+    public PetsClient(@Value("${app.customers-service-reactive.host}") String petsServiceHost,
+                      @Value("${app.customers-service-reactive.port}") String petsServicePort){
 
-        petClientServiceBaseURL = "http://" + petsServiceHost + ":" + petsServicePort + "/pet";
+        petClientServiceBaseURL = "http://" + petsServiceHost + ":" + petsServicePort + "/vets";
 
         this.webClient = WebClient.builder()
                 .baseUrl(petClientServiceBaseURL)
@@ -45,7 +45,5 @@ public class PetsClient {
                     .bodyToMono(PetResponseDTO.class);
         }
     }
-
-
 
 
