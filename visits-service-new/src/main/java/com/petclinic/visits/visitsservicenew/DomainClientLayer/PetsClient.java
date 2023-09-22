13e046ef8ed518d79af20c8f7a5be16effe5412c
Cristian <cf93082@gmail.com>
2023-09-22 00:10:35 -0400

    Testing Done. Ready to merge to main

--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/PetsClient.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/PetsClient.java
@@ -19,29 +19,33 @@ public class PetsClient {
     public PetsClient(@Value("${app.customers-service-reactive.host}") String petsServiceHost,
                       @Value("${app.customers-service-reactive.port}") String petsServicePort){
 
+
         petClientServiceBaseURL = "http://" + petsServiceHost + ":" + petsServicePort + "/pet";
 
         this.webClient = WebClient.builder()
                 .baseUrl(petClientServiceBaseURL)
                 .build();
 
+        }
+
+
+        public Mono<PetResponseDTO> getPetById( final int petId){
+            return webClient
+                    .get()
+                    .uri(petClientServiceBaseURL + "/{petId}",petId)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, error -> {
+                        HttpStatusCode statusCode = error.statusCode();
+                        if (statusCode.equals(HttpStatus.NOT_FOUND))
+                            return Mono.error(new NotFoundException("No pet was found with petId: " + petId));
+                        return Mono.error(new IllegalArgumentException("Something went wrong"));
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, error ->
+                            Mono.error(new IllegalArgumentException("Something went wrong"))
+                    )
+                    .bodyToMono(PetResponseDTO.class);
+        }
     }
 
-    public Mono<PetResponseDTO> getPetById(final int petId) {
-        return webClient
-                .get()
-                .uri(petClientServiceBaseURL + "/" + petId)
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, error -> {
-                    HttpStatusCode statusCode = error.statusCode();
-                    if (statusCode.equals(HttpStatus.NOT_FOUND))
-                        return Mono.error(new NotFoundException("No pet was found with petId: " + petId));
-                    return Mono.error(new IllegalArgumentException("Something went wrong"));
-                })
-                .onStatus(HttpStatusCode::is5xxServerError, error ->
-                        Mono.error(new IllegalArgumentException("Something went wrong"))
-                )
-                .bodyToMono(PetResponseDTO.class);
-    }
-}
+
 
