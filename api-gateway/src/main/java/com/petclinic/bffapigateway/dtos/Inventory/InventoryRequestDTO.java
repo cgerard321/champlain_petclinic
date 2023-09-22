50a97bebb9e8d2f9de9069196c067be8140fd64b
Abdur Siddiqui <103337298+SandwichKing978@users.noreply.github.com>
2023-09-21 20:36:30 -0400

    Feat/invt cpc 757 new/part1/create inventory (#417)
    * did some stuff
    
    * I implemented a create inventory and update inventory and did all the testsing in inventory service
    
    * fixed merged conflict
    
    * fixed merge conflict

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Inventory/InventoryRequestDTO.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Inventory/InventoryRequestDTO.java
@@ -1,20 +1,20 @@
-package com.petclinic.inventoryservice.datalayer.Inventory;
+package com.petclinic.bffapigateway.dtos.Inventory;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Inventory {
-    @Id
-    private String id;
-    private String inventoryId;
+public class InventoryRequestDTO {
+
+
     private String inventoryName;
+
     private InventoryType inventoryType;
+
     private String inventoryDescription;
 }
--- inventory-service/src/main/java/com/petclinic/inventoryservice/businesslayer/ProductInventoryService.java
+++ inventory-service/src/main/java/com/petclinic/inventoryservice/businesslayer/ProductInventoryService.java
@@ -1,11 +1,15 @@
 package com.petclinic.inventoryservice.businesslayer;
 
+import com.petclinic.inventoryservice.presentationlayer.InventoryRequestDTO;
+import com.petclinic.inventoryservice.presentationlayer.InventoryResponseDTO;
 import com.petclinic.inventoryservice.presentationlayer.ProductRequestDTO;
 import com.petclinic.inventoryservice.presentationlayer.ProductResponseDTO;
 import reactor.core.publisher.Mono;
 
 public interface ProductInventoryService {
     Mono<ProductResponseDTO> addProductToInventory(Mono<ProductRequestDTO> productRequestDTOMono, String inventoryId);
+    Mono<InventoryResponseDTO> addInventory(Mono<InventoryRequestDTO> inventoryRequestDTO);
 
+    Mono<InventoryResponseDTO> updateInventory(Mono<InventoryRequestDTO> inventoryRequestDTO, String inventoryId);
     Mono<Void> deleteProductInInventory(String inventoryId, String productId);
 }
