1c0f189f399d4ddda17d50cc78487b46532dfd0b
Cristian <cf93082@gmail.com>
2023-09-21 22:58:09 -0400

    Implemented Domain Client, need to put service in exposed port in order to test

--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/VetsClient.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/VetsClient.java
@@ -1,8 +1,12 @@
 package com.petclinic.visits.visitsservicenew.DomainClientLayer;
 
+import com.petclinic.visits.visitsservicenew.Exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class VetsClient {
@@ -19,5 +23,25 @@ public class VetsClient {
                 .baseUrl(vetClientServiceBaseURL)
                 .build();
 
+    }
+
+    public Mono<VetDTO> getVetByVetId(String vetId) {
+        Mono<VetDTO> vetDTOMono =
+                webClient
+                        .get()
+                        .uri(vetClientServiceBaseURL + "/{vetId}", vetId)
+                        .retrieve()
+                        .onStatus(HttpStatusCode::is4xxClientError, error -> {
+                            HttpStatusCode statusCode = error.statusCode();
+                            if(statusCode.equals(HttpStatus.NOT_FOUND))
+                                return Mono.error(new NotFoundException("No veterinarian was found with vetId: " + vetId));
+                            return Mono.error(new IllegalArgumentException("Something went wrong"));
+                        })
+                        .onStatus(HttpStatusCode::is5xxServerError, error ->
+                                Mono.error(new IllegalArgumentException("Something went wrong"))
+                        )
+                        .bodyToMono(VetDTO.class);
+
+        return vetDTOMono;
     }
 }
