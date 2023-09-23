9dede302c8b82e5f860376720385e06ac67afcc7
Timbro <57909059+AlexeiTimbro@users.noreply.github.com>
2023-09-23 00:03:11 -0400

    feat(INVT-CPC-734): Update product inside of inventory (#427)
    JIRA: link to jira ticket
    https://champlainsaintlambert.atlassian.net/browse/CPC-734
    
    What is the ticket about and why are we doing this change.
    
    This pull request is about the backend implementation of the Update
    Product In Inventory feature of the inventory Service.
    
    ## Changes
    - Fully working backend methods UpdateProductInInventory
    - ControllerUnitTest
    - ControllerIntegrationTest
    - ProductRepositoryIntegrationTest
    - ServiceUnitTest

--- inventory-service/src/test/java/com/petclinic/inventoryservice/businesslayer/ProductInventoryServiceUnitTest.java
+++ inventory-service/src/test/java/com/petclinic/inventoryservice/businesslayer/ProductInventoryServiceUnitTest.java
@@ -6,6 +6,7 @@ import com.petclinic.inventoryservice.datalayer.Product.Product;
 import com.petclinic.inventoryservice.datalayer.Product.ProductRepository;
 import com.petclinic.inventoryservice.presentationlayer.InventoryRequestDTO;
 import com.petclinic.inventoryservice.presentationlayer.InventoryResponseDTO;
+import com.petclinic.inventoryservice.presentationlayer.ProductRequestDTO;
 import com.petclinic.inventoryservice.presentationlayer.ProductResponseDTO;
 import com.petclinic.inventoryservice.utils.exceptions.InvalidInputException;
 import com.petclinic.inventoryservice.utils.exceptions.NotFoundException;
@@ -23,6 +24,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -292,6 +294,120 @@ class ProductInventoryServiceUnitTest {
                 .create(productInventoryService.updateInventory(Mono.just(inventoryRequestDTO), invalidInventoryId))
                 .expectError(NotFoundException.class)
                 .verify();
+    }
+
+    @Test
+    void updateProductInInventory_ValidRequest_ShouldUpdateAndReturnProduct() {
+        // Arrange
+        String inventoryId = "1";
+        String productId = UUID.randomUUID().toString();
+
+
+
+        ProductRequestDTO productRequestDTO = ProductRequestDTO.builder()
+                .productName("Updated Product Name")
+                .productPrice(99.99)
+                .productQuantity(20)
+                .build();
+
+        Inventory inventory = Inventory.builder()
+                .id("1")
+                .inventoryId("1")
+                .inventoryType(InventoryType.internal)
+                .inventoryDescription("Medication for procedures")
+                .build();
+
+        Product existingProduct = Product.builder()
+                .id("1")
+                .inventoryId(inventoryId)
+                .productId(productId)
+                .productName("Original Product Name")
+                .productPrice(50.0)
+                .productQuantity(10)
+                .build();
+
+        Product updatedProduct = Product.builder()
+                .id("1")
+                .inventoryId(inventoryId)
+                .productId(productId)
+                .productName("Updated Product Name")
+                .productPrice(99.99)
+                .productQuantity(20)
+                .build();
+
+        when(inventoryRepository.findInventoryByInventoryId(anyString())).thenReturn(Mono.just(inventory));
+
+        when(productRepository.findProductByProductId(anyString())).thenReturn(Mono.just(existingProduct));
+
+        when(productRepository.save(any(Product.class))).thenReturn(Mono.just(updatedProduct));
+
+        // Act and Assert
+        StepVerifier
+                .create(productInventoryService.updateProductInInventory(Mono.just(productRequestDTO), inventoryId, productId))
+                .expectNextMatches(responseDTO -> {
+                    assertNotNull(responseDTO);
+                    assertEquals(productId, responseDTO.getProductId());
+                    assertEquals("Updated Product Name", responseDTO.getProductName());
+                    assertEquals(99.99, responseDTO.getProductPrice());
+                    assertEquals(20, responseDTO.getProductQuantity());
+                    return true;
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void updateProductInInventory_ProductNotFound_ShouldThrowNotFoundException() {
+        // Arrange
+        String inventoryId = "1";
+        String productId = UUID.randomUUID().toString();
+
+        ProductRequestDTO productRequestDTO = ProductRequestDTO.builder()
+                .productName("Updated Product Name")
+                .productPrice(99.99)
+                .productQuantity(20)
+                .build();
+
+        when(inventoryRepository.findInventoryByInventoryId(anyString())).thenReturn(Mono.empty());
+
+        // Act and Assert
+        StepVerifier
+                .create(productInventoryService.updateProductInInventory(Mono.just(productRequestDTO), inventoryId, productId))
+                .expectErrorMatches(throwable -> {
+                    assertEquals("Inventory not found with id: " + inventoryId, throwable.getMessage());
+                    return throwable instanceof NotFoundException;
+                })
+                .verify();
+    }
+
+    @Test
+    void updateProductInInventory_InvalidInput_ShouldThrowInvalidInputException() {
+        // Arrange
+        String inventoryId = "1";
+        String productId = "2";
+
+        ProductRequestDTO productRequestDTO = ProductRequestDTO.builder()
+                .productName("Updated Product Name")
+                .productPrice(-99.99)
+                .productQuantity(20)
+                .build();
+
+        Inventory inventory = Inventory.builder()
+                .id("1")
+                .inventoryId("1")
+                .inventoryType(InventoryType.internal)
+                .inventoryDescription("Medication for procedures")
+                .build();
+
+        Mockito.when(inventoryRepository.findInventoryByInventoryId(inventoryId))
+                .thenReturn(Mono.just(inventory));
+
+        // Act and Assert
+        Throwable exception = assertThrows(InvalidInputException.class, () -> {
+            productInventoryService.updateProductInInventory(Mono.just(productRequestDTO), inventoryId, productId).block();
+        });
+
+        // Assert
+        assertTrue(exception.getMessage().contains("Product price and quantity must be greater than 0."));
     }
 
 
