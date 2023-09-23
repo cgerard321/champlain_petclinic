6d24bfa9fbafce1edfb94af1c53aa5a06369290b
Matthias Boisvert <83222041+mattjulienn@users.noreply.github.com>
2023-09-23 17:24:44 -0400

    feat(CSTM-CPC-854) AddRegisterPage (#433)
    Separating concerns for edit and register pages. Register now has its
    own page, Edit has its own page per usual.
    PLEASE NOTE: 'owners/register' is the new endpoint for registering an
    owner. 'owners/new' might still work, but 'owners/register' is the new
    POST URL.
    
    JIRA:
    https://champlainsaintlambert.atlassian.net/jira/software/c/projects/CPC/boards/14?search=register&selectedIssue=CPC-854
    
    ## Context:
    Separating concerns for Edit and Register Pages.
    'Owner Details' is fully functional and brings you to 'Edit Owner' page,
    all fully working.
    Nav Bar 'Register' brings you to 'Register Owner' page, all fully
    working.
    
    ## Changes
    - Added 'Register Owner' page, accessible from Nav Bar. (owners/register
    instead of owners/new)
    - 'Edit Owner' page is no longer accessible via Nav Bar, but only via
    Edit Owner functionality.
    
    ## Before and After UI (Required for UI-impacting PRs)
    
    ![image](https://github.com/cgerard321/champlain_petclinic/assets/83222041/514c820e-e49e-45d4-a4e2-ebd5985bf502)
    
    ![image](https://github.com/cgerard321/champlain_petclinic/assets/83222041/996d64bc-9fdc-4ec6-b18b-c04ab82fbbe3)
    
    ## Dev notes (Optional)
    - 'owners/register' endpoint is the new 'owners/new' endpoint, please
    take this into consideration when working on your endpoints.
    - BILLS TEAM: I was concerned when trying out the entire PetClinic with
    the new Register Page. When I checked BILLS and clicked "Owner Details",
    the form was blank. At first I thought I may have affected it, but I
    confirmed with two peers who had the most up-to-date version of main and
    it was like that before my branch updates.
    
    ## Linked pull requests (Optional)
    - pull request link

--- api-gateway/src/main/resources/static/scripts/app.js
+++ api-gateway/src/main/resources/static/scripts/app.js
@@ -7,7 +7,7 @@ const whiteList = new Set([
 
 /* App Module */
 var petClinicApp = angular.module('petClinicApp', [
-    'ui.router', 'layoutNav', 'layoutFooter', 'layoutWelcome', 'ownerList', 'ownerDetails', 'ownerForm', /*'ownerRegister',*/ 'petForm'
+    'ui.router', 'layoutNav', 'layoutFooter', 'layoutWelcome', 'ownerList', 'ownerDetails', 'ownerForm', 'ownerRegister', 'petForm'
     , 'visits', 'vetList','vetForm','vetDetails', 'visitList', 'billForm', 'loginForm', 'rolesDetails', 'signupForm',
     'billDetails', 'billsByOwnerId', 'billHistory','billsByVetId','inventoryList','productForm','inventoryProductList','bundleForm'
     , 'verification' , 'adminPanel']);
