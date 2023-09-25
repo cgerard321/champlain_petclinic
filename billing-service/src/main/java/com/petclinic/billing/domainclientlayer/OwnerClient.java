ef248c9b447fad4cead7c03ad64c414c3e4f3d2f
eb07 <EvanBond007@outlook.com>
2023-09-24 23:33:07 -0400

    Commented out the domain client layer as we don't need it for now but could potentially need it later

--- billing-service/src/main/java/com/petclinic/billing/domainclientlayer/OwnerClient.java
+++ billing-service/src/main/java/com/petclinic/billing/domainclientlayer/OwnerClient.java
@@ -1,27 +1,27 @@
-package com.petclinic.billing.domainclientlayer;
-
-import com.petclinic.billing.datalayer.OwnerResponseDTO;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-@Service
-public class OwnerClient {
-    private final WebClient webClient;
-    private final String ownerClientServiceBaseURL;
-
-    private OwnerClient(@Value("${app.customers-service.host}") String ownerServiceHost,
-                        @Value("${app.customers-service.port}") String ownerServicePort) {
-        ownerClientServiceBaseURL = "http://" + ownerServiceHost + ":" + ownerServicePort + "/owners";
-        this.webClient = WebClient.builder()
-                .baseUrl(ownerClientServiceBaseURL).build();
-    }
-    public Mono<OwnerResponseDTO> getOwnerByOwnerId(final String ownerId) {
-        return this.webClient
-                .get()
-                .uri("/{ownerId}", ownerId)
-                .retrieve()
-                .bodyToMono(OwnerResponseDTO.class);
-    }
-}
+//package com.petclinic.billing.domainclientlayer;
+//
+//import com.petclinic.billing.datalayer.OwnerResponseDTO;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.reactive.function.client.WebClient;
+//import reactor.core.publisher.Mono;
+//
+//@Service
+//public class OwnerClient {
+//    private final WebClient webClient;
+//    private final String ownerClientServiceBaseURL;
+//
+//    private OwnerClient(@Value("${app.customers-service.host}") String ownerServiceHost,
+//                        @Value("${app.customers-service.port}") String ownerServicePort) {
+//        ownerClientServiceBaseURL = "http://" + ownerServiceHost + ":" + ownerServicePort + "/owners";
+//        this.webClient = WebClient.builder()
+//                .baseUrl(ownerClientServiceBaseURL).build();
+//    }
+//    public Mono<OwnerResponseDTO> getOwnerByOwnerId(final String ownerId) {
+//        return this.webClient
+//                .get()
+//                .uri("/{ownerId}", ownerId)
+//                .retrieve()
+//                .bodyToMono(OwnerResponseDTO.class);
+//    }
+//}
--- billing-service/src/main/java/com/petclinic/billing/domainclientlayer/VetClient.java
+++ billing-service/src/main/java/com/petclinic/billing/domainclientlayer/VetClient.java
@@ -1,32 +1,32 @@
-package com.petclinic.billing.domainclientlayer;
-
-import com.petclinic.billing.datalayer.VetDTO;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-@Service
-public class VetClient {
-
-    private final WebClient webClient;
-
-    private final String vetClientServiceBaseURL;
-
-    private VetClient(@Value("${app.vet-service.host}") String vetServiceHost,
-                      @Value("${app.vet-service.port}") String vetServicePort) {
-        vetClientServiceBaseURL = "http://" + vetServiceHost + ":" + vetServicePort + "/vets";
-
-        this.webClient = WebClient.builder()
-                .baseUrl(vetClientServiceBaseURL)
-                .build();
-    }
-
-    public Mono<VetDTO> getVetByVetId(final String vetId) {
-        return this.webClient
-                .get()
-                .uri("/{vetId}", vetId)
-                .retrieve()
-                .bodyToMono(VetDTO.class);
-    }
-}
+//package com.petclinic.billing.domainclientlayer;
+//
+//import com.petclinic.billing.datalayer.VetDTO;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.reactive.function.client.WebClient;
+//import reactor.core.publisher.Mono;
+//
+//@Service
+//public class VetClient {
+//
+//    private final WebClient webClient;
+//
+//    private final String vetClientServiceBaseURL;
+//
+//    private VetClient(@Value("${app.vet-service.host}") String vetServiceHost,
+//                      @Value("${app.vet-service.port}") String vetServicePort) {
+//        vetClientServiceBaseURL = "http://" + vetServiceHost + ":" + vetServicePort + "/vets";
+//
+//        this.webClient = WebClient.builder()
+//                .baseUrl(vetClientServiceBaseURL)
+//                .build();
+//    }
+//
+//    public Mono<VetDTO> getVetByVetId(final String vetId) {
+//        return this.webClient
+//                .get()
+//                .uri("/{vetId}", vetId)
+//                .retrieve()
+//                .bodyToMono(VetDTO.class);
+//    }
+//}
