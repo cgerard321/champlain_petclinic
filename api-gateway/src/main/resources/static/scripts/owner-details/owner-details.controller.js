9e0501ee7967a3e56de0351802b30db48dd83075
Bruh <ejcavalheiro@outlook.com>
2023-09-25 02:49:46 -0400

    Patch function works
    Patch function now works on the api-gateway

--- api-gateway/src/main/resources/static/scripts/owner-details/owner-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/owner-details/owner-details.controller.js
@@ -13,31 +13,32 @@ angular.module('ownerDetails')
 
         });
         self.toggleActiveStatus = function (petId) {
-            console.log(petId)
 
             $http.get('api/gateway/pets/' + petId).then(function (resp) {
-                console.log("Pet id is " + $stateParams.petId)
+                console.log("Pet id is " + petId)
                 console.log(resp.data)
                 self.pet = resp.data;
+                console.log("Pet id is " + self.pet.petId)
+                console.log(self.pet)
+                console.log("=====================================")
+                console.log(resp.data)
+                console.log("Active status before is:" + self.pet.isActive)
+                self.pet.isActive = self.pet.isActive === "true" ? "false" : "true";
+                console.log("Active status after is:" + self.pet.isActive)
+
+                $http.patch('api/gateway/pet/' + petId, {
+                    isActive: self.pet.isActive
+                }).then(function (resp) {
+                    console.log("Pet active status updated successfully");
+                    $scope.$apply();
+                }).catch(function (error) {
+                    console.error("Error updating pet active status:", error);
+                    // Handle the error appropriately
+                });
+
 
             });
-            console.log(self.pet)
-            console.log(self.pet.activeStatus)
-            if (self.pet.activeStatus === "true") {
-                self.pet.activeStatus = "false";
-            } else {
-                self.pet.activeStatus = "true";
-            }
-            console.log(self.pet.activeStatus)
-            // Call the API to update the active status
-
-            $http.patch('api/gateway/pet/' + petId, {
-                activeStatus: self.pet.activeStatus
-            }).then(function (resp) {
-                console.log("Pet active status updated successfully");
-            }).catch(function (error) {
-                console.error("Error updating pet active status:", error);
-                // Handle the error appropriately
-            });
+            location.reload();
         };
+
     }]);
