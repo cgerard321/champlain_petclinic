1b5b81951cb7d0d71c244e38ea8b74769d9b92fc
Bruh <ejcavalheiro@outlook.com>
2023-09-25 18:13:56 -0400

    Update owner-details.controller.js
    Troubleshooting the ui, added cache control and a timeout

--- api-gateway/src/main/resources/static/scripts/owner-details/owner-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/owner-details/owner-details.controller.js
@@ -1,20 +1,17 @@
-'use strict';
-
 angular.module('ownerDetails')
-    .controller('OwnerDetailsController', ['$http', '$state', '$stateParams', function ($http, $state, $stateParams) {
+    .controller('OwnerDetailsController', ['$http', '$state', '$stateParams', '$scope', '$timeout', function ($http, $state, $stateParams, $scope, $timeout) {
         var self = this;
         self.owner = {};
         self.pet = {};
 
-        $http.get('api/gateway/owners/' + $stateParams.ownerId).then(function (resp) {
+        $http.get('api/gateway/owners/' + $stateParams.ownerId, { headers: { 'Cache-Control': 'no-cache' } }).then(function (resp) {
             console.log("Owner id is " + $stateParams.ownerId)
             console.log(resp.data)
             self.owner = resp.data;
-
         });
-        self.toggleActiveStatus = function (petId) {
 
-            $http.get('api/gateway/pets/' + petId + '?_=' + new Date().getTime()).then(function (resp) {
+        self.toggleActiveStatus = function (petId) {
+            $http.get('api/gateway/pets/' + petId + '?_=' + new Date().getTime(), { headers: { 'Cache-Control': 'no-cache' } }).then(function (resp) {
                 console.log("Pet id is " + petId)
                 console.log(resp.data)
                 self.pet = resp.data;
@@ -28,16 +25,14 @@ angular.module('ownerDetails')
 
                 $http.patch('api/gateway/pet/' + petId, {
                     isActive: self.pet.isActive
-                }).then(function (resp) {
+                }, { headers: { 'Cache-Control': 'no-cache' } }).then(function (resp) {
                     console.log("Pet active status updated successfully");
                     self.pet = resp.data;
+                    $timeout(); // Manually trigger the $digest cycle to update the UI
                 }).catch(function (error) {
                     console.error("Error updating pet active status:", error);
                     // Handle the error appropriately
                 });
-
-
             });
-            location.reload();
         };
     }]);
