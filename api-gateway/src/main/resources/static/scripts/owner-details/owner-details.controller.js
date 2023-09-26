3b99e73e422f1dfcd50d8ab470c32aba76e868ec
Emil Vezina <vezinaemil@gmail.com>
2023-09-25 20:08:20 -0400

    feat/CSTM-CPC-760_Update_Pet_Details
    Added testing for updatePet and edit pet works but still has bugs

--- api-gateway/src/main/resources/static/scripts/owner-details/owner-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/owner-details/owner-details.controller.js
@@ -1,15 +1,22 @@
 'use strict';
 
 angular.module('ownerDetails')
-    .controller('OwnerDetailsController', ['$http', '$state', '$stateParams', function ($http, $state, $stateParams) {
+    .controller('OwnerDetailsController', ['$http', '$state', '$stateParams', '$q', function ($http, $state, $stateParams, $q) {
         var self = this;
 
 
         $http.get('api/gateway/owners/' + $stateParams.ownerId).then(function (resp) {
-            console.log("Owner id is " + $stateParams.ownerId)
-            console.log(resp.data)
             self.owner = resp.data;
-        });
 
+            var petPromises = self.owner.pets.map(function(pet) {
+                return $http.get('api/gateway/pets/' + pet.petId);
+            });
+
+            $q.all(petPromises).then(function(responses) {
+                self.owner.pets = responses.map(function(response) {
+                    return response.data;
+                });
+            });
+        });
     }]);
 
