9bf149eef19f0faf782f49c347e1997929346dbe
Giuliano <giulianoiantomasi@gmail.com>
2023-09-29 17:31:37 -0400

    add formatter in visitRequestDTO contructor

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Visits/VisitRequestDTO.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Visits/VisitRequestDTO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @AllArgsConstructor
@@ -21,14 +22,14 @@ public class VisitRequestDTO {
     private int month;
     private int day;*/
     private String description;
-    private String petId;
+    private String petId = "1";
     private String practitionerId;
 /*    private int petId;
     private int practitionerId;*/
     private boolean status;
 
     public VisitRequestDTO(LocalDateTime now, String description, String petId, String practitionerId) {
-        this.visitDate = now;
+        this.visitDate = LocalDateTime.parse(now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
         this.description = description;
         this.petId =  petId;
         this.practitionerId = practitionerId;
