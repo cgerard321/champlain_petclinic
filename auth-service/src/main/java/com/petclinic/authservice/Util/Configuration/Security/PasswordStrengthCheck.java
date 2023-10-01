e6c74607e12ad2ec85b9a88cfeba3c18a53521df
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2023-10-01 14:32:32 -0400

    feat(AUTH-CPC-917): Make password check apply to reset password (#491)
    JIRA: [link to jira
    ticket](https://champlainsaintlambert.atlassian.net/browse/CPC-917?atlOrigin=eyJpIjoiMDc5Zjc3NjE3NzliNGMxMmI2NzI0MzYzOWI4YjBjYjQiLCJwIjoiaiJ9)
    ## Context:
    This ticket is to make sure that the users password are strong when they
    are reset through the forgot password process.
    ## Changes
    - Changed package name due to complaint.
    - Changed all auth endpoints in the api-gateway to be reactive.
    - Added the validation check 
    
    ## Before and After UI (Required for UI-impacting PRs)
    The UI is being changed by another teammate. And he is handling what I
    changed.
    
    ![image](https://github.com/cgerard321/champlain_petclinic/assets/71225455/e52540a6-da72-43e8-8485-3afab5d09ddd)

---
+++ auth-service/src/main/java/com/petclinic/authservice/Util/Configuration/Security/PasswordStrengthCheck.java
@@ -0,0 +1,31 @@
+/**
+ * Created by IntelliJ IDEA.
+ * <p>
+ * User: @MaxGrabs
+ * Date: 26/09/21
+ * Ticket: feat(AUTH-CPC-13)
+ */
+package com.petclinic.authservice.Util.Configuration.Security;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//type class
+@Target(ElementType.FIELD)
+//validate during runtime
+@Retention(RetentionPolicy.RUNTIME)
+//constraint
+@Constraint(validatedBy = PasswordValidation.class)
+public @interface PasswordStrengthCheck {
+    String message() default "Invalid Password, password must be at least 8 characters, have 1 digit, lower and upper case letters and a special character.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
