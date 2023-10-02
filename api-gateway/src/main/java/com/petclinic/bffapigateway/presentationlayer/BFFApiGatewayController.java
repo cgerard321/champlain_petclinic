cb16e537d44d6a711ab647100bd38c4cc149c03a
William Chalifoux <william.chalifoux@gmail.com>
2023-10-01 20:34:13 -0400

    Endpoint secured

--- api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/BFFApiGatewayController.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/BFFApiGatewayController.java
@@ -177,6 +177,7 @@ public class BFFApiGatewayController {
     /**
      * Visits Methods
      * **/
+    @SecuredEndpoint(allowedRoles = {Roles.ADMIN})
     @GetMapping(value = "visits", produces= MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<VisitResponseDTO> getAllVisits() {return visitsServiceClient.getAllVisits();}
     @GetMapping(value = "visits/previous/{petId}")
