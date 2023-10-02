03ae9194ab5a29ebf5ae080932bfcae3f1dd2541
Karina Evangelista <karinaevang@hotmail.com>
2023-10-02 13:31:30 -0400

    fixed bug in the front end

--- api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
@@ -152,9 +152,11 @@ angular.module('vetDetails')
             }
         };
 
-
-
         //photo
+        $http.get('api/gateway/vets/photo/' + $stateParams.vetId).then(function (resp) {
+            self.vetPhoto = resp.data;
+        });
+//photo
         $http.get('api/gateway/vets/' + $stateParams.vetId + '/photo').then(function (resp) {
             self.vetPhoto = resp.data;
         });
@@ -164,6 +166,7 @@ angular.module('vetDetails')
                 self.vetPhoto = resp.data;
             });
         }
+
         self.submitRatingForm = function () {
             var rating = {
                 vetId: $stateParams.vetId,
@@ -185,7 +188,6 @@ angular.module('vetDetails')
                 }
                 $http.post("api/gateway/vets/" + $stateParams.vetId + "/ratings", rating)
                     .then(function (resp) {
-                        console.log(resp.data);
                         self.rating = resp.data;
                         alert('Your review was successfully added!');
 
@@ -214,11 +216,44 @@ angular.module('vetDetails')
                             errorMessage = error.data.errors;
                         }
                         alert(errorMessage);
+                        self.educations = resp.data;
                     });
-
-                // clear form with .reset()
                 document.getElementById("ratingForm").reset();
             }
+                    }
+
+        self.submitEducationForm = function (education) {
+            education.vetId = $stateParams.vetId;
+            education.degree = document.getElementById("degree").value;
+            education.schoolName = document.getElementById("schoolName").value;
+            education.fieldOfStudy = document.getElementById("fieldOfStudy").value;
+            education.startDate = document.getElementById("startDate").value;
+            education.endDate = document.getElementById("endDate").value;
+
+            // Send a POST request to add the new education
+            $http.post("api/gateway/vets/" + $stateParams.vetId + "/educations", education)
+                .then(function (resp) {
+                    console.log(resp.data);
+                    self.education = resp.data;
+                    self.addEducationFormVisible = false;
+                    alert('Your education was successfully added!');
+
+                    $http.get('api/gateway/vets/' + $stateParams.vetId + '/educations').then(function (resp) {
+                        console.log(resp.data);
+                        self.educations = resp.data;
+                    });
+                    // clear the form with .reset()
+
+                    document.getElementById("educationForm").reset();
+              
+                    // Refresh the education data
+                    $http.get('api/gateway/vets/' + $stateParams.vetId + '/educations').then(function (resp) {
+                        console.log(resp.data);
+                        self.educations = resp.data;
+                    });
+                }, function (error) {
+                    alert('Error adding education: ' + error.data.message);
+                });
         };
 
         self.submitEducationForm = function (education) {
