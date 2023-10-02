8261ace336f725594df7ac89dfb07e61a46ee569
William Chalifoux <william.chalifoux@gmail.com>
2023-10-02 14:26:41 -0400

    feat(VIST-CPC-820): secured the getAllVisits endpoint to require an Admin account (#498)
    JIRA: [link to jira
    ticket](https://champlainsaintlambert.atlassian.net/browse/CPC-820)
    ## Context:
    Prevents accounts other than admin from viewing the visits list. Visit
    lists are being added for the customer and vets specifically in other
    PR's.
    ## Changes
    Secured the getAllVisits endpoint, requiring an admin account to access
    it.
    Removed some unused imports and commented out code.

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Visits/VisitRequestDTO.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Visits/VisitRequestDTO.java
@@ -8,7 +8,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Data
 @AllArgsConstructor
@@ -18,14 +17,9 @@ public class VisitRequestDTO {
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime visitDate;
-/*   private int year;
-    private int month;
-    private int day;*/
     private String description;
     private String petId;
     private String practitionerId;
-/*    private int petId;
-    private int practitionerId;*/
     private Status status;
 
     public VisitRequestDTO(LocalDateTime now, String description, String petId, String practitionerId) {
