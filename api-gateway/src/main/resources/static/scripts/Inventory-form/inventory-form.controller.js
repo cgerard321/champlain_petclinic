a61c9294b5327117f0cffdd92e0e0320627a1807
Emile Girard <emilegirard42@gmail.com>
2023-10-02 20:29:25 -0400

    Finished front end

--- api-gateway/src/main/resources/static/scripts/Inventory-form/inventory-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/Inventory-form/inventory-form.controller.js
@@ -4,29 +4,64 @@ angular.module('inventoryForm')
     .controller('InventoryFormController', ["$http", '$state', '$stateParams', '$scope', function ($http, $state, $stateParams, $scope) {
         var self = this;
         console.log("State params: " + $stateParams)
-
+        $scope.inventoryTypeFormSearch = "";
+        $scope.inventoryTypeOptions = ["New Type", "Internal", "Sales"] //get all form the inventory type repository but dont remove the New Type
+        $scope.selectedOption = $scope.inventoryTypeOptions[0]
 
         self.submitInventoryForm = function () {
-                var req;
-                req = $http.post("api/gateway/inventory", self.inventory);
-                req.then(function () {
-                    $state.go('inventories');
-                }, function (response) {
-                    var error = response.data;
-                    error.errors = error.errors || [];
-                    alert(error.error + "\r\n" + error.errors.map(function (e) {
-                        return e.field + ": " + e.defaultMessage;
-                    }).join("\r\n"));
-                });
+                var data;
+                if ($scope.selectedOption === "New Type" && $scope.inventoryTypeFormSearch === "") {
+                    alert("Search field cannot be empty when you want to add a new type")
+                }
+                else if ($scope.selectedOption === "New Type") {
+                    $scope.selectedOption = $scope.inventoryTypeFormSearch
 
+                    data = {
+                        inventoryName: self.inventory.inventoryName,
+                        inventoryType: $scope.selectedOption,
+                        inventoryDescription: self.inventory.inventoryDescription
+                    }
+                    console.log($scope.selectedOption)
+                    $http.post("api/gateway/inventory/inventoryType", {"type":$scope.selectedOption})
+                        .then(function (resp) {
+                            $http.post("api/gateway/inventory", data)
+                                .then(function (resp) {
+                                    console.log(resp)
+                                    $state.go('inventories');
+                                }, function (response) {
+                                    var error = response.data;
+                                    error.errors = error.errors || [];
+                                    alert(error.error + "\r\n" + error.errors.map(function (e) {
+                                       return e.field + ": " + e.defaultMessage;
+                                    }).join("\r\n"));
+                                });
+                        })
+                        .catch(function (error) {
+                            alert(error)
+                        })
+                }
+                else {
+                    data = {
+                        inventoryName: self.inventory.inventoryName,
+                        inventoryType: $scope.selectedOption,
+                        inventoryDescription: self.inventory.inventoryDescription
+                    }
+                    $http.post("api/gateway/inventory", data)
+                        .then(function (resp) {
+                            console.log(resp)
+                            $state.go('inventories');
+                        }, function (response) {
+                            var error = response.data;
+                            error.errors = error.errors || [];
+                            alert(error.error + "\r\n" + error.errors.map(function (e) {
+                               return e.field + ": " + e.defaultMessage;
+                            }).join("\r\n"));
+                        });
+                }
             };
-        $scope.inventoryTypeFormSearch = "";
-        $scope.inventoryTypeOptions = ["Internal", "Sales"];
-        $scope.selectedOption = "";
-
         $scope.updateOption = function() {
             var searchLowerCase = $scope.inventoryTypeFormSearch.toLowerCase();
-            $scope.selectedOption = "";
+            $scope.selectedOption = $scope.inventoryTypeOptions[0];
             for (var i = 0; i < $scope.inventoryTypeOptions.length; i++) {
                 var optionLowerCase = $scope.inventoryTypeOptions[i].toLowerCase();
                 if (optionLowerCase.indexOf(searchLowerCase) !== -1) {
