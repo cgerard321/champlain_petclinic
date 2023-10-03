6d7a106faff72abc96b0ea9cf532dbfcb75f71b7
Timbro <alexei.timbro@gmail.com>
2023-10-03 14:34:24 -0400

    Setting up the update form.

---
+++ api-gateway/src/main/resources/static/scripts/inventory-product-update-form/inventory-product-form.component.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('inventoryProductUpdateForm')
+    .component('inventoryProductForm', {
+        templateUrl: 'scripts/inventory-product-update-form/inventory-product-form.template.html',
+        controller: 'InventoryProductUpdateFormController'
+    });
