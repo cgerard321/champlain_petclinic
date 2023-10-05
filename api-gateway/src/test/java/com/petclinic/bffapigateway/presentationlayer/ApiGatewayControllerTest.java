9756feccac0fde632d14309a2d80bd5c77e496ae
Matthias Boisvert <83222041+mattjulienn@users.noreply.github.com>
2023-10-05 13:10:34 -0400

    feat/CSTM-CPC-966-AddProvinceToOwner (#516)
    JIRA: [link to jira
    ticket](https://champlainsaintlambert.atlassian.net/browse/CPC-966)
    
    ## Context:
    
    Added Province to Owner Entity. Updated the Front-end. Updated all tests
    regarding Owners.
    
    ## Changes
    
    - Added Province to Owner Entity
    - Updated Front-End to ensure seamless integration with the new field,
    Province.
    - Updated AUTH to ensure Province is properly passed when creating an
    Owner
    - All tests regarding Owner were updated / AUTH tests regarding Owner
    were updated
    
    ## Before and After UI (Required for UI-impacting PRs)
    
    BEFORE
    
    ![image](https://github.com/cgerard321/champlain_petclinic/assets/83222041/e05bf4c2-155d-4150-aad2-d0aafa0a38f0)
    
    AFTER
    
    ![image](https://github.com/cgerard321/champlain_petclinic/assets/83222041/a84b3a82-284f-40d2-b89b-33291aec122e)
    
    ## Dev notes (Optional)
    
    Did you know, a bird's lungs are much more complicated and efficient and
    take up more space than those of mammals, such as humans. A human's
    lungs compose about 1/20 of its body, but a bird's takes up 1/5 !!

--- api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/ApiGatewayControllerTest.java
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/ApiGatewayControllerTest.java
@@ -889,6 +889,7 @@ class ApiGatewayControllerTest {
                 .lastName("Johnny")
                 .address("111 John St")
                 .city("Johnston")
+                .province("Quebec")
                 .telephone("51451545144")
                 .build();
 
@@ -900,6 +901,7 @@ class ApiGatewayControllerTest {
                 .lastName("Johnny")
                 .address("111 John St")
                 .city("Johnston")
+                .province("Quebec")
                 .telephone("51451545144")
                 .build();
 
@@ -929,6 +931,7 @@ class ApiGatewayControllerTest {
                     assertEquals(dto.getLastName(),owner.getLastName());
                     assertEquals(dto.getAddress(),owner.getAddress());
                     assertEquals(dto.getCity(),owner.getCity());
+                    assertEquals(dto.getProvince(),owner.getProvince());
                     assertEquals(dto.getTelephone(),owner.getTelephone());
                 });
 
@@ -1026,6 +1029,7 @@ class ApiGatewayControllerTest {
         owner1.setLastName("Johnny");
         owner1.setAddress("111 John St");
         owner1.setCity("Johnston");
+        owner1.setProvince("Quebec");
         owner1.setTelephone("51451545144");
 
         Flux<OwnerResponseDTO> ownerResponseDTOFlux = Flux.just(owner1);
@@ -1058,6 +1062,7 @@ class ApiGatewayControllerTest {
         owner.setLastName("Test");
         owner.setAddress("Test");
         owner.setCity("Test");
+        owner.setProvince("Test");
         owner.setTelephone("Test");
 
         Optional<Integer> page = Optional.of(0);
@@ -1108,6 +1113,7 @@ class ApiGatewayControllerTest {
         owner.setLastName("Johnny");
         owner.setAddress("111 John St");
         owner.setCity("Johnston");
+        owner.setProvince("Quebec");
         owner.setTelephone("51451545144");
         when(customersServiceClient.getOwner("ownerId-123"))
                 .thenReturn(Mono.just(owner));
