044a83bfcfc42413073841c4ecae9c4b34948a48
Matthewtan9 <matthew999099@gmail.com>
2023-10-05 23:46:59 -0400

    A bit of testing

--- inventory-service/src/test/java/com/petclinic/inventoryservice/businesslayer/ProductInventoryServiceUnitTest.java
+++ inventory-service/src/test/java/com/petclinic/inventoryservice/businesslayer/ProductInventoryServiceUnitTest.java
@@ -815,5 +815,27 @@ class ProductInventoryServiceUnitTest {
                 })
                 .verifyComplete();
     }
+//search
+    @Test
+    void testSearchInventories_TypeAndDescriptionGiven() {
+        // Given
+        String testType = "Internal";
+        String testDescription = "Medication for procedures";
+
+        when(inventoryRepository.findAllByInventoryTypeAndInventoryDescription(testType, testDescription))
+                .thenReturn(Flux.just(inventory));
+
+        // When
+        Flux<InventoryResponseDTO> result = productInventoryService.searchInventories(null, testType, testDescription);
+
+        // Then
+        StepVerifier.create(result)
+                .expectNextMatches(dto -> dto.getInventoryType().equals(testType) && dto.getInventoryDescription().equals(testDescription))
+                .verifyComplete();
+
+        verify(inventoryRepository, times(1))
+                .findAllByInventoryTypeAndInventoryDescription(testType, testDescription);
+    }
+
 
 }
