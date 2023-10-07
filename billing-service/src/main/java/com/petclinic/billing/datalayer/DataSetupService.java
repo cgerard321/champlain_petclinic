ed5c871ccef360dd5e804a027c1cdcd535299e1f
MinseokBUZZ <minseokshim9@gmail.com>
2023-10-07 13:38:22 -0400

    Fixed small error for due date in DataSetupService

--- billing-service/src/main/java/com/petclinic/billing/datalayer/DataSetupService.java
+++ billing-service/src/main/java/com/petclinic/billing/datalayer/DataSetupService.java
@@ -24,7 +24,7 @@ public class DataSetupService implements CommandLineRunner {
 
         BillRequestDTO b1 = new BillRequestDTO( "1", "general", "1", LocalDate.of(2023,9,19),59.99,BillStatus.PAID, LocalDate.of(2023, 10,3));
         BillRequestDTO b3 = new BillRequestDTO( "3", "operation", "1", LocalDate.of(2023,9,21), 199.99,BillStatus.PAID,LocalDate.of(2023, 10,5));
-        BillRequestDTO b4 = new BillRequestDTO( "4", "injury", "1",  LocalDate.of(2023,9,30), 199.99,BillStatus.UNPAID,LocalDate.of(2023, 10,10));
+        BillRequestDTO b4 = new BillRequestDTO( "4", "injury", "1",  LocalDate.of(2023,9,30), 199.99,BillStatus.UNPAID,LocalDate.of(2023, 10,14));
         BillRequestDTO b2 = new BillRequestDTO( "2", "operation", "2", LocalDate.of(2023,9,20), 199.99,BillStatus.OVERDUE,LocalDate.of(2023, 10,4));
         BillRequestDTO b5 = new BillRequestDTO( "5", "chronic", "3",  LocalDate.of(2023,10,3), 199.99,BillStatus.UNPAID,LocalDate.of(2023, 10,17));
 
