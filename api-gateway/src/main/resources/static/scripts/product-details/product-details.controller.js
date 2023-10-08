c98528a68359fc0cc3ba6140bb9a0d155a90cc99
Abdur Siddiqui <abdursiddiqui50@gmail.com>
2023-10-08 16:42:09 -0400

    the details page shows information about product

--- api-gateway/src/main/resources/static/scripts/product-details/product-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/product-details/product-details.controller.js
@@ -1,22 +1,22 @@
 'use strict';
 
 angular.module('productDetails')
-    .controller('ProductDetailsController', ['$http', '$stateParams', function ($http, $stateParams) {
+    .controller('ProductDetailsController', ["$http", '$state', '$stateParams', '$scope', 'InventoryService', function ($http, $state, $stateParams, $scope, InventoryService) {
         var self = this;
         self.product = {}; // Initialize self.product
-        var inventoryId = $stateParams.inventoryId || "";
-        var productId = $stateParams.productId || "";
+        var inventoryId = InventoryService.getInventoryId();
+        var productId = $stateParams.productId;
 
-            $http.get('api/gateway/inventory/' + $stateParams.inventoryId + '/products/' + productId)
-                .then(function (resp) {
-                    // Handle the response data for the specific product
-                    var product = resp.data;
-                    console.log("Product found:", product);
-                    self.product = product; // Update the product data in your controller
-                })
-                .catch(function (error) {
-                    // Handle errors if the product is not found or other issues
-                    console.error("Error fetching product:", error);
-                });
+        $http.get('/api/gateway/inventory/' + inventoryId + '/products/' + productId)
+            .then(function (resp) {
+                // Handle the response data for the specific product
+                var product = resp.data;
+                console.log("Product found:", product);
+                self.product = product; // Update the product data in your controller
+            })
+            .catch(function (error) {
+                // Handle errors if the product is not found or other issues
+                console.error("Error fetching product:", error);
+            });
 
     }]);
