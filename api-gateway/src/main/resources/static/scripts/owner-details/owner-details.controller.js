2885fa59c2d0e21cfb20e0b1f4ab52930d8c4fe5
Emil Vezina <40618145+1-emil@users.noreply.github.com>
2023-10-10 11:22:17 -0400

    Bug(CSTM-CPC-879): fix pet edit to edit all values (#538)
    JIRA: [link to jira
    ticket](https://champlainsaintlambert.atlassian.net/browse/CPC-879)
    ## Context:
    Making It so the customer can edit the Name, Birthdate, and Pet Type.
    Previously you could only change the name.
    ## Changes
    
    - Fixed Bug where the pet info in the form would dissapear
    - Added birthdate and pet type to owner details
    - Made birthdate and pet type editable
    
    ## Before and After UI (Required for UI-impacting PRs)
    <img width="1440" alt="image"
    src="https://github.com/cgerard321/champlain_petclinic/assets/40618145/8bd0dae3-3f67-4342-8417-4937cda94c1c">
    <img width="1440" alt="2"
    src="https://github.com/cgerard321/champlain_petclinic/assets/40618145/ab9498a5-8177-4096-b546-93b65c3ae690">
    
    ---------
    
    Co-authored-by: Eduardo J.C <ejcavalheiro@outlook.com>

--- api-gateway/src/main/resources/static/scripts/owner-details/owner-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/owner-details/owner-details.controller.js
@@ -11,65 +11,46 @@ function OwnerDetailsController($http, $state, $stateParams, $scope, $timeout, $
     vm.pet = {};
     vm.pets = [];
 
+    // Function to get pet type name based on petTypeId
+    vm.getPetTypeName = function (petTypeId) {
+        switch (petTypeId) {
+            case '1':
+                return 'Cat';
+            case '2':
+                return 'Dog';
+            case '3':
+                return 'Lizard';
+            case '4':
+                return 'Snake';
+            case '5':
+                return 'Bird';
+            case '6':
+                return 'Hamster';
+            default:
+                return 'Unknown';
+        }
+    };
+
     // Fetch owner data
     $http.get('api/gateway/owners/' + $stateParams.ownerId)
         .then(function (resp) {
             vm.owner = resp.data;
             console.log(vm.owner);
+
+            var petPromises = vm.owner.pets.map(function (pet) {
+                return $http.get('api/gateway/pets/' + pet.petId, { cache: false });
+            });
+
+            $q.all(petPromises).then(function (responses) {
+                vm.owner.pets = responses.map(function (response) {
+                    return response.data;
+                });
+            });
         })
         .catch(function (error) {
             console.error('Error fetching owner data:', error);
         });
 
-    // Fetch associated pets and their details
-    $http.get(`api/gateway/owners/${$stateParams.ownerId}/pets`)
-        .then(function (response) {
-            // Split the response by newline characters to get individual pet objects
-            var petResponses = response.data.split('\n');
-
-// Parse each pet response as JSON, remove the "data:" prefix, and trim any leading/trailing whitespace
-            var petObjects = petResponses.map(function (petResponse) {
-                // Remove the "data:" prefix and trim any leading/trailing whitespace
-                var trimmedResponse = petResponse.replace(/^data:/, '').trim();
-                console.log("Trimmed results: ", trimmedResponse)
-
-                // Check if the trimmed response is empty
-                if (!trimmedResponse) {
-                    return null; // Skip empty responses
-                }
-
-                try {
-                    return JSON.parse(trimmedResponse);
-                } catch (error) {
-                    console.error('Error parsing pet response:', error);
-                    return null;
-                }
-            });
-
-// Filter out any parsing errors (null values)
-            petObjects = petObjects.filter(function (pet) {
-                return pet !== null;
-            });
-
-
-            // Assuming that each pet has a 'petId' property, you can create an array of promises to fetch detailed pet data
-            var petPromises = petObjects.map(function (pet) {
-                return $http.get(`api/gateway/pets/${pet.petId}`);
-            });
-
-            return $q.all(petPromises);
-        })
-        .then(function (responses) {
-            vm.pets = responses.map(function (response) {
-                return response.data;
-            });
-            console.log("Pet Array:", vm.pets);
-        })
-        .catch(function (error) {
-            console.error('Error fetching pet data:', error);
-        });
-
-    // Toggle pet's active status
     vm.toggleActiveStatus = function (petId) {
         $http.get('api/gateway/pets/' + petId + '?_=' + new Date().getTime(), { headers: { 'Cache-Control': 'no-cache' } })
             .then(function (resp) {
