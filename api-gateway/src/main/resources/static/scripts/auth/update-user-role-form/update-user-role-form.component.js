8795836a6ae0e533b18e9968b1c5830e8f80e298
SamuelNguyen2121 <2032373@champlaincollege.qc.ca>
2023-10-10 20:19:23 -0400

    Removed front end code for backend PR

--- api-gateway/src/main/resources/static/scripts/auth/update-user-role-form/update-user-role-form.component.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('updateUserRoleForm')
-    .component('updateUserRoleForm', {
-        templateUrl: 'scripts/auth/update-user-role-form/update-user-role-form.template.html',
-        controller: 'UpdateUserRoleFormController'
-    });
