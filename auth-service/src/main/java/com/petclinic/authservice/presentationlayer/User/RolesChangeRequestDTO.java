fd71be749a76a13062d4fbe7e7a77afdbaaaf6f7
SamuelNguyen2121 <2032373@champlaincollege.qc.ca>
2023-10-11 19:50:02 -0400

    Removed unused class that prevents build

--- auth-service/src/main/java/com/petclinic/authservice/presentationlayer/User/RolesChangeRequestDTO.java
+++ auth-service/src/main/java/com/petclinic/authservice/presentationlayer/User/RolesChangeRequestDTO.java
@@ -1,6 +1,5 @@
 package com.petclinic.authservice.presentationlayer.User;
 
-import com.petclinic.authservice.security.Roles;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
