b5cb5ac19634345f70e90d540f765cade932e16a
Nick Mazza <nick@mazzafamily.ca>
2023-10-21 04:09:23 -0400

    Finishing touches
    End Result mimics the End Product Goal

--- api-gateway/src/main/resources/static/scripts/pet-details/pet-details.controller.js
+++ api-gateway/src/main/resources/static/scripts/pet-details/pet-details.controller.js
@@ -19,4 +19,37 @@ function PetDetailsController($http, $state, $stateParams, $scope, $timeout, $q)
             console.error('Error fetching pet data:', error);
         });
 
+
+    // Function to get pet type name based on petTypeId
+    vm.getPetTypeName = function (petTypeId) {
+        switch (petTypeId) {
+            case '1':
+                return 'Cat';
+            case '2':
+                return 'Dog';
+            case '3':
+                return 'Lizard';
+            case '4':
+                return 'Snake';
+            case '5':
+                return 'Bird';
+            case '6':
+                return 'Hamster';
+            default:
+                return 'Unknown';
+        }
+    };
+
+    vm.getBirthday = function(birthday) {
+        if (birthday) {
+            var date = new Date(birthday);
+            var year = date.getUTCFullYear();
+            var month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); // Months are zero-based, so we add 1
+            var day = date.getUTCDate().toString().padStart(2, '0');
+            return year + ' / ' + month + ' / ' + day;
+        } else {
+            return '';
+        }
+    };
+
 }
