4dc3823744cc1d83cd38cdc7531167dc32a337a9
Emil Vezina <vezinaemil@gmail.com>
2023-10-22 01:48:24 -0400

    Added weight to frontend
    Updated confirmation and added a text box to input weight and fixed a small bug where pet status disappeared after editing pet

--- api-gateway/src/main/resources/static/scripts/pet-form/pet-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/pet-form/pet-form.controller.js
@@ -67,14 +67,16 @@ angular.module('petForm')
             if (confirm("Are you sure you want to submit this form with the following details?\n\n" +
                 "Pet Name: " + self.pet.name + "\n" +
                 "Pet Birth Date: " + formattedBirthDate + "\n" +
-                "Pet Type: " + petTypeName + "\n" +
-                "Weight: " + self.pet.weight)) {
+                "Weight: " + self.pet.weight + " KG" + "\n" +
+                "Pet Type: " + petTypeName)) {
                 var data = {
                     petId: self.pet.petId,
                     name: self.pet.name,
                     birthDate: new Date(self.pet.birthDate).toISOString(),
                     ownerId: self.pet.ownerId,
-                    petTypeId: self.pet.petTypeId
+                    petTypeId: self.pet.petTypeId,
+                    weight: self.pet.weight,
+                    isActive: self.pet.isActive
                 };
 
                 var req;
