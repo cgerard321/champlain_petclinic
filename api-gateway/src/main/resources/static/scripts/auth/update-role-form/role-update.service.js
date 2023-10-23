4371189c76abc4adb8136350ed2a466012649468
SamuelNguyen2121 <102634920+SamuelNguyen2121@users.noreply.github.com>
2023-10-23 16:58:49 -0400

    feat(AUTH-CPC-755): Add More Roles Front End (#573)
    JIRA: link to jira ticket
    https://champlainsaintlambert.atlassian.net/browse/CPC-755
     
    ## Context:
    What is the ticket about and why are we doing this change.
    This ticket is about adding three roles, and one of them to be assigned
    to users because we want them to have a restricted access in the system.
    It is also about the feature for the admin to update the users' role if
    ever a user needs a different permission on the website.
    
    ## Changes
    What are the various changes and what other modules do those changes
    effect.
    This can be bullet point or sentence format.
    
    - A new column in the admin panel called "Role" to see the users'
    current role assigned
    - A new button in the admin panel called "Update Role" to navigate to
    the update user role form
    - A new form for updating a user's role with list of availble roles to
    change
    
    ## Before and After UI (Required for UI-impacting PRs)
    If this is a change to the UI, include before and after screenshots to
    show the differences.
    If this is a new UI feature, include screenshots to show reviewers what
    it looks like.
    
    New "Update Role" Button and New "Role" Column in Admin Panel
    
    ![image](https://github.com/cgerard321/champlain_petclinic/assets/102634920/8b06d53d-b35f-4b64-b507-2f30cb75ac41)New
    Update User Role Form (After Clicking Update Role Button)
    
    ![image](https://github.com/cgerard321/champlain_petclinic/assets/102634920/8195f48b-95ec-42c1-9044-da3d3e892195)
    
    ## Dev notes (Optional)
    - Specific technical changes that should be noted
    ## Linked pull requests (Optional)
    - pull request link

---
+++ api-gateway/src/main/resources/static/scripts/auth/update-role-form/role-update.service.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('userModule').service('UserService', [function() {
+    this.getAvailableRoles = function() {
+        return ['ADMIN', 'VET', 'OWNER'];
+    };
+
+}]);
