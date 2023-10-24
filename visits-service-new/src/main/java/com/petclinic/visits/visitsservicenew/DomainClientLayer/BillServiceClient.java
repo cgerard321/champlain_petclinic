0b3aba9c83a33831fbde5f1620f4f7d815e4b9ee
CptDoritos <fred.vezina@hotmail.com>
2023-10-23 22:44:04 -0400

    Fixed Database

--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/BillServiceClient.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DomainClientLayer/BillServiceClient.java
@@ -1,25 +1,45 @@
 package com.petclinic.visits.visitsservicenew.DomainClientLayer;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
-@Service
+@Component
 public class BillServiceClient {
-    private final WebClient webClient;
+
+    private final WebClient.Builder webClientBuilder;
+    private final String billServiceUrl;
+
+
     public BillServiceClient(
+            WebClient.Builder webClientBuilder,
             @Value("${app.billing-service.host}") String billingServiceHost,
             @Value("${app.billing-service.port}") String billingServicePort
     ) {
-//        this.webClient = WebClient.builder().baseUrl("http://" + billingServiceHost + ":" + billingServicePort).build();
-        this.webClient = WebClient.builder().baseUrl("http://" + "localhost" + ":" + "7004").build();
+        this.webClientBuilder = webClientBuilder;
+
+        billServiceUrl = "http://" + billingServiceHost + ":" + billingServicePort + "/bills";
+
     }
+
+    public Mono<BillResponseDTO> getBilling(final String billId) {
+        return webClientBuilder.build().get()
+                .uri(billServiceUrl + "/{billId}", billId)
+                .retrieve()
+                .bodyToMono(BillResponseDTO.class);
+    }
+
     public Flux<BillResponseDTO> getAllBilling() {
-        return webClient.get()
-                .uri("/bills")
+        return webClientBuilder.build().get()
+                .uri(billServiceUrl)
                 .retrieve()
                 .bodyToFlux(BillResponseDTO.class);
     }
 }
+
+
