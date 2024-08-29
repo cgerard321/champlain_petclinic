25ee9b352c20d6216b4c5e060aed4b317836d6d0
dylanbrass <dylan.brassard@outlook.com>
2024-08-29 14:21:59 -0400

    Done

--- api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/v2/InventoryController.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/v2/InventoryController.java
@@ -5,7 +5,9 @@ import com.petclinic.bffapigateway.dtos.Inventory.InventoryResponseDTO;
 import com.petclinic.bffapigateway.dtos.Inventory.InventoryTypeResponseDTO;
 import com.petclinic.bffapigateway.utils.Security.Annotations.SecuredEndpoint;
 import com.petclinic.bffapigateway.utils.Security.Variables.Roles;
-import io.swagger.annotations.ResponseHeader;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -24,15 +26,16 @@ import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:3000, http://localhost:80")
 public class InventoryController {
     private final InventoryServiceClient inventoryServiceClient;
-    @SecuredEndpoint(allowedRoles = {Roles.ADMIN,Roles.INVENTORY_MANAGER,Roles.VET})
+
+    @SecuredEndpoint(allowedRoles = {Roles.ADMIN, Roles.INVENTORY_MANAGER, Roles.VET})
     @GetMapping()//, produces= MediaType.TEXT_EVENT_STREAM_VALUE)
-    @ResponseHeader(name = "Search Inventory & Get All Inventories pagination", description = "Total number of items available")
+    @ApiResponses(value = {@ApiResponse(useReturnTypeSchema = true, description = "Total number of items available", responseCode = "200")})
     public ResponseEntity<Flux<InventoryResponseDTO>> searchInventory(@RequestParam Optional<Integer> page,
-                                                      @RequestParam Optional<Integer> size,
-                                                      @RequestParam(required = false) String inventoryName,
-                                                      @RequestParam(required = false) String inventoryType,
-                                                      @RequestParam(required = false) String inventoryDescription){
-        if(page.isEmpty()){
+                                                                      @RequestParam Optional<Integer> size,
+                                                                      @RequestParam(required = false) String inventoryName,
+                                                                      @RequestParam(required = false) String inventoryType,
+                                                                      @RequestParam(required = false) String inventoryDescription) {
+        if (page.isEmpty()) {
             page = Optional.of(0);
         }
 
@@ -42,28 +45,28 @@ public class InventoryController {
         return ResponseEntity.ok().body(inventoryServiceClient.searchInventory(page, size, inventoryName, inventoryType, inventoryDescription));
     }
 
-    @SecuredEndpoint(allowedRoles = {Roles.ADMIN,Roles.INVENTORY_MANAGER})
+    @SecuredEndpoint(allowedRoles = {Roles.ADMIN, Roles.INVENTORY_MANAGER})
     @GetMapping(value = "/types")
-    @ResponseHeader(name = "Get All Inventory Types", description = "All available inventory types")
-    public ResponseEntity<Flux<InventoryTypeResponseDTO>> getAllInventoryTypes(){
+    @ApiResponses(value = {@ApiResponse(description = "All available inventory types", responseCode = "200")})
+    public ResponseEntity<Flux<InventoryTypeResponseDTO>> getAllInventoryTypes() {
         return ResponseEntity.ok().body(inventoryServiceClient.getAllInventoryTypes());
     }
+
     @DeleteMapping(value = "/{inventoryId}")
-    @SecuredEndpoint(allowedRoles = {Roles.ADMIN,Roles.INVENTORY_MANAGER})
-    @ResponseHeader(name = "Delete Inventory by Inventory Id", description = "Deletes an inventory by inventory id")
+    @SecuredEndpoint(allowedRoles = {Roles.ADMIN, Roles.INVENTORY_MANAGER})
+    @ApiResponses(value = {@ApiResponse(description = "Deletes an inventory by inventory id", responseCode = "204"), @ApiResponse(description = "Deletes an inventory by invalid inventory id", responseCode = "404")})
     public Mono<ResponseEntity<Void>> deleteInventoryByInventoryId(@PathVariable String inventoryId) {
         try {
             return inventoryServiceClient.deleteInventoryByInventoryId(inventoryId).then(Mono.just(ResponseEntity.noContent().<Void>build()))
                     .defaultIfEmpty(ResponseEntity.notFound().build());
         } catch (Exception e) {
-            e.printStackTrace();
             return Mono.just(ResponseEntity.notFound().build());
         }
     }
 
-    @SecuredEndpoint(allowedRoles = {Roles.ADMIN,Roles.INVENTORY_MANAGER})
+    @SecuredEndpoint(allowedRoles = {Roles.ADMIN, Roles.INVENTORY_MANAGER})
     @DeleteMapping(value = "")
-    @ResponseHeader(name = "Delete All Inventories", description = "Deletes all inventories")
+    @ApiResponses(value = {@ApiResponse(useReturnTypeSchema = true, description = "Deletes all inventories", responseCode = "204")})
     public Mono<ResponseEntity<Void>> deleteAllInventories() {
         return inventoryServiceClient.deleteAllInventories().then(Mono.just(ResponseEntity.noContent().<Void>build()))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
