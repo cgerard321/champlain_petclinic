6ca1f3d50b45558234d166f41fc9e0b8cd7ec1ce
volking <vik.kuts@hotmail.com>
2024-09-09 18:43:49 -0400

    Spring Init & Docker Config

---
+++ products-service/src/main/java/com/petclinic/products/ProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+
+}
