2afc1dee4b7f37fd6ad627bb9a9ded4a975c3a2b
Valentine Nneji <ama.val100@gmail.com>
2024-09-11 11:30:26 -0400

    Finished configuring the products-service and setup the api-gateway for it

---
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Products/ProductRequestDTO.java
@@ -0,0 +1,17 @@
+package com.petclinic.bffapigateway.dtos.Products;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequestDTO {
+
+    private String productName;
+    private String productDescription;
+    private Double productSalePrice;
+}
