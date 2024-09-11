ec4c70baf6884b08b120d6312e332b659284d560
Valentine Nneji <ama.val100@gmail.com>
2024-09-11 13:27:23 -0400

    Fixed a problem with the tests

--- products-service/src/test/java/com/petclinic/products/ProductsServiceApplicationTests.java
+++ products-service/src/test/java/com/petclinic/products/ProductsServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.petclinic.products;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductsServiceApplicationTests {
 
 	@Test
