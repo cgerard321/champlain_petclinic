0050dd276b4d61b9e09aa437c0f956064fdd0f3e
Mariya Zolovska <132389537+mzolovska@users.noreply.github.com>
2024-09-12 11:25:07 -0400

    conf/CART-CPC-1088_Initialize_Cart_Service (#616)
    Created the cart service
    
    JIRA:
    (https://champlainsaintlambert.atlassian.net/jira/software/c/projects/CPC/boards/15/backlog?selectedIssue=CPC-1088)
    ## Context:
    Created the cart service for further use for my team
    ## Changes
    Added a new service, changed the docket-compose.yml by adding the
    service
    ## Dev notes (Optional)
    Try if it builds on your side
    
    ---------
    
    Co-authored-by: volking <vik.kuts@hotmail.com>

---
+++ cart-service/src/main/java/com/petclinic/cartsservice/CartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartServiceApplication.class, args);
+    }
+
+}
---
+++ cart-service/src/test/java/com/petclinic/cartsservice/CartServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
