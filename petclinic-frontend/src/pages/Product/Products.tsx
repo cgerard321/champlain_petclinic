02e2278b9eaf896475a5bed40221327eeba5e606
Zachary Lelievre <lelievrezachary@gmail.com>
2024-09-11 23:45:38 -0400

    The product page was added including the nav bar link for the product page

---
+++ petclinic-frontend/src/pages/Product/Products.tsx
@@ -0,0 +1,11 @@
+import {NavBar} from "@/layouts/AppNavBar.tsx";
+
+
+export default function Products():JSX.Element {
+    return (
+        <div>
+            <NavBar/>
+        <h1>Products</h1>
+        </div>
+    );
+}
