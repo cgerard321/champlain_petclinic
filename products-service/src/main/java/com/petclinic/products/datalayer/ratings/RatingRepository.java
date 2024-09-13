831e8fd21cc3eff74b089311843849a4fab2cced
volking <vik.kuts@hotmail.com>
2024-09-13 00:36:27 -0400

    Added averageRating field to Product, auto-updates average

--- products-service/src/main/java/com/petclinic/products/datalayer/ratings/RatingRepository.java
+++ products-service/src/main/java/com/petclinic/products/datalayer/ratings/RatingRepository.java
@@ -7,4 +7,5 @@ import reactor.core.publisher.Mono;
 public interface RatingRepository extends ReactiveMongoRepository<Rating, String>{
     Flux<Rating> findRatingsByProductId(String productId);
     Mono<Rating> findRatingByCustomerIdAndProductId(String customerId, String productId);
+    Flux<Rating> deleteRatingsByProductId(String productId);
 }
