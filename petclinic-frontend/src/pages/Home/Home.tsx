2a971a493a59c55469eed6ad6e4cc070b56346ab
kyrusho <131713245+kyrusho@users.noreply.github.com>
2024-09-12 22:35:56 -0400

    feat(VETS-CPC-1110): make veterinarian and home page accessible. (#619)
    JIRA: link to jira ticket
    ## Context:
    What is the ticket about and why are we doing this change.
    ## Changes
    What are the various changes and what other modules do those changes
    effect.
    This can be bullet point or sentence format.
    ## Before and After UI (Required for UI-impacting PRs)
    If this is a change to the UI, include before and after screenshots to
    show the differences.
    If this is a new UI feature, include screenshots to show reviewers what
    it looks like.
    ## Dev notes (Optional) 
    ##### (The next part is an addition from @nic5694 to help with the
    issues that some people have been running into in the past couple of
    days)
    Added workspace configurations to VS Code because some people were
    missing plugins needed to see linting errors they might introduce. This
    update includes settings for format on save—when you modify a file and
    save it, VS Code will automatically format the code for you.
    
    **Important**: If you already have a .vscode folder in the root of the
    petclinic-frontend folder, remove it before pulling from main, or you’ll
    get a conflict. It’s best that everyone uses this configuration to
    ensure smoother development and avoid build failures in CI.
    
    **Extension Recommendations**:
    Added recommended extensions that you’ll find at the bottom of the
    Extensions Marketplace in VS Code. Please **install the following**:
            Prettier: For consistent code formatting.
            ESLint: To catch linting errors.
    "try-material-icon-theme": To make it easier to navigate the folder
    structure with icons.
    
    These tools will help you spot issues early and streamline the
    development process.
    ## Linked pull requests (Optional)
    - pull request link
    
    ---------
    
    Co-authored-by: nic5694 <nicholasmartoccia04@icloud.com>

---
+++ petclinic-frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from '@/layouts/AppNavBar.tsx';
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <NavBar />
+      <h1>Welcome to pet clinic</h1>
+    </div>
+  );
+}
--- petclinic-frontend/src/router.tsx
+++ petclinic-frontend/src/router.tsx
@@ -2,12 +2,22 @@ import { createBrowserRouter } from 'react-router-dom';
 import { AppRoutePaths } from './shared/models/path.routes';
 import Login from './pages/Login/Login';
 import Inventories from '@/pages/Inventory/Inventories.tsx';
+import Vet from '@/pages/Vet/Vet.tsx';
 import { ProtectedRoute } from '@/shared/components/ProtectedRouteProps.tsx';
+import Home from '@/pages/Home/Home.tsx';
 
 const router = createBrowserRouter([
   {
     path: AppRoutePaths.Default,
     children: [
+      {
+        path: AppRoutePaths.Default,
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
+      },
       {
         path: AppRoutePaths.Inventories,
         element: (
@@ -16,7 +26,14 @@ const router = createBrowserRouter([
           </ProtectedRoute>
         ),
       },
-      //   {
+      {
+        path: AppRoutePaths.Vet,
+        element: (
+          <ProtectedRoute>
+            <Vet />
+          </ProtectedRoute>
+        ),
+      }, //   {
       //       path: AppRoutePaths.PageNotFound,
       //       element: /* PageNotFoundComponent */
       //   },
