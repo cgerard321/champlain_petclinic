020c3939b43db7448861398e4de06b4a4541e76e
Futurespast <loadingrevived@gmail.com>
2024-09-13 20:24:20 -0400

    Linted the frontend lol

--- petclinic-frontend/src/features/bills/models/Bill.ts
+++ petclinic-frontend/src/features/bills/models/Bill.ts
@@ -1,15 +1,15 @@
 export interface Bill {
-    billId: string;
-    customerId: string;
-    ownerFirstName: string;
-    ownerLastName: string;
-    visitType: string;
-    vetId: string;
-    vetFirstName: string;
-    vetLastName: string;
-    date: string;
-    amount: number;
-    taxedAmount: number;
-    billStatus: string;
-    dueDate: string;
+  billId: string;
+  customerId: string;
+  ownerFirstName: string;
+  ownerLastName: string;
+  visitType: string;
+  vetId: string;
+  vetFirstName: string;
+  vetLastName: string;
+  date: string;
+  amount: number;
+  taxedAmount: number;
+  billStatus: string;
+  dueDate: string;
 }
--- petclinic-frontend/src/layouts/AppNavBar.tsx
+++ petclinic-frontend/src/layouts/AppNavBar.tsx
@@ -5,126 +5,129 @@ import { AppRoutePaths } from '@/shared/models/path.routes.ts';
 import './AppNavBar.css';
 
 export function NavBar(): JSX.Element {
-    const { user } = useUser();
-    const navigate = useNavigate();
+  const { user } = useUser();
+  const navigate = useNavigate();
 
-    const logoutUser = (): void => {
-        axiosInstance.post(axiosInstance.defaults.baseURL + 'logout').then(() => {
-            navigate(AppRoutePaths.login);
-            localStorage.removeItem('username');
-            localStorage.removeItem('email');
-            localStorage.removeItem('UUID');
-            localStorage.removeItem('roles');
-        });
-    };
+  const logoutUser = (): void => {
+    axiosInstance.post(axiosInstance.defaults.baseURL + 'logout').then(() => {
+      navigate(AppRoutePaths.login);
+      localStorage.removeItem('username');
+      localStorage.removeItem('email');
+      localStorage.removeItem('UUID');
+      localStorage.removeItem('roles');
+    });
+  };
 
-    const customerBillsUrl = AppRoutePaths.CustomerBills.replace(':userId', user.userId);
+  const customerBillsUrl = AppRoutePaths.CustomerBills.replace(
+    ':userId',
+    user.userId
+  );
 
-    return (
-        <nav className="navbar">
-            <a className="navbar-brand" href="#">
-                PetClinic
-            </a>
+  return (
+    <nav className="navbar">
+      <a className="navbar-brand" href="#">
+        PetClinic
+      </a>
 
-            <div className="navbar-collapse" id="navbarSupportedContent">
-                {user ? (
-                    <>
-                        <ul className="navbar-nav">
-                            <li className="nav-item active">
-                                <Link className="nav-link" to={AppRoutePaths.Default}>
-                                    Home
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to={AppRoutePaths.Vet}>
-                                    Veterinarians
-                                </Link>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a
-                                    className="nav-link dropdown-toggle"
-                                    href="#"
-                                    id="navbarDropdown"
-                                    role="button"
-                                    aria-haspopup="true"
-                                    aria-expanded="false"
-                                >
-                                    Owners
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <Link className="nav-link" to="">
-                                        Owners
-                                    </Link>
-                                    <Link className="nav-link" to="">
-                                        Edit Account
-                                    </Link>
-                                    <Link className="nav-link" to="">
-                                        Pet Types
-                                    </Link>
-                                </div>
-                            </li>
-                            {IsAdmin() && (
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="">
-                                        Bills
-                                    </Link>
-                                </li>
-                            )}
-                            <li className="nav-item">
-                                <Link className="nav-link" to={customerBillsUrl}>
-                                    Bills
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="">
-                                    Visits
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to={AppRoutePaths.Inventories}>
-                                    Inventories
-                                </Link>
-                            </li>
-                        </ul>
-                        <ul className="navbar-nav justify-content-end">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="">
-                                    Welcome back {user.username}!
-                                </Link>
-                            </li>
-                            {IsAdmin() && (
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="">
-                                        Admin-Panel
-                                    </Link>
-                                </li>
-                            )}
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link"
-                                    onClick={logoutUser}
-                                    style={{ cursor: 'pointer' }}
-                                >
-                                    Logout
-                                </a>
-                            </li>
-                        </ul>
-                    </>
-                ) : (
-                    <ul>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="">
-                                Signup
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to={AppRoutePaths.login}>
-                                Login
-                            </Link>
-                        </li>
-                    </ul>
-                )}
-            </div>
-        </nav>
-    );
+      <div className="navbar-collapse" id="navbarSupportedContent">
+        {user ? (
+          <>
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link" to={AppRoutePaths.Default}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={AppRoutePaths.Vet}>
+                  Veterinarians
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Owners
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <Link className="nav-link" to="">
+                    Owners
+                  </Link>
+                  <Link className="nav-link" to="">
+                    Edit Account
+                  </Link>
+                  <Link className="nav-link" to="">
+                    Pet Types
+                  </Link>
+                </div>
+              </li>
+              {IsAdmin() && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="">
+                    Bills
+                  </Link>
+                </li>
+              )}
+              <li className="nav-item">
+                <Link className="nav-link" to={customerBillsUrl}>
+                  Bills
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">
+                  Visits
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={AppRoutePaths.Inventories}>
+                  Inventories
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav justify-content-end">
+              <li className="nav-item">
+                <Link className="nav-link" to="">
+                  Welcome back {user.username}!
+                </Link>
+              </li>
+              {IsAdmin() && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="">
+                    Admin-Panel
+                  </Link>
+                </li>
+              )}
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  onClick={logoutUser}
+                  style={{ cursor: 'pointer' }}
+                >
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </>
+        ) : (
+          <ul>
+            <li className="nav-item">
+              <Link className="nav-link" to="">
+                Signup
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={AppRoutePaths.login}>
+                Login
+              </Link>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
 }
