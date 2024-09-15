8176e959923ceeea36e6782635d335df6482a103
AndrewBadIdea <132387897+AndrewBadIdea@users.noreply.github.com>
2024-09-15 13:02:25 -0400

    Fixed lint

--- petclinic-frontend/src/features/customers/api/getAllOwners.ts
+++ petclinic-frontend/src/features/customers/api/getAllOwners.ts
@@ -11,4 +11,5 @@ export const getAllOwners = async (): Promise<
 
   );
 };
+
 */
--- petclinic-frontend/src/pages/Customer/AllOwners.tsx
+++ petclinic-frontend/src/pages/Customer/AllOwners.tsx
@@ -7,25 +7,26 @@ const AllOwners: React.FC = (): JSX.Element => {
   const [owners, setOwners] = useState<OwnerResponseModel[]>([]);
 
   useEffect(() => {
-
-
     // Now handle real-time updates with EventSource
-    const eventSource = new EventSource('http://localhost:8080/api/v2/gateway/owners',
-      {
-        withCredentials: true
-      });
-
     // Listen for updates in the event stream
-    eventSource.onmessage = (event) => {
+
+    const eventSource = new EventSource(
+      'http://localhost:8080/api/v2/gateway/owners',
+
+      {
+        withCredentials: true,
+      }
+    );
+    eventSource.onmessage = event => {
       try {
         const parsedData: OwnerResponseModel = JSON.parse(event.data);
-        setOwners((prevOwners) => [...prevOwners, parsedData]); // Add new owner to state
+        setOwners(prevOwners => [...prevOwners, parsedData]); // Add new owner to state
       } catch (error) {
         console.error('Error parsing event data:', error);
       }
     };
 
-    eventSource.onerror = (error) => {
+    eventSource.onerror = error => {
       console.error('EventSource error:', error);
       eventSource.close(); // Close the connection on error
     };
@@ -41,26 +42,26 @@ const AllOwners: React.FC = (): JSX.Element => {
       <h1>Owners</h1>
       <table>
         <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Address</th>
-          <th>City</th>
-          <th>Province</th>
-          <th>Telephone</th>
-        </tr>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>Province</th>
+            <th>Telephone</th>
+          </tr>
         </thead>
         <tbody>
-        {owners.map((owner) => (
-          <tr key={owner.ownerId}>
-            <td>{owner.firstName}</td>
-            <td>{owner.lastName}</td>
-            <td>{owner.address}</td>
-            <td>{owner.city}</td>
-            <td>{owner.province}</td>
-            <td>{owner.telephone}</td>
-          </tr>
-        ))}
+          {owners.map(owner => (
+            <tr key={owner.ownerId}>
+              <td>{owner.firstName}</td>
+              <td>{owner.lastName}</td>
+              <td>{owner.address}</td>
+              <td>{owner.city}</td>
+              <td>{owner.province}</td>
+              <td>{owner.telephone}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
