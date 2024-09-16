83f195abf0fc9f38957c0688e73b61bbf9b24e1b
AndrewBadIdea <132387897+AndrewBadIdea@users.noreply.github.com>
2024-09-16 15:49:49 -0400

    fix lint

--- petclinic-frontend/src/pages/Customer/AllOwners.tsx
+++ petclinic-frontend/src/pages/Customer/AllOwners.tsx
@@ -39,34 +39,34 @@ const AllOwners: React.FC = (): JSX.Element => {
 
   return (
     <div>
-    <NavBar/>
-    <div className="owners-container">
-      <h1>Owners</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Address</th>
-            <th>City</th>
-            <th>Province</th>
-            <th>Telephone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {owners.map(owner => (
-            <tr key={owner.ownerId}>
-              <td>{owner.firstName}</td>
-              <td>{owner.lastName}</td>
-              <td>{owner.address}</td>
-              <td>{owner.city}</td>
-              <td>{owner.province}</td>
-              <td>{owner.telephone}</td>
+      <NavBar />
+      <div className="owners-container">
+        <h1>Owners</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Address</th>
+              <th>City</th>
+              <th>Province</th>
+              <th>Telephone</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {owners.map(owner => (
+              <tr key={owner.ownerId}>
+                <td>{owner.firstName}</td>
+                <td>{owner.lastName}</td>
+                <td>{owner.address}</td>
+                <td>{owner.city}</td>
+                <td>{owner.province}</td>
+                <td>{owner.telephone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
