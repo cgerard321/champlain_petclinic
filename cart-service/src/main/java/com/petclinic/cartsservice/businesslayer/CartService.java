2d1ae87bede42f6bd149e4922f0da927a3184bc8
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2024-09-16 12:10:19 -0400

    Feat/cart/cpc 1033/retrieve cart for client (#623)
    JIRA: https://champlainsaintlambert.atlassian.net/browse/CPC-1033
    ## Context:
    Retrieve the informations of a cart with all its products using a
    clientId
    ## Changes
     - created the cart entity
     - implemented the get cart by cart id
     - added a domain client layer for a product client
     - added some tests
     - modified the application.yaml to integrate a mongo Database
    ## Dev notes (Optional)
    - Still need to add it to the API gateway
    
    ---------
    
    Co-authored-by: NtsoaElie <113870458+NtsoaElie@users.noreply.github.com>

--- cart-service/src/main/java/com/petclinic/cartsservice/businesslayer/CartService.java
+++ cart-service/src/main/java/com/petclinic/cartsservice/businesslayer/CartService.java
@@ -1,4 +1,11 @@
 package com.petclinic.cartsservice.businesslayer;
 
+
+import com.petclinic.cartsservice.presentationlayer.CartResponseModel;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface CartService {
+
+    public Mono<CartResponseModel> getCartByCartId(String cartId);
 }
