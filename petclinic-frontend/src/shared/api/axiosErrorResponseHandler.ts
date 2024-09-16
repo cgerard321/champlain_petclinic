33083f3d45f59fc873c2f41a20e9941ff6642e24
Debo <deemohuang@proton.me>
2024-09-16 13:03:10 -0400

    feat/(AUTH-CPC-1129): Create error pages for 4xx and 5xx status codes (#638)
    JIRA: https://champlainsaintlambert.atlassian.net/browse/CPC-1129
    ## Context:
    We currently do not have custom error pages.
    ## Changes
    - Created error pages for status code: 401, 403, 404, 408, 500, and 503.
    - Button to go back home.
    - Added some styling (might change in future). All pages use the same
    design.
    - Routed all the pages accordingly.
    ## Before and After UI (Required for UI-impacting PRs)
    **Before**
    
    ![image](https://github.com/user-attachments/assets/ed69762e-da37-4937-85c6-a44ce7672c2b)
    
    **After**
    
    ![image](https://github.com/user-attachments/assets/ab5a21f0-fa90-4fd9-8cd4-0292e6121d3f)
    
    
    ![image](https://github.com/user-attachments/assets/a2eeb6d5-7bf8-42e3-9d5e-57801b6a1a70)
    The other error pages look the same with their corresponding messages.

--- petclinic-frontend/src/shared/api/axiosErrorResponseHandler.ts
+++ petclinic-frontend/src/shared/api/axiosErrorResponseHandler.ts
@@ -7,23 +7,23 @@ export default function axiosErrorResponseHandler(
 ): void {
   switch (statusCode) {
     case 401:
-      // redirrect to the unauthorized page
+      // redirect to the unauthorized page
       router.navigate('/unauthorized');
       break;
     case 403:
-      // redirrect to the forbidden page
-      router.navigate('/unauthorized');
+      // redirect to the forbidden page
+      router.navigate('/forbidden');
       break;
     case 408:
-      // redirrect to the request timeout page
+      // redirect to the request timeout page
       router.navigate('/request-timeout');
       break;
     case 500:
-      // redirrect to the internal server error page
+      // redirect to the internal server error page
       router.navigate('/internal-server-error');
       break;
     case 503:
-      // redirrect to the service unavailable page
+      // redirect to the service unavailable page
       router.navigate('/service-unavailable');
       break;
     default:
