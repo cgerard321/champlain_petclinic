d9a1857aa429aca5e5a83e125b72a7ad09f49c5e
Debo <deemohuang@proton.me>
2024-09-15 22:32:02 -0400

    ...

--- petclinic-frontend/src/pages/Error/Forbidden.tsx
+++ petclinic-frontend/src/pages/Error/Forbidden.tsx
@@ -14,7 +14,9 @@ const Forbidden: React.FC = () => {
     <div className="error-wrapper">
       <div className="error-container">
         <h1>403</h1>
-        <p>Sorry, you do not have permission to access this page or resource.</p>
+        <p>
+          Sorry, you do not have permission to access this page or resource.
+        </p>
         <button onClick={handleGoHome}>Go Back Home</button>
       </div>
     </div>
