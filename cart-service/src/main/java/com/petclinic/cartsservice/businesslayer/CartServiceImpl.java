7596c4a5eb40d31133188912b51de8a3324efa82
NtsoaElie <ntsoaelie@gmail.com>
2024-09-16 14:30:16 -0400

    Implemented Update Method in CartService, CartServiceImpl and CartController

--- cart-service/src/main/java/com/petclinic/cartsservice/businesslayer/CartServiceImpl.java
+++ cart-service/src/main/java/com/petclinic/cartsservice/businesslayer/CartServiceImpl.java
@@ -2,6 +2,8 @@ package com.petclinic.cartsservice.businesslayer;
 
 import com.petclinic.cartsservice.dataaccesslayer.CartRepository;
 import com.petclinic.cartsservice.domainclientlayer.ProductClient;
+import com.petclinic.cartsservice.domainclientlayer.ProductResponseModel;
+import com.petclinic.cartsservice.presentationlayer.CartRequestModel;
 import com.petclinic.cartsservice.presentationlayer.CartResponseModel;
 import com.petclinic.cartsservice.utils.EntityModelUtil;
 import com.petclinic.cartsservice.utils.exceptions.NotFoundException;
@@ -36,5 +38,29 @@ public class CartServiceImpl implements CartService {
                             .collectList()
                             .map(products -> EntityModelUtil.toCartResponseModel(cart, products));
                 });
+    }
+
+    @Override
+    public Mono<CartResponseModel> updateCartByCartId(Mono<CartRequestModel> cartRequestModel, String cartId) {
+        return cartRepository.findCartByCartId(cartId)
+                .switchIfEmpty(Mono.defer(() -> Mono.error(new NotFoundException("Cart id was not found: " + cartId))))
+                .flatMap(foundCart -> cartRequestModel
+                        .map(EntityModelUtil::toCartEntity)
+                        .doOnNext(c -> c.setCartId(foundCart.getCartId()))
+                        .doOnNext(c -> c.setCustomerId(foundCart.getCustomerId()))
+                        .doOnNext(c -> c.setProductIds(foundCart.getProductIds()))
+                        .doOnNext(c -> c.setId(foundCart.getId()))
+                )
+
+                .flatMap(cartRepository::save)
+                .flatMap(cart -> {
+                    List<String> productIds = cart.getProductIds();
+                    return productIds
+                            .stream().map(productId -> productClient.getProductByProductId(productId).flux())
+                            .reduce(Flux.empty(), Flux::merge)
+                            .collectList()
+                            .map(products -> EntityModelUtil.toCartResponseModel(cart, products));
+                });
+
     }
 }
