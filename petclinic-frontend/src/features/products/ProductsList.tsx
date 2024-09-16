9e91edbe814a70ea04f3e7f2f80449571f03484e
volking <vik.kuts@hotmail.com>
2024-09-15 21:12:12 -0400

    Made Products use same GetAll Event-Stream path instead of ApplicationJSON Path

--- petclinic-frontend/src/features/products/ProductsList.tsx
+++ petclinic-frontend/src/features/products/ProductsList.tsx
@@ -4,32 +4,37 @@ import './ProductList.css';
 import { ProductModel } from '@/features/inventories/models/ProductModels/ProductModel.ts';
 
 export default function ProductList(): JSX.Element {
-    const [productList, setProductList] = useState<ProductModel[]>([]);
+  const [productList, setProductList] = useState<ProductModel[]>([]);
 
-    const fetchProducts = async (): Promise<void> => {
-            const products = await getAllProducts();
-            setProductList(products);
-    };
+  const fetchProducts = async (): Promise<void> => {
+    const list = await getAllProducts();
+    setProductList(list);
+  };
 
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-
-    useEffect(() => {
-        fetchProducts();
-    }, []);
-
-    return (
-        <div>
-            <div className="grid">
-                {productList.map((product) => (
-                    <div className="card" key={product.productId}>
-                        <h2>{product.productName}</h2>
-                        <p>{product.productDescription}</p>
-                        <p>Price: ${product.productSalePrice.toFixed(2)}</p>
-                        <p>Reviews: </p>
-                        <p>Quantity: </p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="grid">
+        {productList
+          .filter(data => data != null)
+          .map(
+            (product: {
+              productId: string;
+              productName: string;
+              productDescription: string;
+              productSalePrice: number;
+            }) => (
+              <div className="card" key={product.productId}>
+                <h2>{product.productName}</h2>
+                <p>{product.productDescription}</p>
+                <p>Price: ${product.productSalePrice.toFixed(2)}</p>
+              </div>
+            )
+          )}
+      </div>
+    </div>
+  );
 }
--- petclinic-frontend/src/features/products/api/getAllProducts.ts
+++ petclinic-frontend/src/features/products/api/getAllProducts.ts
@@ -2,9 +2,16 @@ import axiosInstance from '@/shared/api/axiosInstance.ts';
 import { ProductModel } from '@/features/inventories/models/ProductModels/ProductModel.ts';
 
 export async function getAllProducts(): Promise<ProductModel[]> {
-        const response = await axiosInstance.get<ProductModel[]>(
-            '/products/display'
-        );
-        return response.data;
-
+  const res = await axiosInstance.get('/products', { responseType: 'stream' });
+  return res.data
+    .split('data:')
+    .map((dataChunk: string) => {
+      try {
+        if (dataChunk == '') return null;
+        return JSON.parse(dataChunk);
+      } catch (err) {
+        console.error('Could not parse JSON: ' + err);
+      }
+    })
+    .filter((data?: JSON) => data !== null);
 }
