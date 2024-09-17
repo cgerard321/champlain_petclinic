36f47e9f384f2801b087ac058c57ee651cec95da
Zakaria_ <135060272+tramway23@users.noreply.github.com>
2024-09-17 18:56:38 -0400

    Created a review entity in Visit, created the service and service impl. Done the test. In the api gateway, addeded the review. In the front end, add, update and get all reviews

---
+++ petclinic-frontend/src/features/visits/Review/Api/addReview.ts
@@ -0,0 +1,9 @@
+import { AxiosResponse } from 'axios';
+import { ReviewRequestDTO } from '../Model/ReviewRequestDTO';
+import axiosInstance from '@/shared/api/axiosInstance';
+
+export const addReview = async (
+  review: ReviewRequestDTO
+): Promise<AxiosResponse<void>> => {
+  return await axiosInstance.post<void>('/visits/reviews', review);
+};
