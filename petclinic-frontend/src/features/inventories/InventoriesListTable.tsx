07782f93e9212ce5e9bee7764d3383f7162a25b4
vini <vinivelozo02@outlook.com>
2024-09-17 00:06:24 -0400

    Implemented back-end for availability status category

--- petclinic-frontend/src/features/inventories/InventoriesListTable.tsx
+++ petclinic-frontend/src/features/inventories/InventoriesListTable.tsx
@@ -100,7 +100,6 @@ export default function InventoriesListTable(): JSX.Element {
                     inventoryName,
                     inventoryType,
                     inventoryDescription
-
                   )
                 }
               />
