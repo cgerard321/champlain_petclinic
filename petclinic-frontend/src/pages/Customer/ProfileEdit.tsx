322e9c2d48df5cca2f9dae55628e6f6631abbc90
Debo <deemohuang@proton.me>
2024-09-17 23:21:15 -0400

    show navbar in profileedit

--- petclinic-frontend/src/pages/Customer/ProfileEdit.tsx
+++ petclinic-frontend/src/pages/Customer/ProfileEdit.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import UpdateCustomerForm from '@/features/customers/components/UpdateCustomerForm.tsx';
+import { NavBar } from '@/layouts/AppNavBar.tsx';
 
 const ProfileEdit: React.FC = (): JSX.Element => {
   return (
     <div>
+      <NavBar />
       <UpdateCustomerForm />
     </div>
   );
