105bc0c10931859a7ddd06f1f1eadb4522c4526f
VladLoghin <92529319+VladLoghin@users.noreply.github.com>
2024-09-18 22:01:36 -0400

    added visitStartDate and visitEndDate:
    Changed the visitDate name to visitStartDate to reflect what the field is supposed to project better.
    
    The visitEndDate, reflect when the visit is supposed to end, always at an interval of 1 hour from the visitStartDate
    
    finalized and tested version of the EndDate that works
    
    fixed a merge conflict regarding imports

--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DataLayer/DataSetupService.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DataLayer/DataSetupService.java
@@ -12,7 +12,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
