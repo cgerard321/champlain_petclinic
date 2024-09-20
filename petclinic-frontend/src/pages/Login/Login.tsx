1f959d7e80300a84b30aba8443bb0fe02493a268
Zagreus <artemij.ca@gmail.com>
2024-09-20 00:40:51 -0400

    Slide show works
    added images and shi

--- petclinic-frontend/src/pages/Login/Login.tsx
+++ petclinic-frontend/src/pages/Login/Login.tsx
@@ -1,49 +1,56 @@
 import axiosInstance from '@/shared/api/axiosInstance';
+import anotherDoctorAndDoggy from '@/assets/Login/another-doctor-and-doggy.jpg';
+import doctorAndDoggy from '@/assets/Login/doctor-and-doggy.jpg';
 import { UserResponseModel } from '@/shared/models/UserResponseModel';
 import { FormEvent } from 'react';
 import { useSetUser } from '@/context/UserContext.tsx';
 import { useNavigate } from 'react-router-dom';
 import { AppRoutePaths } from '@/shared/models/path.routes.ts';
 import './Login.css';
+import Slideshow from './Slideshow';
+
+const images = [doctorAndDoggy, anotherDoctorAndDoggy];
 
 export default function Login(): JSX.Element {
-  const setUser = useSetUser();
-  const navigate = useNavigate();
-  const login = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const formElements = form.elements as typeof form.elements & {
-      emailInput: HTMLInputElement;
-      passwordInput: HTMLInputElement;
-    };
-    await axiosInstance
-      .post<UserResponseModel>(
-        'http://localhost:8080/api/gateway/' + 'users/login',
-        {
-          email: formElements.emailInput.value,
-          password: formElements.passwordInput.value,
-        }
-      )
-      .then(response => {
-        setUser(response.data);
-        if (response.data.userId !== '') {
-          navigate(AppRoutePaths.Home);
-        }
-      });
+ const setUser = useSetUser();
+ const navigate = useNavigate();
+ const login = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const formElements = form.elements as typeof form.elements & {
+   emailInput: HTMLInputElement;
+   passwordInput: HTMLInputElement;
   };
-  return (
-
+  await axiosInstance
+   .post<UserResponseModel>(
+    'http://localhost:8080/api/gateway/' + 'users/login',
+    {
+     email: formElements.emailInput.value,
+     password: formElements.passwordInput.value,
+    }
+   )
+   .then(response => {
+    setUser(response.data);
+    if (response.data.userId !== '') {
+     navigate(AppRoutePaths.Home);
+    }
+   });
+ };
+ return (
+  <div className="login-page">
    <div className="login-container">
-     <h1>User Login</h1>
-     <form onSubmit={login}>
-       <label>Email: </label>
-       <input type="text" id="emailInput" />
-       <br />
-       <label>Password: </label>
-       <input type="text" id="passwordInput" />
-       <br />
-       <button type="submit">Login</button>
-     </form>
+    <h1>User Login</h1>
+    <form onSubmit={login}>
+     <label>Email: </label>
+     <input type="text" id="emailInput" />
+     <br />
+     <label>Password: </label>
+     <input type="password" id="passwordInput" />
+     <br />
+     <button type="submit">Login</button>
+    </form>
    </div>
-  );
+   <Slideshow images={images} interval={7000} />
+  </div>
+ );
 }
