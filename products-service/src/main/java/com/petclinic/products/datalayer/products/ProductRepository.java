573b862bb461d5e84dd02baa514ef398c234df05
Thomas <thomas.bedard0@gmail.com>
2024-09-19 22:31:11 -0400

    fix a problem where the min and max numbers themselves didn't get included in the filter range

--- products-service/src/main/java/com/petclinic/products/datalayer/products/ProductRepository.java
+++ products-service/src/main/java/com/petclinic/products/datalayer/products/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.petclinic.products.datalayer.products;
 
 import com.petclinic.products.datalayer.products.Product;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,9 +10,12 @@ public interface ProductRepository extends ReactiveMongoRepository<Product, Stri
 
     Mono<Product> findProductByProductId(String productId);
 
+    @Query("{ 'productSalePrice' : { $gte: ?0, $lte: ?1 } }")
     Flux<Product> findByProductSalePriceBetween(Double minPrice, Double maxPrice);
 
+    @Query("{ 'productSalePrice' : { $gte: ?0 } }")
     Flux<Product> findByProductSalePriceGreaterThanEqual(Double minPrice);
 
+    @Query("{ 'productSalePrice' : { $lte: ?0 } }")
     Flux<Product> findByProductSalePriceLessThanEqual(Double maxPrice);
 }
