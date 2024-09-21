41b90019edb8bad3c76ad75afd9ae421f9ae664a
Aian Batoochirov <119423256+orell2j@users.noreply.github.com>
2024-09-21 18:28:22 -0400

    Feat/vets cpc 1081 delete vet by vet (#716)
    **JIRA:** link to jira ticket
    ## Context:
    What is the ticket about and why are we doing this change.
    ## Does this PR change the .vscode folder in petclinic-frontend?:
    If the PR changes the .vscode folder, explain why in detail because it
    should not.
    Be sure to include cgerard321 as a reviewer.
    
    <span style="color:red">**Reviewers need to check for any changes to the
    .vscode folder and add a comment about it to their review
    comments.**</span>
    
    ## Changes
    What are the various changes and what other modules do those changes
    affect.
    This can be bullet point or sentence format.
    
    Backend:
    - delete vet with all the associated data such as education, ratings,
    badges and photos
       that ensures that no orphaned data remains in the database.
    
    
    Frontend:
    - added delete buttons in the list of vets
    
    ## Before and After UI (Required for UI-impacting PRs)
    If this is a change to the UI, include before and after screenshots to
    show the differences.
    If this is a new UI feature, include screenshots to show reviewers what
    it looks like.
    
    Before:
    
    <img width="1280" alt="CPC_frontend_delete_before"
    src="https://github.com/user-attachments/assets/d81db0ba-863f-4070-b4c7-f00b75f28e8c">
    
    After:
    
    <img width="1280" alt="CPC_frontend_delete_after"
    src="https://github.com/user-attachments/assets/4467ebef-496b-4588-a3d6-e2d1e583db3e">
    
    
    ## Dev notes (Optional)
    Specific technical changes that should be noted
    
    - understand how photo and badge entities work
    - work on photo and badge entities, I commented them for now because
    they give 500 status
    
    ## Linked pull requests (Optional)
    Pull request links
    
    ---------
    
    Co-authored-by: kyrusho <131713245+kyrusho@users.noreply.github.com>
    Co-authored-by: AndrewBadIdea <132387897+AndrewBadIdea@users.noreply.github.com>
    Co-authored-by: PedroSilveira <96072825+bluement@users.noreply.github.com>
    Co-authored-by: Sunveer <142905066+Sunveerg@users.noreply.github.com>
    Co-authored-by: Felix Zhang <132387996+Apoleh@users.noreply.github.com>
    Co-authored-by: Debo <deemohuang@proton.me>

--- vet-service/src/main/java/com/petclinic/vet/servicelayer/VetServiceImpl.java
+++ vet-service/src/main/java/com/petclinic/vet/servicelayer/VetServiceImpl.java
@@ -18,6 +18,8 @@ import com.petclinic.vet.dataaccesslayer.VetRepository;
 import com.petclinic.vet.dataaccesslayer.badges.Badge;
 import com.petclinic.vet.dataaccesslayer.badges.BadgeRepository;
 import com.petclinic.vet.dataaccesslayer.badges.BadgeTitle;
+import com.petclinic.vet.dataaccesslayer.education.EducationRepository;
+import com.petclinic.vet.dataaccesslayer.ratings.RatingRepository;
 import com.petclinic.vet.exceptions.InvalidInputException;
 import com.petclinic.vet.exceptions.NotFoundException;
 import com.petclinic.vet.presentationlayer.VetRequestDTO;
@@ -29,6 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StreamUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -45,6 +48,8 @@ public class VetServiceImpl implements VetService {
     private final VetRepository vetRepository;
     private final BadgeRepository badgeRepository;
     private final PhotoRepository photoRepository;
+    private final RatingRepository ratingRepository;
+    private final EducationRepository educationRepository;
 
     @Override
     public Flux<VetResponseDTO> getAll() {
@@ -109,6 +114,7 @@ public class VetServiceImpl implements VetService {
                 .map(EntityDtoUtil::vetEntityToResponseDTO);
     }
 
+    @Transactional
     @Override
     public Mono<VetResponseDTO> getVetByFirstName(String firstName) {
         return vetRepository.findVetByFirstName(firstName)
@@ -123,13 +129,24 @@ public class VetServiceImpl implements VetService {
                 .map(EntityDtoUtil::vetEntityToResponseDTO);
     }
 
-
     @Override
     public Mono<Void> deleteVetByVetId(String vetId) {
         return vetRepository.findVetByVetId(vetId)
                 .switchIfEmpty(Mono.error(new NotFoundException("No vet with this vetId was found: " + vetId)))
-                .flatMap(vetRepository::delete);
+                .flatMap(vet -> {
+                    log.info("Deleting associated data for vetId: {}", vetId);
+                    //Mono<Integer> deleteBadges = badgeRepository.deleteByVetId(vetId);
+                    //Mono<Integer> deletePhotos = photoRepository.deleteByVetId(vetId);
+                    Mono<String> deleteRatings = ratingRepository.deleteByVetId(vetId);
+                    Mono<String> deleteEducations = educationRepository.deleteByVetId(vetId);
+
+                    return Mono.when( deleteRatings, deleteEducations)
+                            .then(vetRepository.delete(vet))
+                            .doOnSuccess(unused -> log.info("Successfully deleted vetId: {}", vetId))
+                            .doOnError(error -> log.error("Error deleting vetId: {}", vetId, error));
+                });
     }
+
 
     private byte[] loadImage(String imagePath) {
         try {
