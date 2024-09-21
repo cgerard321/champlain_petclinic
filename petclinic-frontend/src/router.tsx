8be1afd06ab1560d873707dbcbdec34b176ca47e
AlexCristea04 <alexcristea004@gmail.com>
2024-09-20 21:54:19 -0400

    Added API gateway functionality, get all methods, front end for all carts and individual carts, get all integration in the front end. Before update

--- petclinic-frontend/src/router.tsx
+++ petclinic-frontend/src/router.tsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: AppRoutePaths.VisitByVisitId,
+        path: AppRoutePaths.GetVisitByVistId,
         element: (
           <ProtectedRoute>
             <VisitByVisitId />
