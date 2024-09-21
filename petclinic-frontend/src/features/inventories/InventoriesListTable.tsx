50e542addef0aee49c2064fd1f66aa3aa1d5ce32
Sunveer <142905066+Sunveerg@users.noreply.github.com>
2024-09-20 22:26:54 -0400

    I added the delete selected inventories feature

--- petclinic-frontend/src/features/inventories/InventoriesListTable.tsx
+++ petclinic-frontend/src/features/inventories/InventoriesListTable.tsx
@@ -12,12 +12,12 @@ import AddInventoryType from '@/features/inventories/AddInventoryType.tsx';
 //TODO: create add inventory form component and change the component being shown on the inventories page on the onClick event of the add inventory button
 export default function InventoriesListTable(): JSX.Element {
   const [selectedInventories, setSelectedInventories] = useState<Inventory[]>(
-      []
+    []
   );
   const [inventoryName, setInventoryName] = useState('');
   const [inventoryType, setInventoryType] = useState('');
   const [inventoryTypeList, setInventoryTypeList] = useState<InventoryType[]>(
-      []
+    []
   );
   const [inventoryDescription, setInventoryDescription] = useState('');
   const [showConfirmDialog, setShowConfirmDialog] = useState(false);
@@ -89,16 +89,17 @@ export default function InventoriesListTable(): JSX.Element {
   };
 
   const handleInventorySelection = (
-      e: React.ChangeEvent<HTMLInputElement>,
-      inventory: Inventory
+    e: React.ChangeEvent<HTMLInputElement>,
+    inventory: Inventory
   ): void => {
     const isChecked = e.target.checked;
-    setSelectedInventories((prevSelected) => {
+    setSelectedInventories(prevSelected => {
       if (isChecked) {
         return [...prevSelected, inventory];
       } else {
         return prevSelected.filter(
-            (selectedInventory) => selectedInventory.inventoryId !== inventory.inventoryId
+          selectedInventory =>
+            selectedInventory.inventoryId !== inventory.inventoryId
         );
       }
     });
@@ -116,11 +117,10 @@ export default function InventoriesListTable(): JSX.Element {
     setSelectedInventories([]);
   };
 
-
   return (
-      <div>
-        <table className="table table-striped">
-          <thead>
+    <div>
+      <table className="table table-striped">
+        <thead>
           <tr>
             {/* <td>Inventory ID</td> */}
             <td></td>
@@ -135,67 +135,67 @@ export default function InventoriesListTable(): JSX.Element {
             <td></td>
             <td>
               <input
-                  type="text"
-                  value={inventoryName}
-                  onChange={e => setInventoryName(e.target.value)}
-                  onKeyUp={e =>
-                      e.key === 'Enter' &&
-                      getInventoryList(
-                          inventoryName,
-                          inventoryType,
-                          inventoryDescription
-                      )
-                  }
+                type="text"
+                value={inventoryName}
+                onChange={e => setInventoryName(e.target.value)}
+                onKeyUp={e =>
+                  e.key === 'Enter' &&
+                  getInventoryList(
+                    inventoryName,
+                    inventoryType,
+                    inventoryDescription
+                  )
+                }
               />
             </td>
             <td>
               <select
-                  className="form-control col-sm-4"
-                  value={inventoryType}
-                  onChange={e => setInventoryType(e.target.value)}
-                  onKeyUp={e =>
-                      e.key === 'Enter' &&
-                      getInventoryList(
-                          inventoryName,
-                          inventoryType,
-                          inventoryDescription
-                      )
-                  }
+                className="form-control col-sm-4"
+                value={inventoryType}
+                onChange={e => setInventoryType(e.target.value)}
+                onKeyUp={e =>
+                  e.key === 'Enter' &&
+                  getInventoryList(
+                    inventoryName,
+                    inventoryType,
+                    inventoryDescription
+                  )
+                }
               >
                 <option value="">None</option>
                 {inventoryTypeList.map(type => (
-                    <option key={type.type}>{type.type}</option>
+                  <option key={type.type}>{type.type}</option>
                 ))}
               </select>
             </td>
             <td>
               <input
-                  type="text"
-                  value={inventoryDescription}
-                  onChange={e => setInventoryDescription(e.target.value)}
-                  onKeyUp={e =>
-                      e.key === 'Enter' &&
-                      getInventoryList(
-                          inventoryName,
-                          inventoryType,
-                          inventoryDescription
-                      )
-                  }
+                type="text"
+                value={inventoryDescription}
+                onChange={e => setInventoryDescription(e.target.value)}
+                onKeyUp={e =>
+                  e.key === 'Enter' &&
+                  getInventoryList(
+                    inventoryName,
+                    inventoryType,
+                    inventoryDescription
+                  )
+                }
               />
             </td>
             <td>
               <button
-                  className="btn btn-success"
-                  onClick={clearQueries}
-                  title="Clear"
+                className="btn btn-success"
+                onClick={clearQueries}
+                title="Clear"
               >
                 <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    fill="white"
-                    className="bi bi-x-circle"
-                    viewBox="0 0 16 16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  fill="white"
+                  className="bi bi-x-circle"
+                  viewBox="0 0 16 16"
                 >
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
@@ -204,98 +204,98 @@ export default function InventoriesListTable(): JSX.Element {
             </td>
             <td>
               <button
-                  className="btn btn-success"
-                  onClick={() =>
-                      getInventoryList(
-                          inventoryName,
-                          inventoryType,
-                          inventoryDescription
-                      )
-                  }
-                  title="Search"
+                className="btn btn-success"
+                onClick={() =>
+                  getInventoryList(
+                    inventoryName,
+                    inventoryType,
+                    inventoryDescription
+                  )
+                }
+                title="Search"
               >
                 <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    fill="white"
-                    className="bi bi-search"
-                    viewBox="0 0 16 16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  fill="white"
+                  className="bi bi-search"
+                  viewBox="0 0 16 16"
                 >
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                 </svg>
               </button>
             </td>
           </tr>
-          </thead>
-          <tbody>
-          {inventoryList.map((inventory) => (
-              <tr key={inventory.inventoryId}>
-                <td>
-                  <input
-                      type="checkbox"
-                      checked={selectedInventories.some(
-                          (selectedInventory) =>
-                              selectedInventory.inventoryId === inventory.inventoryId
-                      )}
-                      onChange={(e) => handleInventorySelection(e, inventory)}
-                  />
-                </td>
-                {/* <td>{inventory.inventoryId}</td> */}
-                <td
-                    onClick={() =>
-                        navigate(`/inventory/${inventory.inventoryId}/products`)
-                    }
-                    style={{
-                      cursor: 'pointer',
-                      textDecoration: 'underline',
-                      color: 'blue',
-                    }}
+        </thead>
+        <tbody>
+          {inventoryList.map(inventory => (
+            <tr key={inventory.inventoryId}>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={selectedInventories.some(
+                    selectedInventory =>
+                      selectedInventory.inventoryId === inventory.inventoryId
+                  )}
+                  onChange={e => handleInventorySelection(e, inventory)}
+                />
+              </td>
+              {/* <td>{inventory.inventoryId}</td> */}
+              <td
+                onClick={() =>
+                  navigate(`/inventory/${inventory.inventoryId}/products`)
+                }
+                style={{
+                  cursor: 'pointer',
+                  textDecoration: 'underline',
+                  color: 'blue',
+                }}
+              >
+                {inventory.inventoryName}
+              </td>
+              <td>{inventory.inventoryType}</td>
+              <td>{inventory.inventoryDescription}</td>
+              <td>
+                <button
+                  onClick={e => {
+                    e.stopPropagation();
+                    navigate(`inventory/${inventory.inventoryId}/edit`);
+                  }}
+                  className="btn btn-warning"
                 >
-                  {inventory.inventoryName}
-                </td>
-                <td>{inventory.inventoryType}</td>
-                <td>{inventory.inventoryDescription}</td>
-                <td>
-                  <button
-                      onClick={e => {
-                        e.stopPropagation();
-                        navigate(`inventory/${inventory.inventoryId}/edit`);
-                      }}
-                      className="btn btn-warning"
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={e => {
+                    e.stopPropagation();
+                    deleteInventoryHandler(inventory);
+                  }}
+                  title="Delete"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    fill="currentColor"
+                    className="bi bi-trash"
+                    viewBox="0 0 16 16"
                   >
-                    Edit
-                  </button>
-                </td>
-                <td>
-                  <button
-                      className="btn btn-danger"
-                      onClick={e => {
-                        e.stopPropagation();
-                        deleteInventoryHandler(inventory);
-                      }}
-                      title="Delete"
-                  >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="32"
-                        height="32"
-                        fill="currentColor"
-                        className="bi bi-trash"
-                        viewBox="0 0 16 16"
-                    >
-                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                    </svg>
-                  </button>
-                </td>
-              </tr>
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                  </svg>
+                </button>
+              </td>
+            </tr>
           ))}
-          </tbody>
-        </table>
-        <div className="text-center">
-          <table className="mx-auto">
-            <tbody>
+        </tbody>
+      </table>
+      <div className="text-center">
+        <table className="mx-auto">
+          <tbody>
             <tr>
               <td>
                 <button className="btn btn-success btn-sm" onClick={pageBefore}>
@@ -311,85 +311,85 @@ export default function InventoriesListTable(): JSX.Element {
                 </button>
               </td>
             </tr>
-            </tbody>
-          </table>
-        </div>
-        <div id="loadingObject" style={{ display: 'none' }}>
-          Loading...
-        </div>
-        <div
-            id="notification"
-            style={{
-              display: 'none',
-              position: 'fixed',
-              bottom: '10px',
-              right: '10px',
-              backgroundColor: '#4CAF50',
-              color: 'white',
-              padding: '10px',
-              borderRadius: '5px',
-            }}
-        >
-          Notification Text Here
-        </div>
-        <button
-            className="btn btn-danger"
-            onClick={deleteSelectedInventories}
-            disabled={selectedInventories.length === 0} // Disable if no items selected
-        >
-          Delete Selected Inventories
-        </button>
-        <button
-            className="delete-bundle-button btn btn-success mx-1"
-            onClick={() => {
-              handleDeleteAllInventories(false);
-            }}
-        >
-          Delete All Inventories
-        </button>
-        <button
-            className="add-inventory-button btn btn-success"
-            onClick={() => {}}
-        >
-          Add Inventory
-        </button>
-        <button
-            className="add-inventorytype-button btn btn-primary"
-            onClick={() => setShowAddTypeForm(true)} // Show the form when clicked
-        >
-          Add InventoryType
-        </button>
-        {showAddTypeForm && (
-            <AddInventoryType
-                show={showAddTypeForm}
-                handleClose={() => setShowAddTypeForm(false)}
-                refreshInventoryTypes={refreshInventoryTypes} // Pass the function to refresh inventory types
-            />
-        )}
-
-        {showConfirmDialog && (
-            <>
-              <div
-                  className="overlay"
-                  onClick={() => setShowConfirmDialog(false)}
-              ></div>
-              <div className="confirm-dialog">
-                <p>Are you sure you want to delete all inventories?</p>
-                <button
-                    className={'btn-danger mx-1'}
-                    onClick={() => handleDeleteAllInventories(true)}
-                >
-                  Yes
-                </button>
-                <button
-                    className={'btn-warning mx-1'}
-                    onClick={() => setShowConfirmDialog(false)}
-                >
-                  No
-                </button>
-              </div>
-            </>
-        )}
+          </tbody>
+        </table>
       </div>
+      <div id="loadingObject" style={{ display: 'none' }}>
+        Loading...
+      </div>
+      <div
+        id="notification"
+        style={{
+          display: 'none',
+          position: 'fixed',
+          bottom: '10px',
+          right: '10px',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          padding: '10px',
+          borderRadius: '5px',
+        }}
+      >
+        Notification Text Here
+      </div>
+      <button
+        className="btn btn-danger"
+        onClick={deleteSelectedInventories}
+        disabled={selectedInventories.length === 0} // Disable if no items selected
+      >
+        Delete Selected Inventories
+      </button>
+      <button
+        className="delete-bundle-button btn btn-success mx-1"
+        onClick={() => {
+          handleDeleteAllInventories(false);
+        }}
+      >
+        Delete All Inventories
+      </button>
+      <button
+        className="add-inventory-button btn btn-success"
+        onClick={() => {}}
+      >
+        Add Inventory
+      </button>
+      <button
+        className="add-inventorytype-button btn btn-primary"
+        onClick={() => setShowAddTypeForm(true)} // Show the form when clicked
+      >
+        Add InventoryType
+      </button>
+      {showAddTypeForm && (
+        <AddInventoryType
+          show={showAddTypeForm}
+          handleClose={() => setShowAddTypeForm(false)}
+          refreshInventoryTypes={refreshInventoryTypes} // Pass the function to refresh inventory types
+        />
+      )}
+
+      {showConfirmDialog && (
+        <>
+          <div
+            className="overlay"
+            onClick={() => setShowConfirmDialog(false)}
+          ></div>
+          <div className="confirm-dialog">
+            <p>Are you sure you want to delete all inventories?</p>
+            <button
+              className={'btn-danger mx-1'}
+              onClick={() => handleDeleteAllInventories(true)}
+            >
+              Yes
+            </button>
+            <button
+              className={'btn-warning mx-1'}
+              onClick={() => setShowConfirmDialog(false)}
+            >
+              No
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
