01bf4287d199c9224f8dd4503afc2c0fe22cbfc3
AlexCristea04 <alexcristea004@gmail.com>
2024-09-22 14:32:59 -0400

    Used ESLint to fix react indentation issues

--- petclinic-frontend/src/features/carts/components/CartItem.tsx
+++ petclinic-frontend/src/features/carts/components/CartItem.tsx
@@ -1,60 +1,69 @@
-import React from 'react';
+import 'react';
 import { ProductModel } from '../models/ProductModel';
 import './CartItem.css';
 
 type CartItemProps = {
-    index: number;
-    item: ProductModel;
-    deleteItem: (index: number) => void;
-    changeItemQuantity: (
-        event: React.ChangeEvent<HTMLInputElement>,
-        index: number
-    ) => void;
+  index: number;
+  item: ProductModel;
+  deleteItem: (index: number) => void;
+  changeItemQuantity: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => void;
 };
 
 const formatPrice = (price: number): string => {
-    return `$${price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+  return `$${price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
 };
 
-const CartItem = ({ index, item, changeItemQuantity, deleteItem }: CartItemProps) => {
-    const { productName, productDescription, productSalePrice, averageRating, quantity } = item;
+const CartItem = ({
+  index,
+  item,
+  changeItemQuantity,
+  deleteItem,
+}: CartItemProps): JSX.Element => {
+  const {
+    productName,
+    productDescription,
+    productSalePrice,
+    averageRating,
+    quantity,
+  } = item;
 
-    return (
-        <div className="CartItem">
-            <div className="CartItem-info">
-                <div className="info-title">
-                    <h2>{productName}</h2>
-                </div>
-                <div className="info-description">
-                    <p>{productDescription}</p>
-                </div>
-                <div className="info-rating">
-                    <p>Average Rating: {averageRating} / 5</p>
-                </div>
-
-                <div className="item-actions">
-                    <div className="item-quantity">
-                        <label htmlFor={`quantity-${index}`}>Quantity: </label>
-                        <input
-                            type="number"
-                            id={`quantity-${index}`}
-                            min="1"
-                            value={quantity}
-                            onChange={(event) => changeItemQuantity(event, index)}
-                            pattern="\d*" //prevents non-numeric input
-                        />
-                    </div>
-                    <div className="item-actions-divider">|</div>
-                    <div className="item-delete" onClick={() => deleteItem(index)}>
-                        <button className="delete-button">Delete</button>
-                    </div>
-                </div>
-            </div>
-            <div className="CartItem-price">
-                {formatPrice(productSalePrice)}
-            </div>
+  return (
+    <div className="CartItem">
+      <div className="CartItem-info">
+        <div className="info-title">
+          <h2>{productName}</h2>
         </div>
-    );
+        <div className="info-description">
+          <p>{productDescription}</p>
+        </div>
+        <div className="info-rating">
+          <p>Average Rating: {averageRating} / 5</p>
+        </div>
+
+        <div className="item-actions">
+          <div className="item-quantity">
+            <label htmlFor={`quantity-${index}`}>Quantity: </label>
+            <input
+              type="number"
+              id={`quantity-${index}`}
+              min="1"
+              value={quantity}
+              onChange={event => changeItemQuantity(event, index)}
+              pattern="\d*" //prevents non-numeric input
+            />
+          </div>
+          <div className="item-actions-divider">|</div>
+          <div className="item-delete" onClick={() => deleteItem(index)}>
+            <button className="delete-button">Delete</button>
+          </div>
+        </div>
+      </div>
+      <div className="CartItem-price">{formatPrice(productSalePrice)}</div>
+    </div>
+  );
 };
 
 export default CartItem;
--- petclinic-frontend/src/features/carts/components/UserCart.tsx
+++ petclinic-frontend/src/features/carts/components/UserCart.tsx
@@ -1,72 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import CartItem from './CartItem.tsx';
 import { ProductModel } from '../models/ProductModel';
 import './UserCart.css';
 
-const UserCart = () => {
-    const { cartId } = useParams<{ cartId: string }>();
-    const [cartItems, setCartItems] = useState<ProductModel[]>([]);
-    const [fixedPrice, setFixedPrice] = useState<number[]>([]);
-    const [error, setError] = useState<string | null>(null);
+const UserCart = (): JSX.Element => {
+  const { cartId } = useParams<{ cartId: string }>();
+  const [cartItems, setCartItems] = useState<ProductModel[]>([]);
+  const [fixedPrice, setFixedPrice] = useState<number[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchCartItems = async () => {
-            try {
-                const response = await axios.get(`/api/v2/gateway/carts/${cartId}`);
-                const products = response.data.products; // Assuming 'products' is the field containing cart items
-                setCartItems(products);
+  useEffect((): void => {
+    const fetchCartItems = async (): Promise<void> => {
+      try {
+        const response = await axios.get(`/api/v2/gateway/carts/${cartId}`);
+        const products = response.data.products; // Assuming 'products' is the field containing cart items
+        setCartItems(products);
 
-                // Store initial fixed prices for each item
-                const initialPrices = products.map((item: ProductModel) => item.productSalePrice);
-                setFixedPrice(initialPrices);
-            } catch (error) {
-                console.error('Error fetching cart items:', error);
-                setError('Failed to fetch cart items');
-            }
-        };
-
-        fetchCartItems();
-    }, [cartId]);
-
-    const changeItemQuantity = (
-        event: React.ChangeEvent<HTMLInputElement>,
-        index: number,
-    ) => {
-        const newItems = [...cartItems];
-        const newQuantity = +event.target.value;
-        newItems[index].quantity = newQuantity;
-        newItems[index].productSalePrice = fixedPrice[index] * newQuantity;
-        setCartItems(newItems);
+        // Store initial fixed prices for each item
+        const initialPrices = products.map(
+          (item: ProductModel) => item.productSalePrice
+        );
+        setFixedPrice(initialPrices);
+      } catch (error) {
+        console.error('Error fetching cart items:', error);
+        setError('Failed to fetch cart items');
+      }
     };
 
-    const deleteItem = (indexToDelete: number) => {
-        const newItems = cartItems.filter((_item, index) => index !== indexToDelete);
-        setCartItems(newItems);
-    };
+    fetchCartItems();
+  }, [cartId]);
 
-    if (error) {
-        return <div>{error}</div>;
-    }
+  const changeItemQuantity = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ): void => {
+    const newItems = [...cartItems];
+    const newQuantity = +event.target.value;
+    newItems[index].quantity = newQuantity;
+    newItems[index].productSalePrice = fixedPrice[index] * newQuantity;
+    setCartItems(newItems);
+  };
 
-    return (
-        <div className="CartItems">
-            <h1>User Cart</h1>
-            <hr />
-            <div className="CartItems-items">
-                {cartItems.map((item, index) => (
-                    <CartItem
-                        key={index}
-                        index={index}
-                        item={item}
-                        changeItemQuantity={changeItemQuantity}
-                        deleteItem={deleteItem}
-                    />
-                ))}
-            </div>
-        </div>
+  const deleteItem = (indexToDelete: number): void => {
+    const newItems = cartItems.filter(
+      (_item, index) => index !== indexToDelete
     );
+    setCartItems(newItems);
+  };
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className="CartItems">
+      <h1>User Cart</h1>
+      <hr />
+      <div className="CartItems-items">
+        {cartItems.map((item, index) => (
+          <CartItem
+            key={index}
+            index={index}
+            item={item}
+            changeItemQuantity={changeItemQuantity}
+            deleteItem={deleteItem}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default UserCart;
