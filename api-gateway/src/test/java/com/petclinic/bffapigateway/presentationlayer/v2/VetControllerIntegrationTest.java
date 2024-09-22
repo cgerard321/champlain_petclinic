347809bf953d86720ab0ae5f959a5854ae5cd48c
hivanov11 <hristogr85@gmail.com>
2024-09-22 19:04:30 -0400

    Created a new branch and put everything that I did about my story, created test in the api-gateway Controller, put my method in the api-gateway controller , fixed old test in the vet-service, created VetDetails Page and made the link from the list of vets to the VetDetails page

--- api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v2/VetControllerIntegrationTest.java
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v2/VetControllerIntegrationTest.java
@@ -30,6 +30,7 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.UUID;
 
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
 import static com.petclinic.bffapigateway.presentationlayer.v2.mockservers.MockServerConfigAuthService.jwtTokenForInvalidOwnerId;
 import static com.petclinic.bffapigateway.presentationlayer.v2.mockservers.MockServerConfigAuthService.jwtTokenForValidAdmin;
 import static org.junit.jupiter.api.Assertions.*;
@@ -57,6 +58,8 @@ class VetControllerIntegrationTest {
         mockServerConfigAuthService = new MockServerConfigAuthService();
         mockServerConfigAuthService.registerValidateTokenForAdminEndpoint();
         mockServerConfigAuthService.registerValidateTokenForVetEndpoint();
+        mockServerConfigVetService.registerGetVetByIdEndpoint();
+        mockServerConfigVetService.registerGetVetByInvalidIdEndpoint();
 
     }
 
@@ -187,9 +190,84 @@ class VetControllerIntegrationTest {
                 .exchange()
                 .expectStatus().isNotFound();
     }
+    @Test
+    public void getVetById_ValidId_ReturnsVet() {
+        String validVetId = "ac9adeb8-625b-11ee-8c99-0242ac120002";
 
+        VetResponseDTO expectedVetResponse = VetResponseDTO.builder()
+                .vetId(validVetId)
+                .vetBillId("5")
+                .firstName("Henry")
+                .lastName("Stevens")
+                .email("stevenshenry@email.com")
+                .phoneNumber("(514)-634-8276 #2389")
+                .resume("Practicing since 1 years")
+                .workday(Set.of(Workday.Wednesday, Workday.Tuesday, Workday.Thursday, Workday.Monday))
+                .workHoursJson("{\"Thursday\":[\"Hour_8_9\",\"Hour_9_10\",\"Hour_10_11\",\"Hour_11_12\"],"
+                        + "\"Monday\":[\"Hour_8_9\",\"Hour_9_10\",\"Hour_10_11\",\"Hour_11_12\","
+                        + "\"Hour_12_13\",\"Hour_13_14\",\"Hour_14_15\",\"Hour_15_16\"],"
+                        + "\"Wednesday\":[\"Hour_10_11\",\"Hour_11_12\",\"Hour_12_13\",\"Hour_13_14\","
+                        + "\"Hour_14_15\",\"Hour_15_16\",\"Hour_16_17\",\"Hour_17_18\"],"
+                        + "\"Tuesday\":[\"Hour_12_13\",\"Hour_13_14\",\"Hour_14_15\",\"Hour_15_16\","
+                        + "\"Hour_16_17\",\"Hour_17_18\",\"Hour_18_19\",\"Hour_19_20\"]}")
+                .active(false)
+                .specialties(Set.of(
+                        SpecialtyDTO.builder()
+                                .specialtyId("surgery")
+                                .name("surgery")
+                                .build(),
+                        SpecialtyDTO.builder()
+                                .specialtyId("radiology")
+                                .name("radiology")
+                                .build()))
+                .build();
 
+        webTestClient.get()
+                .uri(VET_ENDPOINT + "/" + validVetId)
+                .header(AUTHORIZATION, BEARER_TOKEN)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(VetResponseDTO.class)
+                .consumeWith(response -> {
+                    VetResponseDTO vetResponse = response.getResponseBody();
+                    assertNotNull(vetResponse);
+                    assertEquals(expectedVetResponse.getVetId(), vetResponse.getVetId());
+                    assertEquals(expectedVetResponse.getFirstName(), vetResponse.getFirstName());
+                    assertEquals(expectedVetResponse.getLastName(), vetResponse.getLastName());
+                    assertEquals(expectedVetResponse.getEmail(), vetResponse.getEmail());
+                    assertEquals(expectedVetResponse.getPhoneNumber(), vetResponse.getPhoneNumber());
+                    assertEquals(expectedVetResponse.getResume(), vetResponse.getResume());
+                    assertEquals("Henry", vetResponse.getFirstName());
+                    assertEquals("Stevens", vetResponse.getLastName());
+                    assertEquals("stevenshenry@email.com", vetResponse.getEmail());
+                    assertEquals("(514)-634-8276 #2389", vetResponse.getPhoneNumber());
+                    assertEquals("Practicing since 1 years", vetResponse.getResume());
+                    assertEquals(expectedVetResponse.getWorkday(), vetResponse.getWorkday());
+                    assertEquals(expectedVetResponse.getWorkHoursJson(), vetResponse.getWorkHoursJson());
+                    assertEquals(expectedVetResponse.isActive(), vetResponse.isActive());
+                    assertEquals(expectedVetResponse.getSpecialties().size(), vetResponse.getSpecialties().size());
+                    assertTrue(vetResponse.getSpecialties().stream().anyMatch(specialty -> specialty.getName().equals("surgery")));
+                    assertTrue(vetResponse.getSpecialties().stream().anyMatch(specialty -> specialty.getName().equals("radiology")));
+                });
+    }
 
+    @Test
+    public void getVetById_InvalidId_ReturnsNotFound() {
+        String invalidVetId = "ac9adeb8-625b-11ee-8c99-0242ac12000200000";
 
-
+        webTestClient.get()
+                .uri(VET_ENDPOINT + "/" + invalidVetId)
+                .header(AUTHORIZATION, BEARER_TOKEN)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(String.class)
+                .consumeWith(response -> {
+                    String responseBody = response.getResponseBody();
+                    assertNotNull(responseBody);
+                    assertTrue(responseBody.contains("vetId not found: ac9adeb8-625b-11ee-8c99-0242ac12000200000"));
+                });
+    }
 }
+
