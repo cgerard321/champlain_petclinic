6d156327658855e1ba69bc52fafb6b7d937a274f
VladLoghin <92529319+VladLoghin@users.noreply.github.com>
2024-09-21 21:05:50 -0400

    added visitStartDate and visitEndDate:
    Changed the visitDate name to visitStartDate to reflect what the field is supposed to project better.
    
    The visitEndDate, reflect when the visit is supposed to end, always at an interval of 1 hour from the visitStartDate
    
    testing further
    
    No merge conflicts done
    
    finalized and tested version of the EndDate that works

--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DataLayer/DataSetupService.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/DataLayer/DataSetupService.java
@@ -21,6 +21,10 @@ public class DataSetupService implements CommandLineRunner {
     private final VisitRepo visitRepo;
     private final ReviewRepository reviewRepository;
 
+    /**
+     * This is an event function ran at the start to populate visitRepo. Sort of like a main
+     * @throws Exception
+     */
     @Override
     public void run(String... args) throws Exception {
        setupVisits();
