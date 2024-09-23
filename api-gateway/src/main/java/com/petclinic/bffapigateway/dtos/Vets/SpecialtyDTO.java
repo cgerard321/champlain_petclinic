949bfb5c673dbc4e959520c6e28cebacc53ef87f
hristoiv11 <132388038+hristoiv11@users.noreply.github.com>
2024-09-22 20:21:16 -0400

    Feat/vets cpc 1074 get vet by vet Id (#740)
    JIRA: https://champlainsaintlambert.atlassian.net/browse/CPC-1074
    
    Context:
    By clicking on the veterinarian's name in the "Veterinarians" tab, it
    brings you to the veterinarian details page.
    
    Changes
    Created the VetDetails page which can be accessed by valid vetId and
    gives information about the veterinarian.
    Created my method inside the VetController in the v2 folder inside the
    api-gateway.
    Created tests for the "VetControllerIntegrationTest" inside the
    api-gateway.
    Fixed tests inside the vet-service.
    
    BEFORE: it was a blank page, and you couldn't access the VetDetails page
    with Id
    AFTER:
    
    ![image](https://github.com/user-attachments/assets/48d042c4-e548-49dd-b175-ede196239aa8)
    
    ![image](https://github.com/user-attachments/assets/59a05e4a-a1d3-4e88-94ff-f65442b74b70)
    
    ![image](https://github.com/user-attachments/assets/92fd3dbb-ae11-46b9-ac2b-fbd911f06bc2)
    
    ![image](https://github.com/user-attachments/assets/3ced4680-7644-4d9c-8ce0-c971f3335de9)

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Vets/SpecialtyDTO.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Vets/SpecialtyDTO.java
@@ -6,7 +6,15 @@ import lombok.*;
 
 @Data
 @Builder
+@NoArgsConstructor
+@Getter
 public class SpecialtyDTO {
     private String specialtyId;
     private String name;
+
+    public SpecialtyDTO(String specialtyId, String name) {
+        this.specialtyId = specialtyId;
+        this.name = name;
+    }
+
 }
