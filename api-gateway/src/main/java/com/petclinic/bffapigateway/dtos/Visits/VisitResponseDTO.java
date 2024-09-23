6a75ac27129d86ef3caffe5e06f4b84d230a2b6b
VladLoghin <92529319+VladLoghin@users.noreply.github.com>
2024-09-23 01:02:08 -0400

    adapted the AddingVisit.tsx to support the new VisitEndDate

--- api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Visits/VisitResponseDTO.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/dtos/Visits/VisitResponseDTO.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 public class VisitResponseDTO {
+    private String visitId;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime visitStartDate;
@@ -27,7 +28,6 @@ public class VisitResponseDTO {
     private String vetEmail;
     private String vetPhoneNumber;
     private Status status;
-    private String visitId;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime visitEndDate;
 }
