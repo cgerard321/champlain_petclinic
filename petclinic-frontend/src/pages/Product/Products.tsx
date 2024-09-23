61cc7df5b0546cd6fba3e8fd66e363f79cdbc945
Jessy Gjerek <118135201+J91-cloud@users.noreply.github.com>
2024-09-23 13:10:42 -0400

    Feat/prod cpc 1030 add search by product name front end3001 (#763)
    **JIRA:** : https://champlainsaintlambert.atlassian.net/browse/CPC-1030
    
    ## Context:
    This feature allows people to search for a specific product by its name.
    It is not case sensitive. When clikcing on the name of the product that
    pops up it will display all of the products information.
    
    This feature does not enteract with the .vscode folder in the front-end.
    
    
    ## Changes
    
    Added another component and css file that should search for a specific
    product. I then created an instance of the component inside our products
    page.
    
    ## Before and After UI (Required for UI-impacting PRs)
    
    ![Screenshot 2024-09-23
    112643](https://github.com/user-attachments/assets/71786148-ee56-43ee-96a8-3dc5a02531bf)

--- petclinic-frontend/src/pages/Product/Products.tsx
+++ petclinic-frontend/src/pages/Product/Products.tsx
@@ -1,17 +1,18 @@
 import { NavBar } from '@/layouts/AppNavBar.tsx';
 import ProductsList from '@/features/products/ProductsList.tsx';
+import ProductSearch from '@/features/products/components/ProductSearch';
+import './Products.css';
 import TrendingList from '@/features/products/TrendlingList';
 
 export default function Products(): JSX.Element {
   return (
     <div>
       <NavBar />
+      <ProductSearch />
       <h1>Products</h1>
       <h3>Here are the trending Products</h3>
       <TrendingList />
-
       <hr />
-
       <ProductsList />
     </div>
   );
