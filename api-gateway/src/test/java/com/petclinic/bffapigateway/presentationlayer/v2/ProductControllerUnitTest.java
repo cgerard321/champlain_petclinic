99afac51df7a3667a346a78f32a16b2fd5a60d24
Zachary Lelièvre <93009892+ZacharyLelievre@users.noreply.github.com>
2024-09-23 01:13:45 -0400

    feat(prod cpc 1136): product view testing part 2  (#731)
    **JIRA:** https://champlainsaintlambert.atlassian.net/browse/CPC-1043
    ## Context:
    This ticket was about implementing all the tests for the getAll request
    in the product service and API Gateway, and renaming the product link to
    'Shop'.
    ## Does this PR change the .vscode folder in petclinic-frontend?:
    No, this PR does not change the .vscode folder in the
    petclinic-frontend.
    
    
    ## Changes
    
    - Added the GetAll tests for the product-service
    - Added the GetAll tests for the API-Gateway
    - Modified the product page link to "Shop"
    ## Before and After UI (Required for UI-impacting PRs)
    Before:
    
    ![image](https://github.com/user-attachments/assets/039136a4-9a66-4d94-b6d0-a14e883e4b50)
    After:
    
    ![image](https://github.com/user-attachments/assets/aa22ded2-8a5d-4b2d-84fb-b7b75368026d)
    
    ## Dev notes (Optional)
    Specific technical changes that should be noted
    ## Linked pull requests (Optional)
    [Pull request links
    ](https://github.com/cgerard321/champlain_petclinic/pull/639#issue-2527405557)
    
    ---------
    
    Co-authored-by: NtsoaElie <113870458+NtsoaElie@users.noreply.github.com>
    Co-authored-by: PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
    Co-authored-by: Drexjar <142905333+Drexjar@users.noreply.github.com>
    Co-authored-by: AlejandroBrnab <98438629+AlejandroBrnab@users.noreply.github.com>
    Co-authored-by: Aian Batoochirov <119423256+orell2j@users.noreply.github.com>
    Co-authored-by: kyrusho <131713245+kyrusho@users.noreply.github.com>
    Co-authored-by: AndrewBadIdea <132387897+AndrewBadIdea@users.noreply.github.com>
    Co-authored-by: PedroSilveira <96072825+bluement@users.noreply.github.com>
    Co-authored-by: Sunveer <142905066+Sunveerg@users.noreply.github.com>
    Co-authored-by: Felix Zhang <132387996+Apoleh@users.noreply.github.com>
    Co-authored-by: Debo <deemohuang@proton.me>
    Co-authored-by: Jesse Bourassa <129144730+Jesse-Bourassa@users.noreply.github.com>
    Co-authored-by: Zakaria_ <135060272+Zako563@users.noreply.github.com>
    Co-authored-by: Zakaria_ <135060272+tramway23@users.noreply.github.com>
    Co-authored-by: Thomas <90119345+ThomasBedard@users.noreply.github.com>

--- api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v2/ProductControllerUnitTest.java
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v2/ProductControllerUnitTest.java
@@ -10,11 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
@@ -48,6 +52,64 @@ class ProductControllerUnitTest {
             .productSalePrice(100.0)
             .averageRating(0.00)
             .build();
+
+    private final ProductResponseDTO productResponseDTO1 = ProductResponseDTO.builder()
+            .productId("06a7d573-bcab-4db3-956f-773324b92a80")
+            .productName("Dog Food")
+            .productDescription("Premium dog food")
+            .productSalePrice(49.99)
+            .averageRating(4.5)
+            .build();
+
+    private final ProductResponseDTO productResponseDTO2 = ProductResponseDTO.builder()
+            .productId("1501f30e-1db1-44b2-a555-bca6f64450e4")
+            .productName("Fish Tank Heater")
+            .productDescription("Submersible heater for tropical fish tanks")
+            .productSalePrice(14.99)
+            .averageRating(0.0)
+            .build();
+
+
+
+    @Test
+    void getAllProducts_whenProductsExist_thenReturnFluxProductResponseDTO() {
+
+        when(productsServiceClient.getAllProducts(null, null))
+                .thenReturn(Flux.just(productResponseDTO1,productResponseDTO2));
+
+        webTestClient.get()
+                .uri("/api/v2/gateway/products")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(ProductResponseDTO.class)
+                .value(productResponseDTOS -> {
+                    assertNotNull(productResponseDTOS);
+                    assertEquals(2, productResponseDTOS.size());
+                    assertEquals(productResponseDTO1.getProductId(), productResponseDTOS.get(0).getProductId());
+                    assertEquals(productResponseDTO2.getProductId(), productResponseDTOS.get(1).getProductId());
+                });
+        verify(productsServiceClient, times(1)).getAllProducts(null, null);
+    }
+
+    @Test
+    void getAllProducts_whenNoProductsExist_thenReturnEmptyFlux() {
+
+        when(productsServiceClient.getAllProducts(null, null))
+                .thenReturn(Flux.empty());
+
+        webTestClient.get()
+                .uri("/api/v2/gateway/products")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(ProductResponseDTO.class)
+                .value(productResponseDTOS -> {
+                    assertNotNull(productResponseDTOS);
+                    assertEquals(0, productResponseDTOS.size());
+                });
+        verify(productsServiceClient, times(1)).getAllProducts(null, null);
+    }
 
     @Test
     public void whenAddProduct_thenReturnProduct() {
