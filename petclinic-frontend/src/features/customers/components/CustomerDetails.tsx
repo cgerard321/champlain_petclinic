311618df337f81ec04b3980d66672ac4c23b87c9
Debo <deemohuang@proton.me>
2024-09-25 23:10:02 -0400

    feat/(CUST-CPC-1191): admin can update customer information on website (#777)
    **JIRA:** https://champlainsaintlambert.atlassian.net/browse/CPC-1191
    ## Context:
    Allows an admin to have the option to update a customer's information
    after selecting a customer.
    ## Does this PR change the .vscode folder in petclinic-frontend?:
    No
    ## Changes
    
    - Made ownerId a link that redirects to the CustomerDetails page in the
    AllOwners page.
    - Created an edit button in the CustomerDetails page that redirects to
    the admin update form.
    - Created the AdminUpdateForm component and implemented the API calls
    and input validations.
    
    ## Before and After UI (Required for UI-impacting PRs)
    Before:
    
    ![image](https://github.com/user-attachments/assets/21d5ca29-58b4-42ca-a433-0ce9b1c31772)
    
    After:
    
    ![image](https://github.com/user-attachments/assets/f6287a6a-f357-4012-b40a-5cdcae113c4b)
    
    ![image](https://github.com/user-attachments/assets/88d0bd71-cc37-4d2b-b1fe-d073ab633a75)
    
    ![image](https://github.com/user-attachments/assets/e3f05bc0-ac60-45e1-bf56-6a1c5504632b)
    
    
    ## Dev notes (Optional)
    The rest of the CustomerDetails page will be done in another
    [feature](https://champlainsaintlambert.atlassian.net/browse/CPC-1190).
    I only added an edit button and some placeholder information.

---
+++ petclinic-frontend/src/features/customers/components/CustomerDetails.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import './CustomerDetails.css';
+
+const CustomerDetails: FC = () => {
+  const { ownerId } = useParams<{ ownerId: string }>();
+  const navigate = useNavigate();
+
+  const handleEditClick = (): void => {
+    navigate(`/customers/${ownerId}/edit`);
+  };
+
+  return (
+    <div className="customer-details-card">
+      <h2>Customer Details</h2>
+      <p>
+        <strong>First Name: </strong>
+      </p>
+      <p>
+        <strong>Last Name: </strong>
+      </p>
+      <p>
+        <strong>Address: </strong>
+      </p>
+      <p>
+        <strong>City: </strong>
+      </p>
+      <p>
+        <strong>Province: </strong>
+      </p>
+      <p>
+        <strong>Telephone: </strong>
+      </p>
+      <div className="customer-details-buttons">
+        <button className="customer-details-button" onClick={handleEditClick}>
+          Edit Customer
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerDetails;
