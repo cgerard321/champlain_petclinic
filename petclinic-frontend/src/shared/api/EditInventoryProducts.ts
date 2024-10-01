531362e0904837ef96d97dba440b3646e2df34b3
Sunveer <142905066+Sunveerg@users.noreply.github.com>
2024-09-30 21:35:14 -0400

    I implemented the edit inventory product feature with positive and negative testing

--- petclinic-frontend/src/shared/api/EditInventoryProducts.ts
+++ petclinic-frontend/src/shared/api/EditInventoryProducts.ts
@@ -3,22 +3,22 @@ import { ProductResponseModel } from '@/features/inventories/models/InventoryMod
 import { ProductRequestModel } from '@/features/inventories/models/InventoryModels/ProductRequestModel.ts';
 
 export const updateProductInInventory = async (
-    inventoryId: string,
-    productId: string,
-    product: ProductRequestModel
+  inventoryId: string,
+  productId: string,
+  product: ProductRequestModel
 ): Promise<void> => {
-    await axiosInstance.put<void>(
-        `inventories/${inventoryId}/products/${productId}`,
-        product
-    );
+  await axiosInstance.put<void>(
+    `inventories/${inventoryId}/products/${productId}`,
+    product
+  );
 };
 
 export const getProductByProductIdInInventory = async (
-    inventoryId: string,
-    productId: string
+  inventoryId: string,
+  productId: string
 ): Promise<ProductResponseModel> => {
-    const response = await axiosInstance.get<ProductResponseModel>(
-        `http://localhost:8080/api/v2/gateway/inventories/${inventoryId}/products/${productId}`
-    );
-    return response.data;
+  const response = await axiosInstance.get<ProductResponseModel>(
+    `http://localhost:8080/api/v2/gateway/inventories/${inventoryId}/products/${productId}`
+  );
+  return response.data;
 };
