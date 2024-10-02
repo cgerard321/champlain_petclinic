d308918f861c299489548c024d76aa2cdfa9a696
Sunveer <142905066+Sunveerg@users.noreply.github.com>
2024-10-02 18:28:11 -0400

    I added the back button to be able to go back into inventories from products and some changes to the UI of edit product inventory form

--- petclinic-frontend/src/features/inventories/InventoryProducts.tsx
+++ petclinic-frontend/src/features/inventories/InventoryProducts.tsx
@@ -28,7 +28,7 @@ const InventoryProducts: React.FC = () => {
       setError(null);
       try {
         const response = await axios.get<ProductModel[]>(
-            `http://localhost:8080/api/gateway/inventory/${inventoryId}/products`
+          `http://localhost:8080/api/gateway/inventory/${inventoryId}/products`
         );
         setProducts(response.data);
         setProductList(response.data); // Set productList as well
@@ -49,11 +49,11 @@ const InventoryProducts: React.FC = () => {
   const deleteProduct = async (productId: string): Promise<void> => {
     try {
       await axios.delete(
-          `http://localhost:8080/api/gateway/inventory/${inventoryId}/products/${productId}`
+        `http://localhost:8080/api/gateway/inventory/${inventoryId}/products/${productId}`
       );
       // Filter out the deleted product from both lists
       const updatedProducts = products.filter(
-          product => product.productId !== productId
+        product => product.productId !== productId
       );
       setProducts(updatedProducts);
       setFilteredProducts(updatedProducts); // Update filteredProducts as well
@@ -73,9 +73,9 @@ const InventoryProducts: React.FC = () => {
     // Call the backend only if name or description is provided
     if (productName || productDescription) {
       await getProductList(
-          inventoryId!,
-          productName || undefined,
-          productDescription || undefined
+        inventoryId!,
+        productName || undefined,
+        productDescription || undefined
       );
     }
 
@@ -89,9 +89,9 @@ const InventoryProducts: React.FC = () => {
     // Apply frontend status filtering on the updated productList from the backend
     if (productList) {
       setFilteredProducts(
-          productList.filter(
-              product => !productStatus || product.status === productStatus
-          )
+        productList.filter(
+          product => !productStatus || product.status === productStatus
+        )
       );
     }
   }, [productList, productStatus]); // Trigger this effect when productList or productStatus changes
@@ -101,121 +101,121 @@ const InventoryProducts: React.FC = () => {
   if (error) return <p>{error}</p>;
 
   return (
-      <div className="inventory-supplies">
-        <h2 className="inventory-title">
-          Supplies in Inventory: <span>{inventoryId}</span>
-        </h2>
+    <div className="inventory-supplies">
+      <h2 className="inventory-title">
+        Supplies in Inventory: <span>{inventoryId}</span>
+      </h2>
 
-        <button
-            className="btn btn-secondary"
-            onClick={() => navigate('/inventories')}
-        >
-          Back
-        </button>
+      <button
+        className="btn btn-secondary"
+        onClick={() => navigate('/inventories')}
+      >
+        Back
+      </button>
 
-        <br></br>
+      <br></br>
 
-        <div className="products-filtering">
-          <div className="filter-by-name">
-            <label htmlFor="product-name">Filter by Name:</label>
-            <input
-                type="text"
-                id="product-name"
-                placeholder="Enter product name"
-                onChange={e => setProductName(e.target.value)}
-                onKeyUp={e => e.key === 'Enter' && handleFilter()}
-            />
-          </div>
-
-          <div className="filter-by-description">
-            <label htmlFor="product-description">Filter by Description:</label>
-            <input
-                type="text"
-                id="product-description"
-                placeholder="Enter product description"
-                onChange={e => setProductDescription(e.target.value)}
-                onKeyUp={e => e.key === 'Enter' && handleFilter()}
-            />
-          </div>
-
-          <div className="filter-by-status">
-            <label htmlFor="product-status">Filter by Status:</label>
-            <select
-                id="product-status"
-                onChange={e => setProductStatus(e.target.value)}
-                onSelect={() => handleFilter()}
-            >
-              <option value="">All</option>
-              <option value="AVAILABLE">Available</option>
-              <option value="OUT_OF_STOCK">Out of Stock</option>
-              <option value="RE_ORDER">Re-Order</option>
-            </select>
-          </div>
+      <div className="products-filtering">
+        <div className="filter-by-name">
+          <label htmlFor="product-name">Filter by Name:</label>
+          <input
+            type="text"
+            id="product-name"
+            placeholder="Enter product name"
+            onChange={e => setProductName(e.target.value)}
+            onKeyUp={e => e.key === 'Enter' && handleFilter()}
+          />
         </div>
 
-        {/* Product Table */}
-        {filteredProducts.length > 0 ? (
-            <table className="table table-striped">
-              <thead>
-              <tr>
-                <th>SupplyId</th>
-                <th>SupplyName</th>
-                <th>Description</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Status</th>
-                <th colSpan={2}>Actions</th>
-              </tr>
-              </thead>
-              <tbody>
-              {filteredProducts.map((product: ProductModel) => (
-                  <tr key={product.productId}>
-                    <td>{product.productId}</td>
-                    <td>{product.productName}</td>
-                    <td>{product.productDescription}</td>
-                    <td>${product.productSalePrice}</td>
-                    <td>{product.productQuantity}</td>
-                    <td
-                        style={{
-                          color:
-                              product.status === 'RE_ORDER'
-                                  ? '#f4a460'
-                                  : product.status === 'OUT_OF_STOCK'
-                                      ? 'red'
-                                      : product.status === 'AVAILABLE'
-                                          ? 'green'
-                                          : 'inherit',
-                        }}
-                    >
-                      {product.status.replace('_', ' ')}
-                    </td>
-                    <td>
-                      <button
-                          onClick={e => {
-                            e.stopPropagation();
-                            navigate(`${product.productId}/edit`);
-                          }}
-                          className="btn btn-warning"
-                      >
-                        Edit
-                      </button>
-                    </td>
-                    <td>
-                      <button
-                          className="btn btn-danger"
-                          onClick={() => deleteProduct(product.productId)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-              ))}
-              </tbody>
-            </table>
-        ) : (
-            <p>No supplies found for this inventory.</p>
-        )}
+        <div className="filter-by-description">
+          <label htmlFor="product-description">Filter by Description:</label>
+          <input
+            type="text"
+            id="product-description"
+            placeholder="Enter product description"
+            onChange={e => setProductDescription(e.target.value)}
+            onKeyUp={e => e.key === 'Enter' && handleFilter()}
+          />
+        </div>
+
+        <div className="filter-by-status">
+          <label htmlFor="product-status">Filter by Status:</label>
+          <select
+            id="product-status"
+            onChange={e => setProductStatus(e.target.value)}
+            onSelect={() => handleFilter()}
+          >
+            <option value="">All</option>
+            <option value="AVAILABLE">Available</option>
+            <option value="OUT_OF_STOCK">Out of Stock</option>
+            <option value="RE_ORDER">Re-Order</option>
+          </select>
+        </div>
       </div>
+
+      {/* Product Table */}
+      {filteredProducts.length > 0 ? (
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>SupplyId</th>
+              <th>SupplyName</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Status</th>
+              <th colSpan={2}>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredProducts.map((product: ProductModel) => (
+              <tr key={product.productId}>
+                <td>{product.productId}</td>
+                <td>{product.productName}</td>
+                <td>{product.productDescription}</td>
+                <td>${product.productSalePrice}</td>
+                <td>{product.productQuantity}</td>
+                <td
+                  style={{
+                    color:
+                      product.status === 'RE_ORDER'
+                        ? '#f4a460'
+                        : product.status === 'OUT_OF_STOCK'
+                          ? 'red'
+                          : product.status === 'AVAILABLE'
+                            ? 'green'
+                            : 'inherit',
+                  }}
+                >
+                  {product.status.replace('_', ' ')}
+                </td>
+                <td>
+                  <button
+                    onClick={e => {
+                      e.stopPropagation();
+                      navigate(`${product.productId}/edit`);
+                    }}
+                    className="btn btn-warning"
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteProduct(product.productId)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No supplies found for this inventory.</p>
+      )}
+    </div>
   );
 };
 
