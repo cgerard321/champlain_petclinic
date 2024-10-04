fbcabadc5a83a7ae3fbd58a573b4bf8c38ce0a36
Debo <deemohuang@proton.me>
2024-10-04 18:40:02 -0400

    lint

--- petclinic-frontend/src/pages/Customer/AddPetPage.tsx
+++ petclinic-frontend/src/pages/Customer/AddPetPage.tsx
@@ -3,11 +3,11 @@ import { NavBar } from '@/layouts/AppNavBar.tsx';
 import AddPetForm from '@/features/customers/components/AddPetForm.tsx';
 
 const AddPetPage: FC = (): JSX.Element => {
- return (
-  <div>
-   <NavBar />
-   <AddPetForm />
-  </div>
- );
+  return (
+    <div>
+      <NavBar />
+      <AddPetForm />
+    </div>
+  );
 };
 export default AddPetPage;
