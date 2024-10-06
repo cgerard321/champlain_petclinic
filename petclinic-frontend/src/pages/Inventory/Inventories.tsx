52d6db296d08b39555af7bb7c43116040ca09430
Carlos-123321 <142922516+Carlos-123321@users.noreply.github.com>
2024-10-06 13:56:23 -0400

    feat(INVT-CPC-1239): complete integration of cards (#873)
    **JIRA:** https://champlainsaintlambert.atlassian.net/browse/CPC-1239
    ## Context:
    This ticket is about fully integrating our cards with our user interface
    and deleting the ui list of rows of inventories we had before.
    ## Does this PR change the .vscode folder in petclinic-frontend?:
    **No, it doesn't**
    ## Changes
    •**Removed** rows of inventories
    •**Added** menu to each card
    •**Added** checkbox to each card
    ## Before and After UI (Required for UI-impacting PRs)
    
    **Before** this is how the site looked like:
    
    
    ![image](https://github.com/user-attachments/assets/ac055087-0725-48a6-9874-258dd551453c)
    
    **After** this is how the site looks like:
    
    
    ![image](https://github.com/user-attachments/assets/596844ce-54c5-4de1-96bb-9fa5fe6f3c6c)
    
    This is how the menu looks like:
    
    
    ![image](https://github.com/user-attachments/assets/e125cc34-b047-41f9-b38a-113e572ad8e5)
    
    ## Dev notes (Optional)
    Specific technical changes that should be noted
    ## Linked pull requests (Optional)
    Pull request links

--- petclinic-frontend/src/pages/Inventory/Inventories.tsx
+++ petclinic-frontend/src/pages/Inventory/Inventories.tsx
@@ -1,12 +1,14 @@
 import { NavBar } from '@/layouts/AppNavBar.tsx';
 import InventoriesListTable from '@/features/inventories/InventoriesListTable.tsx';
-import './Inventories.css';
+import inventoryPageStyles from './Inventories.module.css';
 
 export default function Inventories(): JSX.Element {
   return (
     <div>
       <NavBar />
-      <h1 id="page-title">Inventories</h1>
+      <div id={inventoryPageStyles.titleSection}>
+        <h1 id={inventoryPageStyles.pageTitle}>Inventories</h1>
+      </div>
       <InventoriesListTable />
     </div>
   );
