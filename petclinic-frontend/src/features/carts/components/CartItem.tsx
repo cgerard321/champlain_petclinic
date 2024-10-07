88eeba675b0f4ad77ba8230e07083f3b1dcf29e8
AlexCristea04 <alexcristea004@gmail.com>
2024-10-06 20:24:08 -0400

    Fixed eslint issues

--- petclinic-frontend/src/features/carts/components/CartItem.tsx
+++ petclinic-frontend/src/features/carts/components/CartItem.tsx
@@ -23,48 +23,47 @@ const CartItem = ({
   deleteItem,
 }: CartItemProps): JSX.Element => {
   const remainingStock = item.productQuantity - (item.quantity ?? 0);
-    return (
-        <div className="CartItem">
-            <div className="CartItem-info">
-                <h2 className="info-title">{item.productName}</h2>
-                <p className="info-description">{item.productDescription}</p>
-            </div>
-            <div className="CartItem-details">
-                <div className="item-quantity">
-                    <input
-                        type="number"
-                        min="1"
-                        max={item.productQuantity}
-                        value={item.quantity || 1}
-                        onChange={e => changeItemQuantity(e, index)}
-                        onBlur={e => changeItemQuantity(e, index)}
-                        aria-label={`Quantity of ${item.productName}`}
-                    />
-                </div>
-                <span className="CartItem-price">
-                {formatPrice(item.productSalePrice)}
-            </span>
-                <button
-                    className="delete-button"
-                    onClick={() => deleteItem(index)}
-                    aria-label={`Remove ${item.productName} from cart`}
-                >
-                    Remove
-                </button>
-            </div>
-
-            <div className="stock-message-container">
-                {remainingStock <= 5 && remainingStock > 0 ? (
-                    <div className="stock-message">
-                        Only {remainingStock} items left in stock.
-                    </div>
-                ) : remainingStock === 0 ? (
-                    <div className="stock-message out-of-stock">Out of stock</div>
-                ) : null}
-            </div>
+  return (
+    <div className="CartItem">
+      <div className="CartItem-info">
+        <h2 className="info-title">{item.productName}</h2>
+        <p className="info-description">{item.productDescription}</p>
+      </div>
+      <div className="CartItem-details">
+        <div className="item-quantity">
+          <input
+            type="number"
+            min="1"
+            max={item.productQuantity}
+            value={item.quantity || 1}
+            onChange={e => changeItemQuantity(e, index)}
+            onBlur={e => changeItemQuantity(e, index)}
+            aria-label={`Quantity of ${item.productName}`}
+          />
         </div>
-    );
+        <span className="CartItem-price">
+          {formatPrice(item.productSalePrice)}
+        </span>
+        <button
+          className="delete-button"
+          onClick={() => deleteItem(index)}
+          aria-label={`Remove ${item.productName} from cart`}
+        >
+          Remove
+        </button>
+      </div>
 
+      <div className="stock-message-container">
+        {remainingStock <= 5 && remainingStock > 0 ? (
+          <div className="stock-message">
+            Only {remainingStock} items left in stock.
+          </div>
+        ) : remainingStock === 0 ? (
+          <div className="stock-message out-of-stock">Out of stock</div>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
