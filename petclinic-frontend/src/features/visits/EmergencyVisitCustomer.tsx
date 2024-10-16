2f485b99a48d594501cda28f7ada173c24260250
Zakaria_ <135060272+tramway23@users.noreply.github.com>
2024-10-16 15:55:29 -0400

    fixing the error for my class

--- petclinic-frontend/src/features/visits/EmergencyVisitCustomer.tsx
+++ petclinic-frontend/src/features/visits/EmergencyVisitCustomer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { useUser } from '@/context/UserContext';
 import { EmergencyResponseDTO } from './Emergency/Model/EmergencyResponseDTO';
