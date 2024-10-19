b718e2cfd50b5248fd0ec516544784ae0db9a2ef
Zachary Lelievre <lelievrezachary@gmail.com>
2024-10-18 23:55:58 -0400

    Fixed lint issues

--- petclinic-frontend/src/pages/Product/Products.tsx
+++ petclinic-frontend/src/pages/Product/Products.tsx
@@ -1,38 +1,39 @@
-import {NavBar} from '@/layouts/AppNavBar.tsx';
+import { NavBar } from '@/layouts/AppNavBar.tsx';
 import ProductsList from '@/features/products/ProductsList.tsx';
 import ProductSearch from '@/features/products/components/ProductSearch';
 import './Products.css';
 import TrendingList from '@/features/products/TrendingList';
 
 export default function Products(): JSX.Element {
-    return (
-        <div>
-            <NavBar/>
-            <header className="header-container">
-                <div className="overlay-text">
-                    <h1>Welcome to PetClinic Shop Page!</h1>
-                    <p>Discover a range of quality items to keep your pets healthy and
-                        happy.</p>
-                </div>
-                <img
-                    src="https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg"
-                    alt="Pets"
-                    className="full-width-image"
-                />
-            </header>
-            <div className="block">
-                <ProductSearch/>
-            </div>
-            <div className="trending-list-container-gold">
-
-                <h2>Trending Products</h2>
-                <TrendingList/>
-            </div>
-
-                <div className="block">
-                    <hr/>
-                </div>
-                <ProductsList/>
+  return (
+    <div>
+      <NavBar />
+      <header className="header-container">
+        <div className="overlay-text">
+          <h1>Welcome to PetClinic Shop Page!</h1>
+          <p>
+            Discover a range of quality items to keep your pets healthy and
+            happy.
+          </p>
         </div>
-    );
+        <img
+          src="https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg"
+          alt="Pets"
+          className="full-width-image"
+        />
+      </header>
+      <div className="block">
+        <ProductSearch />
+      </div>
+      <div className="trending-list-container-gold">
+        <h2>Trending Products</h2>
+        <TrendingList />
+      </div>
+
+      <div className="block">
+        <hr />
+      </div>
+      <ProductsList />
+    </div>
+  );
 }
