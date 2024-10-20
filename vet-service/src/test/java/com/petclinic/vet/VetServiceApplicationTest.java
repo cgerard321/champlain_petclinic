10bd78f75924783ed06718547967cfda55c10238
kyrusho <jshn2004@hotmail.com>
2024-10-19 21:26:53 -0400

    updated gradle version

--- vet-service/src/test/java/com/petclinic/vet/VetServiceApplicationTest.java
+++ vet-service/src/test/java/com/petclinic/vet/VetServiceApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ProductsServiceApplicationTests {
+class VetServiceApplicationTest {
 
     @Test
     void contextLoads() {
