a55ce8d95aa9c634973ae7c08b5a069080398fd2
Anvar Sultanov <anv.sult@gmail.com>
2025-09-25 23:32:22 -0400

    Removed unnecessary comments.

--- petclinic-frontend/src/features/visits/Emergency/Api/getAllEmergency.ts
+++ petclinic-frontend/src/features/visits/Emergency/Api/getAllEmergency.ts
@@ -3,18 +3,7 @@ import { EmergencyResponseDTO } from '../Model/EmergencyResponseDTO';
 
 export async function getAllEmergency(): Promise<EmergencyResponseDTO[]> {
   const response = await axiosInstance.get(`/visits/emergency`, {
-    // responseType: 'stream',
     useV2: true,
   });
-  return response.data // Return only the data
-    // .split('data:')
-    // .map((payLoad: string) => {
-    //   try {
-    //     if (payLoad == '') return null;
-    //     return JSON.parse(payLoad);
-    //   } catch (err) {
-    //     console.error("Can't parse JSON: " + err);
-    //   }
-    // })
-    // .filter((data?: JSON) => data !== null);
+  return response.data;
 }
