ad4a7beabdc978a91b2be20a74b0b371a5162659
Pkdotts <andybaole1234@gmail.com>
2025-09-25 20:21:53 -0400

    Created a basic sidebar on the visits page for the admin

--- petclinic-frontend/src/pages/Visit/Visit.tsx
+++ petclinic-frontend/src/pages/Visit/Visit.tsx
@@ -1,4 +1,5 @@
 import VisitListTable from '@/features/visits/VisitListTable';
+import SideBar from '@/features/visits/Sidebar';
 import { NavBar } from '@/layouts/AppNavBar';
 import './Visits.css';
 
@@ -6,8 +7,9 @@ export default function Visits(): JSX.Element {
   return (
     <>
       <NavBar />
+
       <div className="page-container">
-        <h2>Manage All Visits</h2>
+        <SideBar />
         <VisitListTable />
       </div>
     </>
