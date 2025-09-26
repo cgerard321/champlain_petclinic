1e4a42ca180dc7da2797eda57e1e39eda24d179b
Pkdotts <andybaole1234@gmail.com>
2025-09-26 01:48:46 -0400

    Made buttons to switch between tables functional

--- petclinic-frontend/src/pages/Visit/Visit.tsx
+++ petclinic-frontend/src/pages/Visit/Visit.tsx
@@ -1,5 +1,4 @@
 import VisitListTable from '@/features/visits/VisitListTable';
-import SideBar from '@/features/visits/Sidebar';
 import { NavBar } from '@/layouts/AppNavBar';
 import './Visits.css';
 
@@ -8,8 +7,7 @@ export default function Visits(): JSX.Element {
     <>
       <NavBar />
 
-      <div className="page-container">
-        <SideBar />
+      <div>
         <VisitListTable />
       </div>
     </>
