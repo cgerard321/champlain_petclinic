e6a92446bd3d7c7578cd678af0ee41a394652d54
Zeena Nzigamasabo <142906579+zeenanzigamasabo@users.noreply.github.com>
2025-09-27 11:36:50 -0400

    removed the second search and filter from the page

--- petclinic-frontend/src/features/products/ProductsList.tsx
+++ petclinic-frontend/src/features/products/ProductsList.tsx
@@ -10,83 +10,49 @@ import './components/Sidebar.css';
 //import { getProductsByType } from '@/features/products/api/getProductsByType.ts';
 import { addImage } from './api/addImage';
 import { ImageModel } from './models/ProductModels/ImageModel';
-import StarRating from '@/features/products/components/StarRating.tsx';
 import './components/StarRating.css';
-import { ProductType } from '@/features/products/api/ProductTypeEnum.ts';
 import { getAllProductBundles } from './api/getAllProductBundles';
 import { ProductBundleModel } from './models/ProductModels/ProductBundleModel';
 import ProductBundle from './components/ProductBundle';
-import ProductSearch from './components/ProductSearch';
 
 export default function ProductList({
   view,
+  filters,
 }: {
   view: 'catalog' | 'extras';
+  filters?: {
+    minPrice?: number;
+    maxPrice?: number;
+    ratingSort?: string;
+    minStars?: number;
+    maxStars?: number;
+    deliveryType?: string;
+    productType?: string;
+  };
 }): JSX.Element {
   const [productList, setProductList] = useState<ProductModel[]>([]);
   const [bundleList, setBundleList] = useState<ProductBundleModel[]>([]);
-  const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
-  const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { user } = useUser();
   const [isRightRole, setIsRightRole] = useState<boolean>(false);
-  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
-  const [filterType, setFilterType] = useState<string>('');
   const [recentlyClickedProducts, setRecentlyClickedProducts] = useState<
     ProductModel[]
   >([]);
-  const [ratingSort, setRatingSort] = useState<string>('default');
-  const [minStars, setMinStars] = useState<number>(0);
-  const [maxStars, setMaxStars] = useState<number>(5);
-  const [validationMessage, setValidationMessage] = useState<string>('');
-  const [deliveryType, setDeliveryType] = useState<string>('');
-  const [productType, setProductType] = useState<string>('');
-
-  const validationStars = async (
-    minStars: number,
-    maxStars: number
-  ): Promise<void> => {
-    if (minStars >= maxStars) {
-      setValidationMessage(
-        'Minimum stars cannot be greater than or equal to maximum stars.'
-      );
-    } else {
-      setValidationMessage('');
-    }
-  };
-
-  function FilterByPriceErrorHandling(): void {
-    // Validate inputs for filter by price
-    if (
-      minPrice !== undefined &&
-      maxPrice !== undefined &&
-      minPrice > maxPrice
-    ) {
-      alert('Min Price cannot be greater than Max Price');
-    }
-  }
-
   const fetchProducts = async (): Promise<void> => {
-    FilterByPriceErrorHandling();
     setIsLoading(true);
     try {
-      if (filterType.trim() === '') {
-        const list = await getAllProducts(
-          minPrice,
-          maxPrice,
-          minStars,
-          maxStars,
-          ratingSort,
-          deliveryType,
-          productType
-        );
-        const filteredList = list.filter(product => !product.isUnlisted);
-        setProductList(filteredList);
-      }
-      // } else {
-      //   const filteredList = await getProductsByType(filterType);
-      //   setProductList(filteredList);
-      // }
+      const list = await getAllProducts(
+        filters?.minPrice,
+        filters?.maxPrice,
+        filters?.minStars,
+        filters?.maxStars,
+        filters?.ratingSort ?? 'default',
+        filters?.deliveryType ?? '',
+        filters?.productType ?? ''
+      );
+
+      const filteredList = list.filter(product => !product.isUnlisted);
+      setProductList(filteredList);
     } catch (err) {
       console.error('Error fetching products:', err);
       setProductList([]);
@@ -101,7 +67,6 @@ export default function ProductList({
       console.error('Error fetching product bundles:', err);
     }
   };
-
   useEffect(() => {
     fetchProducts();
     const savedProducts = localStorage.getItem('recentlyClickedProducts');
@@ -109,7 +74,7 @@ export default function ProductList({
       setRecentlyClickedProducts(JSON.parse(savedProducts));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [filters]);
 
   useEffect(() => {
     const hasRightRole =
@@ -141,28 +106,6 @@ export default function ProductList({
     }
   };
 
-  const toggleSidebar = (): void => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
-  const handleOverlayClick = (): void => {
-    setIsSidebarOpen(false);
-  };
-
-  const clearFilters = async (): Promise<void> => {
-    setMinPrice(undefined);
-    setMaxPrice(undefined);
-    setFilterType('');
-    setRatingSort('');
-    setMaxStars(5);
-    setMinStars(0);
-    setValidationMessage('');
-    setDeliveryType('');
-    setProductType('');
-    const list = await getAllProducts(minPrice, maxPrice, minStars, maxStars);
-    setProductList(list);
-  };
-
   const handleProductClick = (product: ProductModel): void => {
     setRecentlyClickedProducts(listOfProducts => {
       const updatedProducts = listOfProducts.filter(
@@ -170,24 +113,19 @@ export default function ProductList({
       );
 
       updatedProducts.unshift(product);
-
-      if (updatedProducts.length > 5) {
-        updatedProducts.pop();
-      }
-
+      if (updatedProducts.length > 5) updatedProducts.pop();
       localStorage.setItem(
         'recentlyClickedProducts',
         JSON.stringify(updatedProducts)
       );
-
       return updatedProducts;
     });
   };
+
   const RecentlyViewedProducts = (): JSX.Element => (
     <div className="recently-viewed-container">
       <h2 className="section-header">Recently Seen</h2>
       <div className="recently-viewed-flex">
-        {' '}
         {recentlyClickedProducts.length > 0 ? (
           recentlyClickedProducts
             .filter(product => !product.isUnlisted)
@@ -203,225 +141,28 @@ export default function ProductList({
 
   return (
     <div className="product-list-container">
-      {isSidebarOpen && (
-        <div className="overlay" onClick={handleOverlayClick}></div>
-      )}
-
-      <div
-        className={`sidebar ${isSidebarOpen ? 'open' : ''}`}
-        id="sidebar"
-        aria-hidden={!isSidebarOpen}
-      >
-        <button
-          className="close-button"
-          onClick={toggleSidebar}
-          aria-label="Close Filters"
-        >
-          &times;
-        </button>
-        <div className="filter-container">
-          <h2>Filters</h2>
-          <label>
-            Min Price:
-            <input
-              type="number"
-              value={minPrice ?? typeof 'number'}
-              onChange={e =>
-                setMinPrice(
-                  e.target.value ? parseFloat(e.target.value) : undefined
-                )
-              }
-              min="0"
-              placeholder="e.g., 10"
-            />
-          </label>
-          <label>
-            Max Price:
-            <input
-              type="number"
-              value={maxPrice ?? typeof 'number'}
-              onChange={e =>
-                setMaxPrice(
-                  e.target.value ? parseFloat(e.target.value) : undefined
-                )
-              }
-              min="0"
-              placeholder="e.g., 100"
-            />
-          </label>
-          <label>
-            Item Type:
-            <select
-              value={productType}
-              onChange={e => setProductType(e.target.value)}
-            >
-              <option value="">Select Item Type</option>
-              {Object.values(ProductType).map(type => (
-                <option key={type} value={type}>
-                  {type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label>
-            Delivery Type:
-            <select
-              value={deliveryType}
-              onChange={e => setDeliveryType(e.target.value)}
-            >
-              <option value="">Sort by Delivery Type</option>
-              <option value="DELIVERY">Delivery</option>
-              <option value="PICKUP">Pickup</option>
-              <option value="DELIVERY_AND_PICKUP">Delivery & Pickup</option>
-              <option value="NO_DELIVERY_OPTION">No Delivery Option</option>
-            </select>
-          </label>
-          <div className="star-rating-container">
-            <h2>Filter by Star Rating</h2>
-            <div className="star-row">
-              <label>Min Stars:</label>
-              <StarRating
-                currentRating={minStars}
-                viewOnly={false}
-                updateRating={rating => {
-                  setMinStars(rating);
-                  validationStars(rating, maxStars);
-                }}
-              />
-            </div>
-            <div className="star-row">
-              <label>Max Stars:</label>
-              <StarRating
-                currentRating={maxStars}
-                viewOnly={false}
-                updateRating={rating => {
-                  setMaxStars(rating);
-                  validationStars(minStars, rating);
-                }}
-              />
-            </div>
-            {validationMessage && (
-              <div style={{ color: 'red' }}>{validationMessage}</div>
-            )}
-          </div>
-          <select
-            name="rating"
-            value={ratingSort}
-            onChange={e => setRatingSort(e.target.value)}
-          >
-            <option value="default">Sort by Rating</option>
-            <option value="asc">Low to High</option>
-            <option value="desc">High to Low</option>
-          </select>
-          <button
-            className="apply-filter-button"
-            disabled={validationMessage !== ''}
-            onClick={fetchProducts}
-          >
-            Apply
-          </button>
-          <button className="clear-filter-button" onClick={clearFilters}>
-            Clear
-          </button>
-        </div>
-      </div>
-
       {isRightRole && (
         <AddProduct addProduct={handleAddProduct} addImage={handleAddImage} />
       )}
-
-      {/*<div className="main-content">
-        <div className="product-bundle-container">
-          <h2>Bundles</h2>
-          <div className="grid product-bundles-grid">
-            {bundleList.length > 0 ? (
-              bundleList.map((bundle: ProductBundleModel) => (
-                <ProductBundle key={bundle.bundleId} bundle={bundle} />
-              ))
-            ) : (
-              <p>No Bundles Available.</p>
-            )}
-          </div>
-        </div>
-        <div>
-          <hr />
-        </div>
-        <div className="list-container">
-          <h2>Catalog</h2>
-
-
-          {!isSidebarOpen && (
-            <button
-              className="toggle-sidebar-button"
-              onClick={toggleSidebar}
-              aria-expanded={isSidebarOpen}
-              aria-controls="sidebar"
-            >
-              &#9776; Filters
-            </button>
-          )}
-
-          
-          <div className="grid">
-            {isLoading ? (
-              <p>Loading items...</p>
-            ) : productList.length > 0 ? (
-              productList.map((product: ProductModel) => (
-                <div
-                  key={product.productId}
-                  onClick={() => handleProductClick(product)}
-                >
-                  <Product key={product.productId} product={product} />
-                </div>
-              ))
-            ) : (
-              <p>No items found.</p>
-            )}
-          </div>
-        </div>
-        <div>
-          <hr />
-        </div>
-        <RecentlyViewedProducts />
-      </div>
-    </div>*/}
-
-      <div className="search-and-filter">
-        {!isSidebarOpen && (
-          <button
-            className="toggle-sidebar-button"
-            onClick={toggleSidebar}
-            aria-expanded={isSidebarOpen}
-            aria-controls="sidebar"
-          >
-            &#9776; Filters
-          </button>
-        )}
-
-        <div className="search-wrapper">
-          <ProductSearch />
-        </div>
-      </div>
 
       <div className="main-content">
         {view == 'catalog' && (
           <div className="list-container">
             <h2 className="section-header">Catalog</h2>
-
             <div className="grid">
               {isLoading ? (
                 <p>Loading items...</p>
               ) : productList.length > 0 ? (
-                productList.map((product: ProductModel) => (
+                productList.map(product => (
                   <div
                     key={product.productId}
                     onClick={() => handleProductClick(product)}
                   >
-                    <Product key={product.productId} product={product} />
+                    <Product product={product} />
                   </div>
                 ))
               ) : (
-                <p>No items found.</p>
+                <p>No items found</p>
               )}
             </div>
           </div>
--- petclinic-frontend/src/pages/Product/Products.tsx
+++ petclinic-frontend/src/pages/Product/Products.tsx
@@ -2,8 +2,49 @@ import { NavBar } from '@/layouts/AppNavBar.tsx';
 import ProductsList from '@/features/products/ProductsList.tsx';
 import './Products.css';
 import TrendingList from '@/features/products/TrendingList';
+import ProductSearch from '@/features/products/components/ProductSearch';
+import StarRating from '@/features/products/components/StarRating';
+import { ProductType } from '@/features/products/api/ProductTypeEnum';
+import { useState } from 'react';
 
 export default function Products(): JSX.Element {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+  const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
+  const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
+  const [ratingSort, setRatingSort] = useState<string>('default');
+  const [minStars, setMinStars] = useState<number>(0);
+  const [maxStars, setMaxStars] = useState<number>(5);
+  const [validationMessage, setValidationMessage] = useState<string>('');
+  const [deliveryType, setDeliveryType] = useState<string>('');
+  const [productType, setProductType] = useState<string>('');
+
+  const toggleSidebar = (): void => setIsSidebarOpen(!isSidebarOpen);
+  const handleOverlayClick = (): void => setIsSidebarOpen(false);
+
+  const validationStars = async (
+    minStars: number,
+    maxStars: number
+  ): Promise<void> => {
+    if (minStars >= maxStars) {
+      setValidationMessage(
+        'Minimum stars cannot be greater than or equal to maximum stars.'
+      );
+    } else {
+      setValidationMessage('');
+    }
+  };
+
+  const clearFilters = (): void => {
+    setMinPrice(undefined);
+    setMaxPrice(undefined);
+    setRatingSort('');
+    setMaxStars(5);
+    setMinStars(0);
+    setValidationMessage('');
+    setDeliveryType('');
+    setProductType('');
+  };
+
   return (
     <div>
       <NavBar />
@@ -22,27 +63,159 @@ export default function Products(): JSX.Element {
         />
       </header>
 
-      {/*      <div className="block">
-        <ProductSearch />
+      {isSidebarOpen && (
+        <div className="overlay" onClick={handleOverlayClick}></div>
+      )}
+
+      <div className="search-and-filter">
+        {!isSidebarOpen && (
+          <button
+            className="toggle-sidebar-button"
+            onClick={toggleSidebar}
+            aria-expanded={isSidebarOpen}
+            aria-controls="sidebar"
+          >
+            &#9776; Filters
+          </button>
+        )}
+
+        <div className="search-wrapper">
+          <ProductSearch />
+        </div>
       </div>
 
+      {isSidebarOpen && (
+        <div
+          className={`sidebar ${isSidebarOpen ? 'open' : ''}`}
+          id="sidebar"
+          aria-hidden={!isSidebarOpen}
+        >
+          <button
+            className="close-button"
+            onClick={toggleSidebar}
+            aria-label="Close Filters"
+          >
+            &times;
+          </button>
+          <div className="filter-container">
+            <h2>Filters</h2>
+            <label>
+              Min Price:
+              <input
+                type="number"
+                value={minPrice ?? typeof 'number'}
+                onChange={e =>
+                  setMinPrice(
+                    e.target.value ? parseFloat(e.target.value) : undefined
+                  )
+                }
+                min="0"
+                placeholder="e.g., 10"
+              />
+            </label>
+            <label>
+              Max Price:
+              <input
+                type="number"
+                value={maxPrice ?? typeof 'number'}
+                onChange={e =>
+                  setMaxPrice(
+                    e.target.value ? parseFloat(e.target.value) : undefined
+                  )
+                }
+                min="0"
+                placeholder="e.g., 100"
+              />
+            </label>
+            <label>
+              Item Type:
+              <select
+                value={productType}
+                onChange={e => setProductType(e.target.value)}
+              >
+                <option value="">Select Item Type</option>
+                {Object.values(ProductType).map(type => (
+                  <option key={type} value={type}>
+                    {type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <label>
+              Delivery Type:
+              <select
+                value={deliveryType}
+                onChange={e => setDeliveryType(e.target.value)}
+              >
+                <option value="">Sort by Delivery Type</option>
+                <option value="DELIVERY">Delivery</option>
+                <option value="PICKUP">Pickup</option>
+                <option value="DELIVERY_AND_PICKUP">Delivery & Pickup</option>
+                <option value="NO_DELIVERY_OPTION">No Delivery Option</option>
+              </select>
+            </label>
+            <div className="star-rating-container">
+              <h2>Filter by Star Rating</h2>
+              <div className="star-row">
+                <label>Min Stars:</label>
+                <StarRating
+                  currentRating={minStars}
+                  viewOnly={false}
+                  updateRating={rating => {
+                    setMinStars(rating);
+                    validationStars(rating, maxStars);
+                  }}
+                />
+              </div>
+              <div className="star-row">
+                <label>Max Stars:</label>
+                <StarRating
+                  currentRating={maxStars}
+                  viewOnly={false}
+                  updateRating={rating => {
+                    setMaxStars(rating);
+                    validationStars(minStars, rating);
+                  }}
+                />
+              </div>
+              {validationMessage && (
+                <div style={{ color: 'red' }}>{validationMessage}</div>
+              )}
+            </div>
+            <select
+              name="rating"
+              value={ratingSort}
+              onChange={e => setRatingSort(e.target.value)}
+            >
+              <option value="default">Sort by Rating</option>
+              <option value="asc">Low to High</option>
+              <option value="desc">High to Low</option>
+            </select>
+            <button
+              className="apply-filter-button"
+              onClick={() => setIsSidebarOpen(false)}
+            >
+              Apply
+            </button>
+            <button className="clear-filter-button" onClick={clearFilters}>
+              Clear
+            </button>
+          </div>
+        </div>
+      )}
 
-      <div className="trending-list-container-gold">
-        <h2>Trending</h2>
-        <TrendingList />
-      </div>
-
-      <div className="block">
-        <hr />
-      </div>
-      <ProductsList />
-    </div>
-
-      <div className="block">
-        <ProductSearch />
-      </div>*/}
-
-      <ProductsList view="catalog" />
+      <ProductsList
+        view="catalog"
+        filters={{
+          minPrice,
+          maxPrice,
+          ratingSort,
+          minStars,
+          maxStars,
+          deliveryType,
+          productType,
+        }}
+      />
 
       <div className="block">
         <hr />
