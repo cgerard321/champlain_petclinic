d61384fd9eb58e5a0788bff676b1a826f1c5591d
Drago935 <2233184@champlaincollege.qc.ca>
2025-09-27 12:46:09 -0400

    added missing 'addVetEducation.ts'

---
+++ petclinic-frontend/src/features/veterinarians/api/addVetEducation.ts
@@ -0,0 +1,13 @@
+import axiosInstance from '@/shared/api/axiosInstance';
+import { EducationRequestModel } from '@/features/veterinarians/models/EducationRequestModel';
+
+export async function addVetEducation(
+  education: EducationRequestModel
+): Promise<EducationRequestModel> {
+  const { vetId, ...educationData } = education;
+  const response = await axiosInstance.post(
+    `/vets/${vetId}/educations`,
+    educationData
+  );
+  return response.data;
+}
