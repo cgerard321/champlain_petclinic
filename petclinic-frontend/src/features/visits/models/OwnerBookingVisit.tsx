9c0542760b7feef3136ec6b112bec2d2f8e86615
MariaIsabelGuerrero <mariaigs2005@gmail.com>
2025-09-28 15:32:03 -0400

    fix(visits):  resolve 400 error

--- petclinic-frontend/src/features/visits/models/OwnerBookingVisit.tsx
+++ petclinic-frontend/src/features/visits/models/OwnerBookingVisit.tsx
@@ -76,11 +76,14 @@ const OwnerBookingVisit: React.FC = (): JSX.Element => {
     setSuccessMessage('');
 
     const formattedVisit: VisitRequestModel = {
-      ...visit,
       visitDate: visit.visitStartDate
         .toISOString()
         .slice(0, 16)
         .replace('T', ' '),
+      description: visit.description,
+      petId: visit.petId,
+      practitionerId: visit.practitionerId,
+      status: visit.status,
       ownerId: user.userId,
       jwtToken:
         localStorage.getItem('authToken') ||
