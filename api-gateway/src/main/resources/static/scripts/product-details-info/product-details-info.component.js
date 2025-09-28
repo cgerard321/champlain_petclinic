0de97aa95306cf347634f4f1869a5c445ddd9de3
Robert Ciobanu <159745215+IPvck@users.noreply.github.com>
2025-09-28 17:55:12 -0400

    feat/PROD-CPC-1496_Create_Product_Management_For_Employee (#1175)
    Added an indexed product tab in the employee portal which leads to a new
    Products list page with crud implementations, made similarly to
    inventory products page
    
    **JIRA:** link to jira ticket
    
    https://champlainsaintlambert.atlassian.net/jira/software/c/projects/CPC/boards/71/backlog?search=p&selectedIssue=CPC-1496&text=prod
    ## Context:
    What is the ticket about and why are we doing this change.
    There was no product list or CRUD in the employee portal, so we created
    an index in the nav named 'Products' which leads to a newly created
    ProductsList with crud implementations similarly to inventory
    ## Does this PR change the .vscode folder in petclinic-frontend?:
    no, angular
    If the PR changes the .vscode folder, explain why in detail because it
    should not.
    it does not
    Be sure to include cgerard321 as a reviewer.
    
    <span style="color:red">**Reviewers need to check for any changes to the
    .vscode folder and add a comment about it to their review
    comments.**</span>
    
    ## Changes
    What are the various changes and what other modules do those changes
    affect.
    This can be bullet point or sentence format.
    due to inventory using the namespace of products, the additions were
    made inside of the product folders/files, without touching any of the
    inventory code.
    a new product list folder was added which includes the component,
    controller, router, and template files
    modules for Products crud implementations were made in the pre-existing
    files sharing the name of products
    ## Does this use the v2 API?:
    If the PR uses the v2 API, explain why
    v2 API calls were used because no v1 version exists
    ## Does this add a new communication between services?:
    If the PR adds a new communication between services, make sure to update
    the C4 L2.
    no
    ## Before and After UI (Required for UI-impacting PRs)
    If this is a change to the UI, include before and after screenshots to
    show the differences.
    If this is a new UI feature, include screenshots to show reviewers what
    it looks like.
    <img width="1893" height="882" alt="image"
    src="https://github.com/user-attachments/assets/5b25249d-63e0-4e9d-9dba-d9dfaa2d1980"
    />
    <img width="1290" height="628" alt="image"
    src="https://github.com/user-attachments/assets/0401c307-b766-4614-89eb-cdea8f08e07e"
    />
    <img width="1272" height="783" alt="image"
    src="https://github.com/user-attachments/assets/a903185c-7ed7-414f-886c-5ffe1bdf0c9d"
    />
    <img width="1366" height="766" alt="image"
    src="https://github.com/user-attachments/assets/3e85d2ea-46bb-4b35-90bf-32724b5876ed"
    />
    
    
    ## Dev notes (Optional)
    Specific technical changes that should be noted
    Perhaps the inventory team should move their code in a properly
    identified inventory-products section and leave the normal product
    naming to us for proper module separation.
    ## Linked pull requests (Optional)
    Pull request links
    
    ---------
    
    Co-authored-by: Seddik Ayoub <102536731+Ayoub-Seddik@users.noreply.github.com>

--- api-gateway/src/main/resources/static/scripts/product-details-info/product-details-info.component.js
+++ api-gateway/src/main/resources/static/scripts/product-details-info/product-details-info.component.js
@@ -5,3 +5,10 @@ angular.module('productDetailsInfo')
         templateUrl: 'scripts/product-details-info/product-details-info.template.html',
         controller: 'ProductDetailsInfoController'
     });
+
+angular.module('shopProductDetailsInfo')
+    .component('shopProductDetailsInfo', {
+        templateUrl: 'scripts/product-details-info/shop-product-details-info.template.html',
+        controller: 'ShopProductDetailsInfoController'
+    });
+
