6ee3a487454c302e8b17a58e7a5cb34ed40c3dac
pvck <pvck.v2@gmail.com>
2025-09-28 14:43:02 -0400

    Added an indexed product tab which leads to a new Products list page with crud implementations, made similarly to inventory products page

--- api-gateway/src/main/resources/static/scripts/product-details-info/product-details-info.controller.js
+++ api-gateway/src/main/resources/static/scripts/product-details-info/product-details-info.controller.js
@@ -18,3 +18,23 @@ angular.module('productDetailsInfo')
             });
 
     }]);
+
+angular.module('shopProductDetailsInfo')
+    .controller('ShopProductDetailsInfoController', ["$http", '$state', '$stateParams', function ($http, $state, $stateParams) {
+        var self = this;
+        self.product = {}; // Initialize self.product
+        var productId = $stateParams.productId;
+
+        $http.get('/api/v2/gateway/products/' + productId)
+            .then(function (resp) {
+                // Handle the response data for the specific product
+                var product = resp.data;
+                //console.log("Product found:", product);
+                self.product = product; // Update the product data in your controller
+            })
+            .catch(function (error) {
+                // Handle errors if the product is not found or other issues
+                console.error("Error fetching product:", error);
+            });
+
+    }]);
--- api-gateway/src/main/resources/static/scripts/product-update-form/product-update-form.controller.js
+++ api-gateway/src/main/resources/static/scripts/product-update-form/product-update-form.controller.js
@@ -49,3 +49,46 @@ angular.module('productUpdateForm')
         }
 
     }]);
+
+angular.module('shopProductUpdateForm')
+    .controller('ShopProductUpdateFormController', [
+        "$http", "$state", "$stateParams",
+        function ($http, $state, $stateParams) {
+            var self = this;
+            var productId = $stateParams.productId;
+
+            console.log("ProductId: " + productId);
+
+            // Load product by ID
+            $http.get('/api/v2/gateway/products/' + productId).then(function (resp) {
+                self.product = resp.data;
+            });
+
+            // update
+            self.submitProductUpdateForm = function () {
+                var data = {
+                    productName: self.product.productName,
+                    productDescription: self.product.productDescription,
+                    productPrice: self.product.productPrice,
+                    productQuantity: self.product.productQuantity,
+                    productSalePrice: self.product.productSalePrice
+                };
+
+                $http.put('/api/v2/gateway/products/' + productId, data)
+                    .then(function (response) {
+                        console.log(response);
+                        $state.go('shopProductList');
+                    }, function (response) {
+                        var error = response.data;
+                        error.errors = error.errors || [];
+                        alert(error.error + "\r\n" + error.errors.map(function (e) {
+                            return e.field + ": " + e.defaultMessage;
+                        }).join("\r\n"));
+                    });
+
+                if (!productId) {
+                    console.error("Product ID is missing");
+                }
+            };
+        }
+    ]);
