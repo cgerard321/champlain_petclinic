23c4d985f1ef96e0c6ebaa6cf1162340e5a2f476
Gazou16 <146896335+Gazou16@users.noreply.github.com>
2025-10-01 21:03:00 -0400

    Fixed Vets Testing

--- api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v2/PetControllerIntegrationTest.java
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v2/PetControllerIntegrationTest.java
@@ -112,6 +112,70 @@ class PetControllerIntegrationTest {
                 .verifyComplete();
     }
 
+
+    @Test
+    void whenDeletePet_asVet_withValidPetId_thenReturnPetResponseDTO() throws ParseException {
+        PetResponseDTO pet = PetResponseDTO.builder()
+                .petId("53163352-8398-4513-bdff-b7715c056d1d")
+                .name("Buddy")
+                .birthDate(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").parse("1999-11-01T00:00:00.000+00:00"))
+                .petTypeId("1")
+                .isActive("true")
+                .weight("1.3")
+                .ownerId("e6c7398e-8ac4-4e10-9ee0-03ef33f0361a")
+                .build();
+
+        Mono<PetResponseDTO> result = webTestClient.delete()
+                .uri("/api/v2/gateway/pets/{petId}", pet.getPetId())
+                .cookie("Bearer", jwtTokenForValidVet)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(PetResponseDTO.class)
+                .getResponseBody()
+                .single();
+
+        StepVerifier
+                .create(result)
+                .expectNextMatches(petResponseDTO -> {
+                    assertNotNull(petResponseDTO);
+                    assertThat(petResponseDTO.getPetId()).isEqualTo(pet.getPetId());
+                    assertThat(petResponseDTO.getName()).isEqualTo(pet.getName());
+                    assertThat(petResponseDTO.getBirthDate()).isEqualTo(pet.getBirthDate());
+                    assertThat(petResponseDTO.getPetTypeId()).isEqualTo(pet.getPetTypeId());
+                    assertThat(petResponseDTO.getIsActive()).isEqualTo(pet.getIsActive());
+                    assertThat(petResponseDTO.getWeight()).isEqualTo(pet.getWeight());
+                    return true;
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void whenDeletePet_asVet_withInvalidPetId_thenReturnInvalidInputException() {
+        String invalidPetId = "invalid-pet-id";
+
+        Mono<InvalidInputException> result = webTestClient.delete()
+                .uri("/api/v2/gateway/pets/{petId}", invalidPetId)
+                .cookie("Bearer", jwtTokenForValidVet)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .returnResult(InvalidInputException.class)
+                .getResponseBody()
+                .single();
+
+        StepVerifier
+                .create(result)
+                .expectNextMatches(errorResponse -> {
+                    assertNotNull(errorResponse);
+                    assertThat(errorResponse.getMessage()).isEqualTo("Provided pet id is invalid: " + invalidPetId);
+                    return true;
+                })
+                .verifyComplete();
+    }
+
+
     @Test
     void whenDeletePet_asOwner_withValidPetId_thenReturnPetResponseDTO() throws ParseException {
         PetResponseDTO pet = PetResponseDTO.builder()
