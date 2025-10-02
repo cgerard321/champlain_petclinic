e236fd4e29390e4e2b02e84bb7ded0b5bd83bad1
Yacine Ihaddadene <ihaddadeneyacine4@gmail.com>
2025-10-02 17:26:07 -0400

    backend vet service changes

--- vet-service/src/main/java/com/petclinic/vet/servicelayer/AlbumService.java
+++ vet-service/src/main/java/com/petclinic/vet/servicelayer/AlbumService.java
@@ -1,12 +1,14 @@
-package com.petclinic.vet.servicelayer;
+ package com.petclinic.vet.servicelayer;
 
-import com.petclinic.vet.dataaccesslayer.Album;
-import reactor.core.publisher.Flux;
+ import com.petclinic.vet.dataaccesslayer.Album;
 
-import org.springframework.core.io.Resource;
-import reactor.core.publisher.Mono;
+ import reactor.core.publisher.Flux;
+ import org.springframework.http.codec.multipart.FilePart;
+ import reactor.core.publisher.Mono;
 
-public interface AlbumService {
-    Flux<Album> getAllAlbumsByVetId(String vetId);
-    Mono<Void> deleteAlbumPhotoById(String vetId, Integer Id);
-}
+ public interface AlbumService {
+   Flux<Album> getAllAlbumsByVetId(String vetId);
+   Mono<Void> deleteAlbumPhotoById(String vetId, Integer Id);
+   Mono<Album> insertAlbumPhoto(String vetId, String photoName, Mono<byte[]> fileData);
+   Mono<Album> insertAlbumPhoto(String vetId, String photoName, FilePart file);
+ }
