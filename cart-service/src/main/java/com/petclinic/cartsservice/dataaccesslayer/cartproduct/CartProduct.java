391fcf015eb251582045a95fe799e21b00e29000
Amine Baha <aminebaha115@gmail.com>
2025-10-03 03:40:31 -0400

    clean up

--- cart-service/src/main/java/com/petclinic/cartsservice/dataaccesslayer/cartproduct/CartProduct.java
+++ cart-service/src/main/java/com/petclinic/cartsservice/dataaccesslayer/cartproduct/CartProduct.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.ArrayList; // Unused import for Qodana test
-import java.util.HashMap; 
-import java.util.List;
 @Data
 @Builder
 @NoArgsConstructor
