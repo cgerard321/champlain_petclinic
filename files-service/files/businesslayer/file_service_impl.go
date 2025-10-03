2e82f39c357ae7319440f076f415a3b8fb37c545
Louis Caron <megadoxs@gmail.com>
2025-10-02 21:32:20 -0400

    added addFile, tests, new bucket and modified database table

--- files-service/files/businesslayer/file_service_impl.go
+++ files-service/files/businesslayer/file_service_impl.go
@@ -5,14 +5,18 @@ import (
 	"files-service/files/datalayer"
 	"files-service/files/models"
 	"files-service/files/util/exception"
+	"path"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 type FilesServiceImpl struct {
-	repository         *datalayer.FileLinkRepo
-	minioServiceClient *clientlayer.MinioServiceClient
+	repository         datalayer.FileInfoRepository
+	minioServiceClient clientlayer.MinioServiceClient
 }
 
-func NewFileService(repository *datalayer.FileLinkRepo, minioServiceClient *clientlayer.MinioServiceClient) *FilesServiceImpl {
+func NewFileService(repository datalayer.FileInfoRepository, minioServiceClient clientlayer.MinioServiceClient) *FilesServiceImpl {
 	return &FilesServiceImpl{
 		repository:         repository,
 		minioServiceClient: minioServiceClient,
@@ -23,7 +27,7 @@ func (i *FilesServiceImpl) GetFile(id string) (*models.FileResponseModel, error)
 	fileInfo := i.repository.GetFileInfo(id)
 
 	if fileInfo == nil {
-		return nil, exception.NewNotFoundException("fileId: " + id + " was not inside the database")
+		return nil, exception.NewNotFoundException("fileId: " + id + " was not found")
 	}
 
 	resp, err := i.minioServiceClient.GetFile(fileInfo)
@@ -35,4 +39,49 @@ func (i *FilesServiceImpl) GetFile(id string) (*models.FileResponseModel, error)
 	resp.FileId = id
 
 	return resp, nil
+}
+
+func (i *FilesServiceImpl) AddFile(model *models.FileRequestModel) (*models.FileResponseModel, error) {
+	var fileId string
+	for {
+		fileId = uuid.New().String()
+
+		exists, err := i.repository.ExistsById(fileId)
+		if err != nil {
+			return nil, err
+		}
+		if !exists {
+			break
+		}
+	}
+
+	fileName := strings.Replace(strings.TrimSuffix(model.FileName, path.Ext(model.FileName)), "_", " ", -1)
+	fileInfo := &datalayer.FileInfo{
+		FileId:   fileId,
+		FileType: model.FileType,
+		FileName: fileName,
+	}
+
+	err := i.repository.AddFileInfo(fileInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	err = i.minioServiceClient.AddFile(fileInfo, model.FileData)
+	if err != nil {
+		err2 := i.repository.DeleteFileInfo(fileInfo.FileId) //this is here to remove the data from the database if adding the file to minio failed
+		if err2 != nil {
+			return nil, err2
+		}
+		return nil, err
+	}
+
+	response := &models.FileResponseModel{
+		FileId:   fileInfo.FileId,
+		FileName: fileName,
+		FileType: model.FileType,
+		FileData: model.FileData,
+	}
+
+	return response, nil
 }
--- files-service/files/datalayer/buckets.go
+++ files-service/files/datalayer/buckets.go
@@ -2,4 +2,5 @@ package datalayer
 
 var Buckets = map[string]string{
 	"image": "petclinic-images",
+	"pdf":   "petclinic-pdfs",
 }
