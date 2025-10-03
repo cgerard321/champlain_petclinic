e5fbed5e7025a867eacbda3575358426dd252a62
Yacine_hdn <153969954+yacineihaddadene@users.noreply.github.com>
2025-10-02 23:43:45 -0400

    Update vet-service/src/main/java/com/petclinic/vet/servicelayer/AlbumService.java
    Co-authored-by: Jack Potts <142905205+JackSchool935@users.noreply.github.com>

--- vet-service/src/main/java/com/petclinic/vet/servicelayer/AlbumService.java
+++ vet-service/src/main/java/com/petclinic/vet/servicelayer/AlbumService.java
@@ -6,9 +6,9 @@ import reactor.core.publisher.Flux;
 import org.springframework.http.codec.multipart.FilePart;
 import reactor.core.publisher.Mono;
 
- public interface AlbumService {
-   Flux<Album> getAllAlbumsByVetId(String vetId);
-   Mono<Void> deleteAlbumPhotoById(String vetId, Integer Id);
-   Mono<Album> insertAlbumPhoto(String vetId, String photoName, Mono<byte[]> fileData);
-   Mono<Album> insertAlbumPhoto(String vetId, String photoName, FilePart file);
- }
+public interface AlbumService {
+  Flux<Album> getAllAlbumsByVetId(String vetId);
+  Mono<Void> deleteAlbumPhotoById(String vetId, Integer Id);
+  Mono<Album> insertAlbumPhoto(String vetId, String photoName, Mono<byte[]> fileData);
+  Mono<Album> insertAlbumPhoto(String vetId, String photoName, FilePart file);
+}
