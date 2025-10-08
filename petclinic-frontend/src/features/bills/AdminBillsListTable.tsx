77dc987a7af596a0cd7fc8c6f3e4ed83827eaf53
VladLoghin <92529319+VladLoghin@users.noreply.github.com>
2025-10-07 22:37:19 -0400

    fixed merge conflict

--- petclinic-frontend/src/features/bills/AdminBillsListTable.tsx
+++ petclinic-frontend/src/features/bills/AdminBillsListTable.tsx
@@ -666,7 +666,6 @@ export default function AdminBillsListTable(): JSX.Element {
                 <th>Status</th>
                 <th>Due Date</th>
                 <th>Interest Exempt</th>
-                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
@@ -693,25 +692,6 @@ export default function AdminBillsListTable(): JSX.Element {
                       onToggleComplete={() => getBillsList(currentPage, 10)}
                       variant="simple"
                     />
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => handleDelete(bill.billId)}
-                      title="delete"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="32"
-                        height="32"
-                        fill="currentColor"
-                        className="bi bi-trash"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                      </svg>
-                    </button>
                   </td>
                 </tr>
               ))}
