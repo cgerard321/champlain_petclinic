bca84eb4ea94faaf70087c061ebe1126d585c05d
MattysLeduc <mattys.leduc@gmail.com>
2025-10-12 15:42:09 -0400

    Commented out all the duplicate methods for reviews

--- api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/v1/VisitsControllerV1.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/presentationlayer/v1/VisitsControllerV1.java
@@ -247,11 +247,11 @@ public class VisitsControllerV1 {
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    @IsUserSpecific(idToMatch = {"ownerId"}, bypassRoles = {Roles.ADMIN})
-    @GetMapping(value = "/owners/{ownerId}/reviews", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Flux<ReviewResponseDTO> getReviewsByOwnerId(final @PathVariable String ownerId) {
-        return visitsServiceClient.getReviewsByOwnerId(ownerId);
-    }
+//    @IsUserSpecific(idToMatch = {"ownerId"}, bypassRoles = {Roles.ADMIN})
+//    @GetMapping(value = "/owners/{ownerId}/reviews", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Flux<ReviewResponseDTO> getReviewsByOwnerId(final @PathVariable String ownerId) {
+//        return visitsServiceClient.getReviewsByOwnerId(ownerId);
+//    }
 
     @SecuredEndpoint(allowedRoles = {Roles.ADMIN})
     @PostMapping(value = "/reviews", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -261,14 +261,14 @@ public class VisitsControllerV1 {
                 .defaultIfEmpty(ResponseEntity.badRequest().build());
     }
 
-    @IsUserSpecific(idToMatch = {"ownerId"}, bypassRoles = {Roles.ADMIN})
-    @PostMapping(value = "/owners/{ownerId}/reviews", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<ReviewResponseDTO>> addReviewCustomer(@PathVariable String ownerId, @RequestBody Mono<ReviewRequestDTO> reviewRequestDTOMono) {
-        return reviewRequestDTOMono
-                .flatMap(reviewRequestDTO -> visitsServiceClient.addCustomerReview(ownerId, reviewRequestDTO))
-                .map(c -> ResponseEntity.status(HttpStatus.CREATED).body(c))
-                .defaultIfEmpty(ResponseEntity.badRequest().build());
-    }
+//    @IsUserSpecific(idToMatch = {"ownerId"}, bypassRoles = {Roles.ADMIN})
+//    @PostMapping(value = "/owners/{ownerId}/reviews", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Mono<ResponseEntity<ReviewResponseDTO>> addReviewCustomer(@PathVariable String ownerId, @RequestBody Mono<ReviewRequestDTO> reviewRequestDTOMono) {
+//        return reviewRequestDTOMono
+//                .flatMap(reviewRequestDTO -> visitsServiceClient.addCustomerReview(ownerId, reviewRequestDTO))
+//                .map(c -> ResponseEntity.status(HttpStatus.CREATED).body(c))
+//                .defaultIfEmpty(ResponseEntity.badRequest().build());
+//    }
 
     @SecuredEndpoint(allowedRoles = {Roles.ADMIN})
     @IsUserSpecific(idToMatch = {"reviewId"})
@@ -293,13 +293,13 @@ public class VisitsControllerV1 {
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    @IsUserSpecific(idToMatch = {"ownerId"}, bypassRoles = {Roles.ADMIN})
-    @DeleteMapping(value="/owners/{ownerId}/reviews/{reviewId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseEntity<Void>> deleteCustomerReview(@PathVariable String ownerId, @PathVariable String reviewId) {
-        return visitsServiceClient.deleteReview(ownerId, reviewId)
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.noContent().build());
-    }
+//    @IsUserSpecific(idToMatch = {"ownerId"}, bypassRoles = {Roles.ADMIN})
+//    @DeleteMapping(value="/owners/{ownerId}/reviews/{reviewId}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Mono<ResponseEntity<Void>> deleteCustomerReview(@PathVariable String ownerId, @PathVariable String reviewId) {
+//        return visitsServiceClient.deleteReview(ownerId, reviewId)
+//                .map(ResponseEntity::ok)
+//                .defaultIfEmpty(ResponseEntity.noContent().build());
+//    }
 
     @SecuredEndpoint(allowedRoles = {Roles.ADMIN})
     @GetMapping(value = "/export", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
--- visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/BusinessLayer/Review/ReviewServiceImpl.java
+++ visits-service-new/src/main/java/com/petclinic/visits/visitsservicenew/BusinessLayer/Review/ReviewServiceImpl.java
@@ -70,37 +70,37 @@ public class ReviewServiceImpl implements ReviewService {
                 .map(EntityDtoUtil::toReviewResponseDTO);
     }
 
-    @Override
-    public Flux<ReviewResponseDTO> GetAllReviewsByOwnerId(String ownerId) {
-        return reviewRepository.findAllByOwnerId(ownerId)
-                .map(EntityDtoUtil::toReviewResponseDTO);
-    }
+//    @Override
+//    public Flux<ReviewResponseDTO> GetAllReviewsByOwnerId(String ownerId) {
+//        return reviewRepository.findAllByOwnerId(ownerId)
+//                .map(EntityDtoUtil::toReviewResponseDTO);
+//    }
 
-    @Override
-    public Mono<ReviewResponseDTO> addReview(String ownerId, Mono<ReviewRequestDTO> reviewRequestDTOMono) {
-        return reviewRequestDTOMono.map(dto -> {
-                    Review review = new Review();
-                    review.setOwnerId(ownerId);
-                    review.setRating(dto.getRating());
-                    review.setReviewerName(dto.getReviewerName());
-                    review.setReview(dto.getReview());
-                    review.setDateSubmitted(dto.getDateSubmitted());
+//    @Override
+//    public Mono<ReviewResponseDTO> addReview(String ownerId, Mono<ReviewRequestDTO> reviewRequestDTOMono) {
+//        return reviewRequestDTOMono.map(dto -> {
+//                    Review review = new Review();
+//                    review.setOwnerId(ownerId);
+//                    review.setRating(dto.getRating());
+//                    review.setReviewerName(dto.getReviewerName());
+//                    review.setReview(dto.getReview());
+//                    review.setDateSubmitted(dto.getDateSubmitted());
+//
+//                    review.setReviewId(UUID.randomUUID().toString());
+//
+//                    return review;
+//                })
+//                .flatMap(reviewRepository::save)
+//                .map(EntityDtoUtil::toReviewResponseDTO);
+//    }
 
-                    review.setReviewId(UUID.randomUUID().toString());
-
-                    return review;
-                })
-                .flatMap(reviewRepository::save)
-                .map(EntityDtoUtil::toReviewResponseDTO);
-    }
-
-    @Override
-    public Mono<ReviewResponseDTO> deleteReview(String ownerId, String reviewId) {
-        return reviewRepository.findReviewByOwnerIdAndReviewId(ownerId, reviewId)
-                .switchIfEmpty(Mono.defer(() -> Mono.error(new NotFoundException("Review not found for owner ID: " + ownerId + " and review ID: " + reviewId))))
-                .flatMap(found -> reviewRepository.delete(found).then(Mono.just(found)))
-                .map(EntityDtoUtil::toReviewResponseDTO);
-    }
+//    @Override
+//    public Mono<ReviewResponseDTO> deleteReview(String ownerId, String reviewId) {
+//        return reviewRepository.findReviewByOwnerIdAndReviewId(ownerId, reviewId)
+//                .switchIfEmpty(Mono.defer(() -> Mono.error(new NotFoundException("Review not found for owner ID: " + ownerId + " and review ID: " + reviewId))))
+//                .flatMap(found -> reviewRepository.delete(found).then(Mono.just(found)))
+//                .map(EntityDtoUtil::toReviewResponseDTO);
+//    }
 
 
 }
