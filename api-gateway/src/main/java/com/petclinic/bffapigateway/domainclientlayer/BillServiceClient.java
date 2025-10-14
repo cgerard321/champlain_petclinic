cb63019f98d57be4df4181ecf69bbf4d26e9c4a3
Sean Bedard <85085502+SeanBedard@users.noreply.github.com>
2025-10-14 19:54:33 -0400

    Apply suggestion from @SeanBedard

--- api-gateway/src/main/java/com/petclinic/bffapigateway/domainclientlayer/BillServiceClient.java
+++ api-gateway/src/main/java/com/petclinic/bffapigateway/domainclientlayer/BillServiceClient.java
@@ -18,7 +18,7 @@ import reactor.core.publisher.Mono;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.Optional;
-
+import java.time.LocalDate;
 @Component
 @Slf4j
 public class BillServiceClient {
