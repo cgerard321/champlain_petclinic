c859fb0761325b137b4aaa151cc3df302c7bb377
Amine Baha <aminebaha115@gmail.com>
2025-10-18 13:52:14 -0400

    Fix Angular frontend Vite configuration and API connectivity

---
+++ angular-frontend/vite.config.ts
@@ -0,0 +1,46 @@
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [],
+    resolve: {
+      alias: {
+        '@/': new URL('./src/', import.meta.url).pathname,
+      },
+    },
+    envDir: 'src/environments/',
+    server: {
+      port: 4200,
+      host: true,
+      fs: {
+        allow: ['..']
+      },
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    },
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      outDir: 'dist',
+      sourcemap: true,
+      assetsDir: 'assets',
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name]-[hash].js',
+          chunkFileNames: 'assets/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash].[ext]'
+        }
+      }
+    },
+    optimizeDeps: {
+      include: ['@angular/core', '@angular/common', '@angular/forms', '@angular/router'],
+      force: true
+    },
+  };
+});
