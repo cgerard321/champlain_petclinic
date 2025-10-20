b97990f92046da63ff7a576a1da87c6ae8e8daa9
OlivierD <olivedufort@gmail.com>
2025-10-20 19:19:33 -0400

    Added complete tests

--- api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v1/Owners/OwnerControllerV1IntegrationTests.java
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v1/Owners/OwnerControllerV1IntegrationTests.java
@@ -21,6 +21,7 @@ import reactor.test.StepVerifier;
 import java.util.List;
 import static com.petclinic.bffapigateway.presentationlayer.v1.mockservers.MockServerConfigAuthService.jwtTokenForValidAdmin;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -230,5 +231,4 @@ public class OwnerControllerV1IntegrationTests {
                 .exchange()
                 .expectStatus().isNotFound();
     }
-
 }
