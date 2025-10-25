a1dfe9f9a0e69404e8aaf0212e6e6ec72e86c864
dylanbrass <dylan.brassard@outlook.com>
2025-10-25 02:06:58 -0400

    Fixed linting

--- dev-tools/src/minio/minio_client.rs
+++ dev-tools/src/minio/minio_client.rs
@@ -2,9 +2,9 @@ use minio::s3::error::Error as MinioError;
 use minio::s3::response::{ListObjectsResponse, PutObjectResponse};
 use minio::s3::segmented_bytes::SegmentedBytes;
 use minio::s3::types::{S3Api, ToStream};
-use rocket::State;
 use rocket::futures::Stream;
 use rocket::http::hyper::body::Bytes;
+use rocket::State;
 
 use crate::http::prelude::{AppError, AppResult};
 use crate::minio::bucket::BucketInfo;
