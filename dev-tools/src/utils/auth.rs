1aec2ea5863b16a4ac7331bcdbea28a281128571
dylanbrass <dylan.brassard@outlook.com>
2025-10-25 14:23:30 -0400

    fmt

--- dev-tools/src/utils/auth.rs
+++ dev-tools/src/utils/auth.rs
@@ -1,6 +1,5 @@
 use crate::core::error::AppError;
 
 pub fn get_pepper() -> Result<String, AppError> {
-    std::env::var("PASSWORD_PEPPER")
-        .map_err(|_| AppError::Internal) // or AppError::FailedDependency
+    std::env::var("PASSWORD_PEPPER").map_err(|_| AppError::Internal) // or AppError::FailedDependency
 }
