84a87423c75564cbf5043e953dd4a6b83f47d172
Amine Baha <148575126+AmineBaha-oss@users.noreply.github.com>
2025-10-26 13:35:12 -0400

    feat(AUTH-CPC-1815): Home page redesign for new angular FE (#1409)

--- employee-frontend/src/app/shared/components/navbar/navbar.component.ts
+++ employee-frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,1 +1,11 @@
-// TODO: To be migrated
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {}
