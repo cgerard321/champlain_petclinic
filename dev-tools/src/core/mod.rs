8c96b99d1a9fe977c62372c13521f7a09edba94d
dylanbrass <dylan.brassard@outlook.com>
2025-10-25 21:10:47 -0400

    Fixed to appresult

--- dev-tools/src/core/mod.rs
+++ dev-tools/src/core/mod.rs
@@ -1,3 +1,4 @@
 pub mod error;
 
+pub mod config;
 pub mod utils;
--- dev-tools/src/domain/usecases/auth/authenticate.rs
+++ dev-tools/src/domain/usecases/auth/authenticate.rs
@@ -1,14 +1,15 @@
 use crate::adapters::output::mysql::auth_repo;
 use crate::adapters::output::mysql::users_repo::get_user_auth_by_email;
 use crate::bootstrap::Db;
-use crate::core::error::AppError;
+use crate::core::error::{AppError, AppResult};
 use crate::core::utils::auth::get_pepper;
+use crate::domain::models::session::Session;
 use crate::domain::usecases::auth::utils::verify_password;
 use chrono::{Duration, NaiveDateTime, Utc};
 use rocket::State;
 use uuid::Uuid;
 
-pub async fn authenticate(db: &State<Db>, email: &str, password: &str) -> Result<Uuid, AppError> {
+pub async fn authenticate(db: &State<Db>, email: &str, password: &str) -> AppResult<Session> {
     let pep = get_pepper();
     let row = get_user_auth_by_email(db, email)
         .await
@@ -25,11 +26,16 @@ pub async fn authenticate(db: &State<Db>, email: &str, password: &str) -> Result
     let session_id = Uuid::new_v4();
     let expires_at: NaiveDateTime = (Utc::now() + Duration::days(1)).naive_utc();
 
-    auth_repo::insert_session(db, session_id, row.id, expires_at)
+    let new_session = auth_repo::insert_session(db, session_id, row.id, expires_at)
         .await
         .map_err(|_e| AppError::Internal)?;
 
     println!("Created session {session_id} for user {}", row.email);
 
-    Ok(session_id)
+    Ok(Session {
+        id: new_session.id,
+        user_id: new_session.user_id,
+        created_at: new_session.created_at,
+        expires_at: new_session.expires_at,
+    })
 }
