7587389a60ddd8318ebaf4a2b3dbd94d28876afa
dylanbrass <dylan.brassard@outlook.com>
2025-11-11 00:50:59 -0500

    Fixed issues

--- dev-tools/src/adapters/input/http/graphql_handler/query_execution_routes.rs
+++ dev-tools/src/adapters/input/http/graphql_handler/query_execution_routes.rs
@@ -3,6 +3,8 @@ use crate::adapters::input::http::guards::auth_guard::AuthenticatedUser;
 use async_graphql::{EmptyMutation, EmptySubscription, Schema};
 use async_graphql_rocket::{GraphQLRequest, GraphQLResponse};
 use rocket::State;
+use crate::application::ports::input::files_port::DynFilesPort;
+use crate::application::ports::output::db_drivers::sql_driver::DynSqlDriver;
 use crate::application::services::user_context::UserContext;
 
 pub type ExecuteSqlQuerySchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
@@ -12,7 +14,8 @@ pub async fn graphql_request(
     schema: &State<ExecuteSqlQuerySchema>,
     request: GraphQLRequest,
     user: AuthenticatedUser,
+    port: &State<DynSqlDriver>,
 ) -> GraphQLResponse {
     let user_ctx: UserContext = user.into();
-    request.data(user_ctx).execute(schema.inner()).await
+    request.data(port).data(user_ctx).execute(schema.inner()).await
 }
