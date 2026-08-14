82d0271a6818e677b772e7de2371703c93bf3db5
dylanbrass <dylan.brassard@outlook.com>
2026-08-14 00:24:52 -0400

    feat: add login to dev tools frontend

--- dev-tools-frontend/src/app/app.config.ts
+++ dev-tools-frontend/src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { apiBaseUrlInterceptor } from '@core/interceptors/api-base-url-interceptor';
+import { authInterceptor } from '@core/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([apiBaseUrlInterceptor, authInterceptor])),
+  ],
 };
