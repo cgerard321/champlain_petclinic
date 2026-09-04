55c68338c592efdc4e42154360f3b4f5d7b5bcf2
Dylan Brassard <71225455+DylanBrass@users.noreply.github.com>
2026-09-03 22:01:10 -0400

    refactor(CPC): remove old and unused services in the code base (#1473)
    **JIRA:** link to jira ticket
    ## Context:
    What is the ticket about and why are we doing this change.
    ## Does this PR change the .vscode folder in petclinic-frontend?:
    If the PR changes the .vscode folder, explain why in detail because it
    should not.
    Be sure to include cgerard321 as a reviewer.
    
    <span style="color:red">**Reviewers need to check for any changes to the
    .vscode folder and add a comment about it to their review
    comments.**</span>
    
    ## Changes
    What are the various changes and what other modules do those changes
    affect.
    This can be bullet point or sentence format.
    ## Does this use the v2 API?:
    If the PR uses the v2 API, explain why
    ## Does this add a new communication between services?:
    If the PR adds a new communication between services, make sure to update
    the C4 L2.
    ## Before and After UI (Required for UI-impacting PRs)
    If this is a change to the UI, include before and after screenshots to
    show the differences.
    If this is a new UI feature, include screenshots to show reviewers what
    it looks like.
    ## Dev notes (Optional)
    Specific technical changes that should be noted
    ## Linked pull requests (Optional)
    Pull request links

--- customers-service/src/main/java/com/petclinic/customers/customerExceptions/exceptions/InvalidInputException.java
+++
@@ -1,9 +0,0 @@
-package com.petclinic.customers.customerExceptions.exceptions;
-
-public class InvalidInputException extends RuntimeException {
-
-    public InvalidInputException() {}
-
-    public InvalidInputException(String message) { super(message); }
-}
-
