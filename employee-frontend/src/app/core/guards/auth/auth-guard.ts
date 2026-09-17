d02e21fe74da57b03ca21df0f3db76a2a189afb5
dylanbrass <dylan.brassard@outlook.com>
2026-09-17 18:21:23 -0400

    doc: added documentation to guard

--- employee-frontend/src/app/core/guards/auth/auth-guard.ts
+++ employee-frontend/src/app/core/guards/auth/auth-guard.ts
@@ -3,6 +3,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthState } from '@core/services/auth-state';
 import { Roles } from '@shared/models/roles';
 
+/**
+ * The auth guard checks if the user is authenticated and has the required roles.
+ * If the roles are not provided, the user is allowed to access the route.
+ * If the user is not authenticated, they are redirected to the login page.
+ * If the user does not have any of the required roles, they are redirected to the forbidden page.
+ * @param allowedRoles
+ */
 export const authGuard =
   (allowedRoles?: Roles[]): CanActivateFn =>
   () => {
