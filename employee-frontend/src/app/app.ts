0e7059b612750020b9f6f1f4b464d25cf456cd04
dylanbrass <dylan.brassard@outlook.com>
2026-09-17 11:41:37 -0400

    refactor: remove unused imports

--- employee-frontend/src/app/app.ts
+++ employee-frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
--- employee-frontend/src/app/features/auth/services/auth.ts
+++ employee-frontend/src/app/features/auth/services/auth.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { Observable, switchMap, tap } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
 import { AuthState } from '@core/services/auth-state';
 import { LoginRequest } from '@features/auth/models/loginRequest';
 
