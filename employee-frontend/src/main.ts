a98f0492e4f9b4579191bd01554196928bd3ff71
Loukmane Bessam <loukmanebessam@gmail.com>
2026-09-22 12:57:52 -0400

    feat(VETS-CPC-1927): install and configure i18n with fr/en translation files

--- employee-frontend/src/main.ts
+++ employee-frontend/src/main.ts
@@ -1,10 +1,34 @@
+//loukmane Bessam 2430635
+
 import 'zone.js';
+//for angular to see automatically the changes
 import '@angular/compiler';
+//compiles HTML templates
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+// contains all other components.
 import { provideHttpClient } from '@angular/common/http';
+// Provides HttpClient service
 import { provideAnimations } from '@angular/platform-browser/animations';
+// required for transitions and animations in the app.
 
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), provideAnimations()],
+import { provideTranslateService } from '@ngx-translate/core';
+
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
+bootstrapApplication(AppComponent, //app component inside DOM
+    {
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/internationalization_json/',   // folder where the translation files are located
+        suffix: '.json',    // file extension of the translation files
+      }),
+      fallbackLang: 'fr',   // language used when the is a key missing in the choosed language
+      lang: 'fr',           // default active language on startup (on est au quebec !)
+    }),
+  ],
 }).catch(err => console.error(err));
+//start application and  .catch() catches and logs any error that occurs during bootstrap.
