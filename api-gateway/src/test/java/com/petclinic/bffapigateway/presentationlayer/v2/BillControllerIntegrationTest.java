1230df28c3c5b8182bb78e32d4f99cad6813f245
Kyle Galbraith <kyleg@WHYWORLD>
2026-09-26 15:28:27 -0400

    Fixed broken tests

--- api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v2/BillControllerIntegrationTest.java
+++ api-gateway/src/test/java/com/petclinic/bffapigateway/presentationlayer/v2/BillControllerIntegrationTest.java
@@ -120,7 +120,7 @@ public class BillControllerIntegrationTest {
     @Test
     void whenGetAllBillsByPageAsAdmin_thenReturnPaginatedBills() {
         webTestClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/api/v2/gateway")
+                .uri(uriBuilder -> uriBuilder.path("/api/v2/gateway/bills/paginated")
                         .queryParam("page", "1")
                         .queryParam("size", "10")
                         .build())
@@ -138,7 +138,7 @@ public class BillControllerIntegrationTest {
     @Test
     void whenGetAllBillsByPageWithInvalidRole_thenUnauthorized() {
         webTestClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/api/v2/gateway/bills")
+                .uri(uriBuilder -> uriBuilder.path("/api/v2/gateway/bills/paginated")
                         .queryParam("page", "1")
                         .queryParam("size", "10")
                         .build())
@@ -221,7 +221,7 @@ public class BillControllerIntegrationTest {
     @Test
     void whenGetBillsByMonthAsAdmin_thenReturnBills() {
         webTestClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/api/v2/gateway/bills")
+                .uri(uriBuilder -> uriBuilder.path("/api/v2/gateway/bills/paginated")
                         .queryParam("month", "10")
                         .queryParam("year", "2024")
                         .build())
